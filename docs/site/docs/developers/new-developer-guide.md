6cf8efe9e3f4c44d53f364bc3404da1e5ad543ed
emiliano bonilla <emilbon99@gmail.com>
2022-09-13 00:03:54 -0400

    19 added tools and technology section

---
+++ docs/site/docs/developers/new-developer-guide.md
@@ -0,0 +1,21 @@
+---
+sidebar_position: 1
+---
+
+# What I need to Write
+
+# Repository Structure
+
+# Tools and Technologies
+
+# Workflows
+
+## Git Workflow
+
+## Documentation
+
+## Testing
+
+## Pull Requests + Code Reviews
+
+## Releases
---
+++ docs/site/src/components/developer/Technology.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Space} from "@synnaxlabs/pluto";
+
+export type TechnologyProps = {
+    imageSrc: string;
+    imageAlt: string;
+    imageWidth: string;
+    imageHref: string;
+    text: string;
+    title: string;
+}
+
+export default function Technology(props: TechnologyProps) {
+    return (
+        <Space direction="vertical" size="small">
+            <h3>{props.title}</h3>
+            <Space direction="horizontal" align="center" size="large">
+                <a href={props.imageHref}>
+                    <img
+                        src={props.imageSrc}
+                        alt={props.imageAlt}
+                        width={props.imageWidth}/>
+                </a>
+                <p>
+                    {props.text}
+
+                </p>
+            </Space>
+        </Space>
+    )
+}
