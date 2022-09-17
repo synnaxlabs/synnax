03b2e5d83269612fc0bc88589db21b021cbdac23
emiliano bonilla <emilbon99@gmail.com>
2022-09-17 10:36:09 -0400

    added tests for ferrors package

--- freighter/go/ferrors/errors.go
+++ freighter/go/ferrors/errors.go
@@ -3,10 +3,17 @@ package ferrors
 type Type string
 
 const (
-	Empty   = Type("")
-	Nil     = Type("nil")
+	// Empty represents an error that hasn't been properly parsed or detected.
+	// Empty errors typically represent a programming error.
+	Empty = Type("")
+	// Nil represents a nil error i.e. one that has not occurred.
+	Nil = Type("nil")
+	// Unknown represents an error that was not registered with the ferrors package.
 	Unknown = Type("unknown")
-	Roach   = Type("roach")
+	// Roach represents an error type that was encoded using cockroachdb's errors package.
+	// This is the default error type for errors that are not registered with the ferrors package,
+	// and is used mostly for go-to-go communication.
+	Roach = Type("roach")
 )
 
 type Error interface {
@@ -16,10 +23,10 @@ type Error interface {
 	FreighterType() Type
 }
 
-func WithType(err error, t Type) error {
-	if err == nil {
-		return nil
-	}
+// Typed is the easiest way to create an error type that satisfies the Error interface.
+// It takes the error and attaches the provided type to it. Then you can define
+// encode and decode functions for the type and register them with the ferrors package.
+func Typed(err error, t Type) error {
 	return &typed{error: err, t: t}
 }
 
@@ -30,4 +37,5 @@ type typed struct {
 
 var _ Error = (*typed)(nil)
 
+// FreighterType implements Error.
 func (t typed) FreighterType() Type { return t.t }
