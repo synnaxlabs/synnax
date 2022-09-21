f4f0210badef10fb432915c08cf7bd8d3217f0fa
emiliano bonilla <emilbon99@gmail.com>
2022-09-21 13:52:46 -0400

    removed unary server interface

--- freighter/ts/src/lib/unary.ts
+++ freighter/ts/src/lib/unary.ts
@@ -1,9 +1,3 @@
 export interface UnaryClient<RQ, RS> {
   send(target: string, req: RQ): Promise<[RS | undefined, Error | undefined]>;
 }
-
-export interface UnaryServer<RQ, RS> {
-  bind_handle(
-    handle: (req: RQ) => Promise<[RS | undefined, Error | undefined]>
-  ): void;
-}
