29dcfadf7aa097658bdf455d089c31e38744bcc4
emiliano bonilla <emilbon99@gmail.com>
2022-10-10 13:08:57 -0400

    increased read buffer size

--- synnax/pkg/server/http.go
+++ synnax/pkg/server/http.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/synnaxlabs/freighter/fhttp"
+	"github.com/synnaxlabs/x/telem"
 )
 
 type HTTPBranch struct {
@@ -19,7 +20,10 @@ func (f *HTTPBranch) Match() []cmux.Matcher {
 func (f *HTTPBranch) Key() string { return "http" }
 
 func (f *HTTPBranch) Serve(cfg BranchConfig) error {
-	f.app = fiber.New(fiber.Config{DisableStartupMessage: true})
+	f.app = fiber.New(fiber.Config{
+		DisableStartupMessage: true,
+		ReadBufferSize:        int(10 * telem.Kilobytes),
+	})
 	f.app.Use(cors.New(cors.Config{AllowOrigins: "*"}))
 	for _, t := range f.Transports {
 		t.BindTo(f.app)
