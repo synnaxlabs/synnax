c448ddd0393d32414d54efd11a8ba4d3bf58ccda
emiliano bonilla <emilbon99@gmail.com>
2022-11-01 12:23:17 -0400

    fixed synnax build issue

--- synnax/pkg/ui/src/main.tsx
+++ synnax/pkg/ui/src/main.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from "react-dom/client";
-import "@synnaxlabs/pluto/dist/style.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 		<h1>Synnax UI</h1>
