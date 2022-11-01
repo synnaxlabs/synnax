96aef562a05d3f83ca5d85bee1b507e25bba0a60
emiliano bonilla <emilbon99@gmail.com>
2022-11-01 11:46:35 -0400

    fixed server UI build

--- synnax/pkg/ui/src/main.tsx
+++ synnax/pkg/ui/src/main.tsx
@@ -1,15 +1,6 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Theme } from "@synnaxlabs/pluto";
-import Login from "./routes/Login";
 import "@synnaxlabs/pluto/dist/style.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Theme.Provider themes={[Theme.themes.synnaxLight, Theme.themes.synnaxDark]}>
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-  </Theme.Provider>
+		<h1>Synnax UI</h1>
 );
