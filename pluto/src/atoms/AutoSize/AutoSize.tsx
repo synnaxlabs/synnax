31a6c57c3b9f97043342bb5ddc563102852a85a5
emiliano bonilla <emilbon99@gmail.com>
2022-11-18 14:10:04 -0600

    component library cleanup

--- pluto/src/atoms/AutoSize/AutoSize.tsx
+++ pluto/src/atoms/AutoSize/AutoSize.tsx
@@ -2,26 +2,23 @@ import { ComponentType, ReactElement, cloneElement, useRef } from "react";
 import { useSize } from "@/hooks";
 
 export interface AutoSizeProps
-  extends Omit<React.HTMLAttributes<HTMLDivElement>, "children"> {
-  children: ComponentType<{ width: number; height: number }> | ReactElement;
-  debounce?: number;
+	extends Omit<React.HTMLAttributes<HTMLDivElement>, "children"> {
+	children: ComponentType<{ width: number; height: number }> | ReactElement;
+	debounce?: number;
 }
-export const AutoSize = ({
-  children: Children,
-  debounce,
-  ...props
-}: AutoSizeProps) => {
-  const ref = useRef<HTMLDivElement>(null);
-  const { width, height } = useSize({ ref, debounce });
-  const content: ReactElement =
-    typeof Children === "function" ? (
-      <Children width={width} height={height} />
-    ) : (
-      cloneElement(Children, { width, height })
-    );
-  return (
-    <div ref={ref} {...props}>
-      {content}
-    </div>
-  );
+
+export const AutoSize = ({ children: Children, debounce, ...props }: AutoSizeProps) => {
+	const ref = useRef<HTMLDivElement>(null);
+	const { width, height } = useSize({ ref, debounce });
+	const content: ReactElement =
+		typeof Children === "function" ? (
+			<Children width={width} height={height} />
+		) : (
+			cloneElement(Children, { width, height })
+		);
+	return (
+		<div ref={ref} {...props}>
+			{content}
+		</div>
+	);
 };
