2a17efb36dd559c419dcd72aa72432fb8fae40fd
emiliano bonilla <emilbon99@gmail.com>
2022-12-02 22:47:36 -0500

    fixed error in gorp preventing a WhereKeys query from proceeding after encountering a key that wasn't found

--- x/io/fs/permissions_test.go
+++ x/io/fs/permissions_test.go
@@ -3,7 +3,7 @@ package fs_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/synnaxlabs/x/fsutil"
+	"github.com/synnaxlabs/x/io/fs"
 	"os"
 )
 
@@ -11,10 +11,10 @@ var _ = Describe("Permissions", func() {
 	Describe("CheckSufficientPermissions", func() {
 		DescribeTable("should return the correct value",
 			func(actual, expected os.FileMode, output bool) {
-				Expect(fsutil.CheckSufficientPermissions(actual, expected)).To(Equal(output))
+				Expect(fs.CheckSufficientPermissions(actual, expected)).To(Equal(output))
 			},
-			Entry("0755 700", os.FileMode(0755), fsutil.OS_USER_RWX, true),
-			Entry("600 700", os.FileMode(600), fsutil.OS_USER_RWX, false),
+			Entry("0755 700", os.FileMode(0755), fs.OS_USER_RWX, true),
+			Entry("600 700", os.FileMode(600), fs.OS_USER_RWX, false),
 		)
 
 	})
