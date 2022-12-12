79bf8d5d3773fa5bffbeebb2ce07c2133ef03d9b
emiliano bonilla <emilbon99@gmail.com>
2022-12-11 21:31:05 -0500

    fixed unchanged css style in InputDate and InputTime

--- pluto/src/atoms/Input/InputTime.tsx
+++ pluto/src/atoms/Input/InputTime.tsx
@@ -5,15 +5,15 @@ import { InputProps } from "./Input";
 export interface InputTimeProps extends InputProps {}
 
 export const InputTime = forwardRef<HTMLInputElement, InputTimeProps>(
-	({ size, ...props }: InputTimeProps, ref) => {
-		return (
-			<input
-				ref={ref}
-				type="time"
-				step="1"
-				className={`pluto-input__input pluto-input__input--${size}`}
-				{...props}
-			/>
-		);
-	}
+  ({ size, ...props }: InputTimeProps, ref) => {
+    return (
+      <input
+        ref={ref}
+        type="time"
+        step="1"
+        className={`pluto-input__input pluto-input--${size}`}
+        {...props}
+      />
+    );
+  }
 );
