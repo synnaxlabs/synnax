e7decca5c8129d498cc23ff65dcaec518734b50e
emiliano bonilla <emilbon99@gmail.com>
2022-12-30 18:38:54 -0600

    added license and headers to all files
    Former-commit-id: 32fe5bece1407adda0dd39d3c2cfe6c0faf065c3

--- synnax/pkg/distribution/core/core.go
+++ synnax/pkg/distribution/core/core.go
@@ -1,3 +1,12 @@
+// Copyright 2022 Synnax Labs, Inc.
+//
+// Use of this software is governed by the Business Source License included in the file
+// licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with the Business Source
+// License, use of this software will be governed by the Apache License, Version 2.0,
+// included in the file licenses/APL.txt.
+
 // Package core provides entities for managing distributed storage and networking in a
 // synnax Cluster. It serves as the base for the larger distribution layer.
 package core
--- x/gorp/compoud.go
+++ x/gorp/compoud.go
@@ -1,3 +1,12 @@
+// Copyright 2022 Synnax Labs, Inc.
+//
+// Use of this software is governed by the Business Source License included in the file
+// licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with the Business Source
+// License, use of this software will be governed by the Apache License, Version 2.0,
+// included in the file licenses/APL.txt.
+
 package gorp
 
 // CompoundRetrieve wraps a slice of Retrieve queries into a set of Clauses that are meant to be
