32fe5bece1407adda0dd39d3c2cfe6c0faf065c3
emiliano bonilla <emilbon99@gmail.com>
2022-12-30 18:38:54 -0600

    added license and headers to all files

--- synnax/pkg/distribution/ontology/ontology.go
+++ synnax/pkg/distribution/ontology/ontology.go
@@ -1,3 +1,12 @@
+// Copyright 2022 Synnax Labs, Inc.
+//
+// Use of this software is governed by the Business Source License included in the file
+// licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with the Business Source
+// License, use of this software will be governed by the Apache License, Version 2.0,
+// included in the file licenses/APL.txt.
+
 // Package ontology provides a service for mapping relationships between different resources
 // within a Synnax cluster. It implements a directed acyclic graph (DAG) that allows
 // for the dynamic definition of complex relationship hierarchies. The primary objective
