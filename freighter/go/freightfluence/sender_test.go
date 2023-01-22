be419cae8672420b0022ef283692fc65ff915093
emiliano bonilla <emilbon99@gmail.com>
2023-01-22 17:20:06 -0500

    fixed freighter go test

--- freighter/go/freightfluence/sender_test.go
+++ freighter/go/freightfluence/sender_test.go
@@ -12,12 +12,15 @@ package freightfluence_test
 import (
 	"context"
 	"github.com/cockroachdb/errors"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	"github.com/synnaxlabs/freighter"
 	"github.com/synnaxlabs/freighter/fmock"
 	"github.com/synnaxlabs/freighter/freightfluence"
 	"github.com/synnaxlabs/x/address"
 	"github.com/synnaxlabs/x/confluence"
 	"github.com/synnaxlabs/x/signal"
+	. "github.com/synnaxlabs/x/testutil"
 )
 
 func clientStreamsToSlice(
