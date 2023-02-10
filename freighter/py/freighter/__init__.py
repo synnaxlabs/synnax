dc3591c556db72225f509eee5dde3d3fec233fd1
Brett Lin <brettlin01@gmail.com>
2023-02-10 22:05:35 +0000

    added kwargs to http.py
    Former-commit-id: 8438f4ab6e20a6be8b4719ce4e233c37a167d203

--- freighter/py/freighter/__init__.py
+++ freighter/py/freighter/__init__.py
@@ -26,7 +26,7 @@ from .stream import AsyncStream, AsyncStreamClient, Stream, StreamClient
 from .unary import UnaryClient
 from .url import URL
 from .websocket import WebsocketClient
-from .http import HTTPClientFactory
+from .http import HTTPClientPool
 from .exceptions import register_exception, encode_exception, decode_exception
 from .encoder import EncoderDecoder, MsgpackEncoder, JSONEncoder
 from .sync import SyncStreamClient
