045017b5e5a304836c6753db0bb7f3cea3c73a2c
emiliano bonilla <emilbon99@gmail.com>
2023-03-11 14:27:20 -0500

    [ops] - added workflow for x testing

--- x/ts/src/telem/telem.spec.ts
+++ x/ts/src/telem/telem.spec.ts
@@ -60,7 +60,9 @@ describe("TimeStamp", () => {
     expect(ts.date().getUTCHours()).toEqual(0);
     const ts2 = new TimeStamp("2021-01-01", "local");
     expect(ts2.date().getUTCFullYear()).toEqual(2021);
-    expect(ts2.date().getUTCHours()).toEqual(0);
+    expect(ts2.date().getUTCHours()).toEqual(
+      TimeStamp.utcOffset.valueOf() / TimeStamp.HOUR.valueOf()
+    );
     expect(ts2.date().getUTCMinutes()).toEqual(0);
   });
 
