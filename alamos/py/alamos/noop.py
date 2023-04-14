6087db66e4e771bc219eab7696de0e1690e34351
Emiliano Bonilla <ebonilla@synnaxlabs.com>
2023-04-14 19:07:04 -0400

    [alamos] - added noop support to python instrumentation

---
+++ alamos/py/alamos/noop.py
@@ -0,0 +1,40 @@
+#  Copyright 2023 Synnax Labs, Inc.
+#
+#  Use of this software is governed by the Business Source License included in the file
+#  licenses/BSL.txt.
+#
+#  As of the Change Date specified in that file, in accordance with the Business Source
+#  License, use of this software will be governed by the Apache License, Version 2.0,
+#  included in the file licenses/APL.txt.
+
+from typing import (
+    Protocol,
+    ParamSpec,
+    Concatenate,
+    Callable
+)
+
+
+class Noop(Protocol):
+    """A protocol for a class that can be marked as noop based on a boolean
+    """
+    noop: bool
+
+
+P = ParamSpec('P')
+
+
+def noop(
+    f: Callable[Concatenate[Noop, P], None],
+) -> Callable[Concatenate[Noop, P], None]:
+    """Decorator around a Noop class that will not call the decorated function if the
+    class is marked as noop
+
+    """
+
+    def wrapper(self: Noop, *args: P.args, **kwargs: P.kwargs) -> None:
+        if self.noop:
+            return
+        return f(self, *args, **kwargs)
+
+    return wrapper
