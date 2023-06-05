d3c00624bf95c7a5107a45d419f50f41f3f2dd20
Emiliano Bonilla <ebonilla@synnaxlabs.com>
2023-06-05 11:30:58 -0600

    [pluto] - cleaned up AxisCanvas rendering code'
    Former-commit-id: 29fce3fc1d2ff776a8e3640111cbe4e498fae833

--- pluto/src/core/vis/Axis/AxisCanvas.tsx
+++ pluto/src/core/vis/Axis/AxisCanvas.tsx
@@ -7,6 +7,8 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
+import { Dimensions, XY } from "@synnaxlabs/x";
+
 import { textDimensions } from "@/core/std/Typography/textDimensions";
 import {
   AxisContext,
@@ -14,7 +16,7 @@ import {
   AxisState,
   ParsedAxisState,
 } from "@/core/vis/Axis/core";
-import { TickFactory, newTickFactory } from "@/core/vis/Axis/TickFactory";
+import { Tick, TickFactory, newTickFactory } from "@/core/vis/Axis/TickFactory";
 import { RenderContext } from "@/core/vis/render";
 
 const TICK_LINE_SIZE = 4;
@@ -60,156 +62,125 @@ export class AxisCanvas {
   drawBottom(ctx: AxisContext): void {
     const { canvas } = this.ctx;
     const { plottingRegion } = ctx;
-    const { position: p, showGrid } = this.state;
     const size = plottingRegion.width;
     const gridSize = plottingRegion.height;
-
-    canvas.beginPath();
-    canvas.moveTo(p.x, p.y);
-    canvas.lineTo(p.x + size, p.y);
-    canvas.stroke();
-
+    const p = this.state.position;
+    this.drawLine(p, p.translateX(size));
     const ticks = this.tickFactory.generate({ ...ctx, size });
-
-    ticks.forEach((tick) => {
-      const { width, height } = textDimensions(
-        tick.label,
-        this.state.font,
-        this.ctx.canvas
-      );
+    this.drawTicks(ticks, (d, tick) => {
       canvas.moveTo(p.x + tick.position, p.y);
       canvas.lineTo(p.x + tick.position, p.y + TICK_LINE_SIZE);
       canvas.stroke();
-      canvas.fillText(tick.label, p.x + tick.position - width / 2, p.y + 5 + height);
+      canvas.fillText(
+        tick.label,
+        p.x + tick.position - d.width / 2,
+        p.y + 5 + d.height
+      );
     });
-    if (showGrid) {
-      canvas.strokeStyle = this.state.gridColor.hex;
-      ticks.forEach((tick) => {
-        canvas.beginPath();
-        canvas.moveTo(p.x + tick.position, p.y);
-        canvas.lineTo(p.x + tick.position, p.y - gridSize);
-        canvas.stroke();
-      });
-    }
+    this.maybeDrawGrid(size, ticks, (tick) => [
+      p.translateX(tick.position),
+      p.translate({ x: tick.position, y: -gridSize }),
+    ]);
   }
 
   drawTop(ctx: AxisContext): void {
     const { canvas } = this.ctx;
     const { plottingRegion } = ctx;
-    const { position: _p, showGrid } = this.state;
     const size = plottingRegion.width;
     const gridSize = plottingRegion.height;
-    const p = { x: _p.x, y: _p.y + 20 };
-
-    canvas.beginPath();
-
-    canvas.moveTo(p.x, p.y);
-    canvas.lineTo(p.x + size, p.y);
-    canvas.stroke();
-
+    const p = this.state.position.translateY(20);
+    this.drawLine(p, p.translateX(size));
     const ticks = this.tickFactory.generate({ ...ctx, size });
-
-    ticks.forEach((tick) => {
-      const { width, height } = textDimensions(
-        tick.label,
-        this.state.font,
-        this.ctx.canvas
-      );
+    this.drawTicks(ticks, (d, tick) => {
       canvas.moveTo(p.x + tick.position, p.y);
       canvas.lineTo(p.x + tick.position, p.y - TICK_LINE_SIZE);
       canvas.stroke();
-
-      canvas.fillText(tick.label, p.x + tick.position - width / 2, p.y - 5 - height);
+      canvas.fillText(
+        tick.label,
+        p.x + tick.position - d.width / 2,
+        p.y - 5 - d.height
+      );
     });
-
-    if (showGrid) {
-      canvas.strokeStyle = this.state.gridColor.hex;
-      ticks
-        .filter((tick) => tick.position !== 0 && tick.position !== size)
-        .forEach((tick) => {
-          canvas.beginPath();
-          canvas.moveTo(p.x + tick.position, p.y);
-          canvas.lineTo(p.x + tick.position, p.y + gridSize);
-          canvas.stroke();
-        });
-    }
+    this.maybeDrawGrid(size, ticks, (tick) => [
+      p.translateX(tick.position),
+      p.translate({ x: tick.position, y: gridSize }),
+    ]);
   }
 
   drawLeft(ctx: AxisContext): void {
     const { canvas } = this.ctx;
     const { plottingRegion } = ctx;
-    const { position: _p, showGrid } = this.state;
     const size = plottingRegion.height;
     const gridSize = plottingRegion.width;
-    const p = { x: _p.x + 40, y: _p.y };
-
-    canvas.beginPath();
-    canvas.moveTo(p.x, p.y);
-    canvas.lineTo(p.x, p.y + size);
-    canvas.stroke();
-
+    const p = this.state.position.translateX(40);
+    this.drawLine(p, p.translateY(size));
     const ticks = this.tickFactory.generate({ ...ctx, size });
-
-    ticks.forEach((tick) => {
-      const { height, width } = textDimensions(
-        tick.label,
-        this.state.font,
-        this.ctx.canvas
-      );
+    this.drawTicks(ticks, (d, tick) => {
       canvas.moveTo(p.x, p.y + tick.position);
       canvas.lineTo(p.x - TICK_LINE_SIZE, p.y + tick.position);
       canvas.stroke();
       canvas.fillText(
         tick.label,
-        p.x - width - TICK_LINE_SIZE * 2,
-        p.y + tick.position + height / 2
+        p.x - d.width - TICK_LINE_SIZE * 2,
+        p.y + tick.position + d.height / 2
       );
     });
-    if (showGrid) {
-      canvas.strokeStyle = this.state.gridColor.hex;
-      ticks
-        .filter((tick) => tick.position !== 0 && tick.position !== size)
-        .forEach((tick) => {
-          canvas.beginPath();
-          canvas.moveTo(p.x, p.y + tick.position);
-          canvas.lineTo(p.x + gridSize, p.y + tick.position);
-          canvas.stroke();
-        });
-    }
+    this.maybeDrawGrid(size, ticks, (tick) => [
+      p.translateY(tick.position),
+      p.translate({ x: gridSize, y: tick.position }),
+    ]);
   }
 
   drawRight(ctx: AxisContext): void {
     const { canvas } = this.ctx;
     const { plottingRegion } = ctx;
-    const { position: _p, showGrid } = this.state;
     const size = plottingRegion.height;
     const gridSize = plottingRegion.width;
-    const p = { x: _p.x, y: _p.y };
-
+    const p = this.state.position;
     canvas.beginPath();
     canvas.moveTo(p.x, p.y);
     canvas.lineTo(p.x, p.y + size);
     canvas.stroke();
-
     const ticks = this.tickFactory.generate({ ...ctx, size });
-
-    ticks.forEach((tick) => {
+    this.drawTicks(ticks, (_, tick) => {
       canvas.moveTo(p.x, p.y + tick.position);
       canvas.lineTo(p.x + 5, p.y + tick.position);
       canvas.stroke();
       canvas.fillText(tick.label, p.x + 10, p.y + tick.position + 5);
     });
+    this.maybeDrawGrid(size, ticks, (tick) => [
+      p.translateY(tick.position),
+      p.translate({ x: -gridSize, y: tick.position }),
+    ]);
+  }
 
+  private drawLine(start: XY, end: XY): void {
+    this.ctx.canvas.beginPath();
+    this.ctx.canvas.moveTo(...start.couple);
+    this.ctx.canvas.lineTo(...end.couple);
+    this.ctx.canvas.stroke();
+  }
+
+  private drawTicks(
+    ticks: Tick[],
+    f: (textDimensions: Dimensions, tick: Tick) => void
+  ): void {
+    ticks.forEach((tick) =>
+      f(textDimensions(tick.label, this.state.font, this.ctx.canvas), tick)
+    );
+  }
+
+  private maybeDrawGrid(
+    size: number,
+    ticks: Tick[],
+    f: (tick: Tick) => [XY, XY]
+  ): void {
+    const { showGrid, gridColor } = this.state;
     if (showGrid) {
-      canvas.strokeStyle = this.state.gridColor.hex;
+      this.ctx.canvas.strokeStyle = gridColor.hex;
       ticks
         .filter((tick) => tick.position !== 0 && tick.position !== size)
-        .forEach((tick) => {
-          canvas.beginPath();
-          canvas.moveTo(p.x, p.y + tick.position);
-          canvas.lineTo(p.x - gridSize, p.y + tick.position);
-          canvas.stroke();
-        });
+        .forEach((tick) => this.drawLine(...f(tick)));
     }
   }
 }
