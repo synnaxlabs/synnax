7d33a2dfeab780bf4bdf7a291e0eacd9dfa88933
Emiliano Bonilla <ebonilla@synnaxlabs.com>
2023-09-17 10:32:15 -0400

    [freighter/integration] - added grpc server on http 8081'

--- freighter/integration/main.go
+++ freighter/integration/main.go
@@ -10,17 +10,52 @@
 package main
 
 import (
+	"context"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	igrp "github.com/synnaxlabs/freighter/integration/grpc"
 	"github.com/synnaxlabs/freighter/integration/server"
+	xsig "github.com/synnaxlabs/x/signal"
 	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"net"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	app := fiber.New(fiber.Config{DisableStartupMessage: true})
 	app.Use(logger.New())
 	server.BindTo(app)
-	if err := app.Listen(":8080"); err != nil {
-		zap.S().Fatalw("server failed", "err", err)
+	interruptC := make(chan os.Signal, 1)
+	signal.Notify(interruptC, os.Interrupt)
+	g := grpc.NewServer()
+	s := igrp.New()
+	s.BindTo(g)
+
+	err := func() error {
+		sCtx, cancel := xsig.Isolated()
+		sCtx.Go(func(ctx context.Context) error {
+			return app.Listen(":8080")
+		})
+
+		lis, err := net.Listen("tcp", ":8081")
+		if err != nil {
+			return err
+		}
+		sCtx.Go(func(ctx context.Context) error {
+			return g.Serve(lis)
+		})
+		<-interruptC
+		g.Stop()
+		if err := app.Shutdown(); err != nil {
+			return err
+		}
+		cancel()
+		return sCtx.Wait()
+	}()
+
+	if err != nil {
+		zap.S().Fatalw("failed to start server", "error", err)
 	}
 }
