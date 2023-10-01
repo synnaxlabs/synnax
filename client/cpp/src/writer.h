e8918fb77dbd995dafaf2f170d53b444e6e4711d
sverlon <sverlon@umich.edu>
2023-10-01 14:58:09 -0400

    mf I need food

---
+++ client/cpp/src/writer.h
@@ -0,0 +1,8 @@
+// Freighter.
+#include "freighter/gRPC/client.h"
+
+// Compiled protos.
+#include "v1/auth_grpc.grpc.pb.h"
+
+/// @brief 
+template <typename response_t, typename request_t, typename stream_t, typename err_t, typename rpc_t>
