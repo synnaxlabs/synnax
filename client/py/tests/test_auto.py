c8738010ca776ffd73c28de0a91cb13b789113e9
Andrew Zhao <andzhao@umich.edu>
2023-10-08 04:52:45 +0000

    Updated state interface

--- client/py/tests/test_auto.py
+++ client/py/tests/test_auto.py
@@ -1,33 +1,31 @@
-
 import pytest
-import numpy as np
-import pandas as pd
-
 import synnax as sy
-from synnax.simulation.auto import Simulator
-from synnax.simulation.auto import TankPressure
-from synnax.simulation.auto import VoltageZero
-from synnax.simulation.auto import VoltageFull
-from synnax.simulation.auto import Nominal
-from synnax.simulation.auto import PreValveStuckOpen
+from synnax.simulation.auto import (
+    Simulator,
+    TankPressure,
+    VoltageFull,
+    VoltageZero,
+    Nominal,
+    PreValveStuckOpen,
+)
+
 
 @pytest.mark.auto
-class TestSimulator:   
+class TestSimulator:
     def test_init(self):
-        state = {}
-        nominal = TankPressure(state)
+        state = {"0": 0, "1": 0, "2": 1}
+        nominal = TankPressure("0", "1", "2")
         anomaly = VoltageZero(nominal, 100)
         simmy = Simulator(state, nominal, anomaly)
-        
+
         assert simmy.state["0"] == 0
         assert simmy.state["1"] == 0
         assert simmy.state["2"] == 1
-
         return None
-    
+
     def test_nominal(self):
-        state = {}
-        nominal = TankPressure(state)
+        state = {"0": 0, "1": 0, "2": 1}
+        nominal = TankPressure("0", "1", "2")
         anomaly = Nominal(nominal, 100)
         simmy = Simulator(state, nominal, anomaly)
         simmy.run()
@@ -35,10 +33,10 @@ class TestSimulator:
         assert simmy.state["0"] == 400
 
         return None
-    
+
     def test_voltage_zero(self):
-        state = {}
-        nominal = TankPressure(state)
+        state = {"0": 0, "1": 0, "2": 1}
+        nominal = TankPressure("0", "1", "2")
         anomaly = VoltageZero(nominal, 100)
         simmy = Simulator(state, nominal, anomaly)
         simmy.run()
@@ -46,10 +44,10 @@ class TestSimulator:
         assert simmy.state["0"] == -200
 
         return None
-    
+
     def test_voltage_full(self):
-        state = {}
-        nominal = TankPressure(state)
+        state = {"0": 0, "1": 0, "2": 1}
+        nominal = TankPressure("0", "1", "2")
         anomaly = VoltageFull(nominal, 100)
         simmy = Simulator(state, nominal, anomaly)
         simmy.run()
@@ -59,8 +57,8 @@ class TestSimulator:
         return None
 
     def test_prevalve_stuck_open(self):
-        state = {}
-        nominal = TankPressure(state)
+        state = {"0": 0, "1": 0, "2": 1}
+        nominal = TankPressure("0", "1", "2")
         anomaly = PreValveStuckOpen(nominal, 100)
         simmy = Simulator(state, nominal, anomaly)
         simmy.run()
