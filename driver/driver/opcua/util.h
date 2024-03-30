dead912acd0a2104377276529823fab52ebcd9da
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-03-30 18:02:02 -0500

    [driver] - added more copts to library headers

--- driver/driver/opcua/util.h
+++ driver/driver/opcua/util.h
@@ -26,28 +26,28 @@ std::pair<UA_Client *, bool> connect(
 inline synnax::Series val_to_series(UA_Variant* val, synnax::DataType dt) {
     if (val->type == &UA_TYPES[UA_TYPES_FLOAT]) {
         const auto value = *static_cast<UA_Float*>(val->data);
-        if (dt == FLOAT32) return Series(value);
-        if (dt == FLOAT64) return Series(static_cast<double>(value));
+        if (dt == synnax::FLOAT32) return Series(value);
+        if (dt == synnax::FLOAT64) return Series(static_cast<double>(value));
     }
     if (val->type == &UA_TYPES[UA_TYPES_DOUBLE]) {
         const auto value = *static_cast<UA_Double*>(val->data);
         // TODO - warn on potential precision drop here
-        if (dt == FLOAT32) return Series(static_cast<float>(value));
-        if (dt == FLOAT64) return Series(value);
+        if (dt == synnax::FLOAT32) return Series(static_cast<float>(value));
+        if (dt == synnax::FLOAT64) return Series(value);
     }
     if (val->type == &UA_TYPES[UA_TYPES_INT32]) {
         const auto value = *static_cast<UA_Int32*>(val->data);
-        if (dt == INT32) return Series(value);
-        if (dt == INT64) return Series(static_cast<int64_t>(value));
-        if (dt == UINT32) return Series(static_cast<uint32_t>(value));
-        if (dt == UINT64) return Series(static_cast<uint64_t>(value));
+        if (dt == synnax::INT32) return Series(value);
+        if (dt == synnax::INT64) return Series(static_cast<int64_t>(value));
+        if (dt == synnax::UINT32) return Series(static_cast<uint32_t>(value));
+        if (dt == synnax::UINT64) return Series(static_cast<uint64_t>(value));
     }
     if (val->type == &UA_TYPES[UA_TYPES_INT64]) {
         const auto value = *static_cast<UA_Int64*>(val->data);
-        if (dt == INT32) return Series(static_cast<int32_t>(value));
-        if (dt == INT64) return Series(value);
-        if (dt == UINT32) return Series(static_cast<uint32_t>(value));
-        if (dt == UINT64) return Series(static_cast<uint64_t>(value));
+        if (dt == synnax::INT32) return Series(static_cast<int32_t>(value));
+        if (dt == synnax::INT64) return Series(value);
+        if (dt == synnax::UINT32) return Series(static_cast<uint32_t>(value));
+        if (dt == synnax::UINT64) return Series(static_cast<uint64_t>(value));
     }
     return Series(1);
 }
