3cf6b594ae5bf98e393ba486cf5552a03518ba69
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-04-19 19:01:31 -0400

    [docs/site] - continued work on docs refactoring

--- docs/site/src/pages/components/typescript-client/nav.tsx
+++ docs/site/src/pages/components/typescript-client/nav.tsx
@@ -10,43 +10,43 @@
 import { type PageNavNode } from "@/components/PageNav";
 
 export const typescriptClientNav: PageNavNode = {
-    key: "typescript-client",
-    name: "Typescript Client",
-    children: [
-        {
-            key: "/components/typescript-client/get-started",
-            href: "/components/typescript-client/get-started",
-            name: "Get Started",
-        },
-        {
-            key: "/components/typescript-client/channels",
-            href: "/components/typescript-client/channels",
-            name: "Channels",
-        },
-        {
-            key: "/components/typescript-client/read-telemetry",
-            href: "/components/typescript-client/read-telemetry",
-            name: "Read Telemetry",
-        },
-        {
-            key: "/components/typescript-client/write-telemetry",
-            href: "/components/typescript-client/write-telemetry",
-            name: "Write Telemetry",
-        },
-        {
-            key: "/components/typescript-client/stream-telemetry",
-            href: "/components/typescript-client/stream-telemetry",
-            name: "Stream Telemetry"
-        },
-        {
-            key: "/components/typescript-client/series-and-frames",
-            href: "/components/typescript-client/series-and-frames",
-            name: "Series and Frames"
-        },
-        {
-            key: "/components/typescript-client/examples",
-            href: "/components/typescript-client/examples",
-            name: "Examples"
-        }
-    ]
-}
+  key: "typescript-client",
+  name: "Typescript Client",
+  children: [
+    {
+      key: "/components/typescript-client/get-started",
+      href: "/components/typescript-client/get-started",
+      name: "Get Started",
+    },
+    {
+      key: "/components/typescript-client/channels",
+      href: "/components/typescript-client/channels",
+      name: "Channels",
+    },
+    {
+      key: "/components/typescript-client/read-data",
+      href: "/components/typescript-client/read-data",
+      name: "Read Data",
+    },
+    {
+      key: "/components/typescript-client/write-data",
+      href: "/components/typescript-client/write-data",
+      name: "Write Data",
+    },
+    {
+      key: "/components/typescript-client/stream-data",
+      href: "/components/typescript-client/stream-data",
+      name: "Stream Data",
+    },
+    {
+      key: "/components/typescript-client/series-and-frames",
+      href: "/components/typescript-client/series-and-frames",
+      name: "Series and Frames",
+    },
+    {
+      key: "/components/typescript-client/examples",
+      href: "/components/typescript-client/examples",
+      name: "Examples",
+    },
+  ],
+};
