1f2c203e91a6da797956b94ace1e2d5f5f8cb408
Elham Islam <elham@synnaxlabs.com>
2024-05-15 15:28:31 -0400

    added ni units map

--- driver/ni/ni.h
+++ driver/ni/ni.h
@@ -40,39 +40,7 @@
 
 namespace ni{
 
-    // TODO: make const
-    // std::map<std::string, uint32_t> NI_UNITS_MAP = {
-    //     {"Volts", DAQmx_Val_Volts},
-    //     {"Amps", DAQmx_Val_Amps},
-    //     {"DegF", DAQmx_Val_DegF},
-    //     {"DegC", DAQmx_Val_DegC},
-    //     {"DegR", DAQmx_Val_DegR},
-    //     {"Kelvins", DAQmx_Val_Kelvins},
-    //     {"Strain", DAQmx_Val_Strain},
-    //     {"Ohms", DAQmx_Val_Ohms},
-    //     {"Hz", DAQmx_Val_Hz},
-    //     {"Seconds", DAQmx_Val_Seconds},
-    //     {"Meters", DAQmx_Val_Meters},
-    //     {"Inches", DAQmx_Val_Inches},
-    //     {"Degrees", DAQmx_Val_Degrees},
-    //     {"Radians", DAQmx_Val_Radians},
-    //     {"g", DAQmx_Val_g},
-    //     {"MetersPerSecondSquared", DAQmx_Val_MetersPerSecondSquared},
-    //     {"Newtons", DAQmx_Val_Newtons},
-    //     {"Pounds", DAQmx_Val_Pounds},
-    //     {"KilogramForce", DAQmx_Val_KilogramForce},
-    //     {"PoundsPerSquareInch", DAQmx_Val_PoundsPerSquareInch},
-    //     {"Bar", DAQmx_Val_Bar},
-    //     {"Pascals", DAQmx_Val_Pascals},
-    //     {"VoltsPerVolt", DAQmx_Val_VoltsPerVolt},
-    //     {"mVoltsPerVolt", DAQmx_Val_mVoltsPerVolt},
-    //     {"NewtonMeters", DAQmx_Val_NewtonMeters},
-    //     {"InchOunces", DAQmx_Val_InchOunces},
-    //     {"InchPounds", DAQmx_Val_InchPounds},
-    //     {"FootPounds", DAQmx_Val_FootPounds},
-    //     {"Strain", DAQmx_Val_Strain},
-    //     {"FromTEDS", DAQmx_Val_FromTEDS}
-    // };
+    extern const std::map<std::string, uint32_t> NI_UNITS_MAP;
 
     typedef struct LinearScale{
         float64 slope;
