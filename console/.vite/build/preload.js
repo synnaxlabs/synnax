7498c14f6062eebd4d22f463e0431531669a758e
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-05-17 14:36:24 -0400

    forge checkpoint

---
+++ console/.vite/build/preload.js
@@ -0,0 +1,1 @@
+"use strict";const{configurePreload:o}=require("@synnaxlabs/drift/electron"),r={};if(process.contextIsolated)try{o()}catch(e){console.error(e)}else window.electron=electronAPI,window.api=r;
