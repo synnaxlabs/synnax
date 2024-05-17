4da1a82334a5d674c6bd33669ddd2c73b34a8a98
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-05-17 16:10:11 -0400

    builder checkpoint

--- console/preload.ts
+++ console/preload.ts
@@ -1,21 +1,20 @@
-const {configurePreload} = require('@synnaxlabs/drift/electron');
-
+import { configurePreload } from "@synnaxlabs/drift/electron";
 
 // Custom APIs for renderer
-const api = {}
+const api = {};
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
 // just add to the DOM global.
 if (process.contextIsolated) {
   try {
-    configurePreload()
+    configurePreload();
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 } else {
   // @ts-ignore (define in dts)
-  window.electron = electronAPI
+  window.electron = electronAPI;
   // @ts-ignore (define in dts)
-  window.api = api
+  window.api = api;
 }
--- console/vite.config.ts
+++ console/vite.config.ts
@@ -7,11 +7,10 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-import * as path from "path";
-
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import electron from "vite-plugin-electron/simple";
 
 const isDev = process.env.TAURI_DEBUG === "true";
 
@@ -23,13 +22,24 @@ export default defineConfig({
     strictPort: true,
   },
   resolve: {
-    alias: {
-      "@synnaxlabs/pluto/dist": path.resolve(__dirname, "../pluto/dist"),
-      "@synnaxlabs/pluto": path.resolve(__dirname, "../pluto/src"),
-    },
+    // alias: {
+    //   "@synnaxlabs/pluto/dist": path.resolve(__dirname, "../pluto/dist"),
+    //   "@synnaxlabs/pluto": path.resolve(__dirname, "../pluto/src"),
+    // },
   },
   envPrefix: ["VITE_", "TAURI_"],
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    electron({
+      main: {
+        entry: "main.ts",
+      },
+      preload: {
+        input: "preload.ts",
+      },
+    }),
+  ],
   build: {
     target: process.env.TAURI_PLATFORM === "windows" ? "chrome105" : "safari16",
     minify: isDev ? "esbuild" : false,
@@ -37,5 +47,7 @@ export default defineConfig({
     // We don't really care about maintaining a small bundle size right now, as this file
     // is loaded directly from disc instead of OTN
     chunkSizeWarningLimit: 10000 /* kbs */,
+    outDir: "dist",
+    emptyOutDir: false,
   },
 });
