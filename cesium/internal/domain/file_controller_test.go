54d9afa48741a0e74c7e4b9144a4ca4adb6a3bc6
Léo Liu <leoliuhy@gmail.com>
2024-05-19 17:01:15 -0400

    Added test to file controller to assert the data is right

--- cesium/internal/domain/file_controller_test.go
+++ cesium/internal/domain/file_controller_test.go
@@ -248,6 +248,56 @@ var _ = Describe("File Controller", func() {
 					Expect(MustSucceed(db.FS.Stat("2.domain")).Size()).To(Equal(int64(10)))
 					Expect(MustSucceed(db.FS.Stat("3.domain")).Size()).To(Equal(int64(4)))
 					Expect(MustSucceed(db.FS.Stat("4.domain")).Size()).To(Equal(int64(6)))
+
+					By("Asserting that the data is correct", func() {
+						var (
+							i   = db.NewIterator(domain.IteratorConfig{Bounds: telem.TimeRangeMax})
+							buf = make([]byte, 10)
+						)
+
+						Expect(i.SeekFirst(ctx)).To(BeTrue())
+						r := MustSucceed(i.NewReader(ctx))
+						_, err = r.ReadAt(buf, 0)
+						Expect(err).ToNot(HaveOccurred())
+						Expect(buf).To(Equal([]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}))
+
+						Expect(i.Next()).To(BeTrue())
+						r = MustSucceed(i.NewReader(ctx))
+						buf = make([]byte, 3)
+						_, err = r.ReadAt(buf, 0)
+						Expect(err).ToNot(HaveOccurred())
+						Expect(buf).To(Equal([]byte{11, 12, 13}))
+
+						Expect(i.Next()).To(BeTrue())
+						r = MustSucceed(i.NewReader(ctx))
+						buf = make([]byte, 3)
+						_, err = r.ReadAt(buf, 0)
+						Expect(err).ToNot(HaveOccurred())
+						Expect(buf).To(Equal([]byte{15, 16, 17}))
+
+						Expect(i.Next()).To(BeTrue())
+						r = MustSucceed(i.NewReader(ctx))
+						buf = make([]byte, 4)
+						_, err = r.ReadAt(buf, 0)
+						Expect(err).ToNot(HaveOccurred())
+						Expect(buf).To(Equal([]byte{20, 21, 22, 23}))
+
+						Expect(i.Next()).To(BeTrue())
+						r = MustSucceed(i.NewReader(ctx))
+						buf = make([]byte, 4)
+						_, err = r.ReadAt(buf, 0)
+						Expect(err).ToNot(HaveOccurred())
+						Expect(buf).To(Equal([]byte{30, 31, 32, 33}))
+
+						Expect(i.Next()).To(BeTrue())
+						r = MustSucceed(i.NewReader(ctx))
+						buf = make([]byte, 6)
+						_, err = r.ReadAt(buf, 0)
+						Expect(err).ToNot(HaveOccurred())
+						Expect(buf).To(Equal([]byte{40, 41, 42, 43, 44, 45}))
+
+						Expect(i.Close()).To(Succeed())
+					})
 				})
 			})
 		})
