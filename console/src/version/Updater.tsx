85bd550d48caa7390c5cc08fdfdc94dfa93b605b
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-05-20 17:46:43 -0400

    [console] - fixed build issues with windows

--- console/src/version/Updater.tsx
+++ console/src/version/Updater.tsx
@@ -1,18 +1,42 @@
 import { check } from "@tauri-apps/plugin-updater";
 import { relaunch } from "@tauri-apps/plugin-process";
 import { useEffect } from "react";
-import { TimeSpan } from "@synnaxlabs/x";
+import { TimeSpan, TimeStamp } from "@synnaxlabs/x";
 import { Status } from "@synnaxlabs/pluto";
+import { NotificationAdapter } from "@/notifications/Notifications";
 
-// export const useCheckForUpdates = () => {
-//   const addStatus = Status.useAggregator();
-//   useEffect(() => {
-//     setInterval(async () => {
-//       const update = await check();
-//       addStatus({
-//         key: "update",
-//         description: "update",
-//       });
-//     }, TimeSpan.seconds(5).milliseconds);
-//   }, []);
-// };
+export const useCheckForUpdates = () => {
+  const addStatus = Status.useAggregator();
+  useEffect(() => {
+    setInterval(async () => {
+      const update = await check();
+      if (update?.available !== true) return;
+      addStatus({
+        key: "update",
+        variant: "info",
+        message: `Update available: ${update.version}`,
+        description: "update",
+      });
+    }, TimeSpan.seconds(5).milliseconds);
+  }, []);
+};
+
+export const notificationAdapter: NotificationAdapter = (status) => {
+  if (!status.key.startsWith("update")) return null;
+  return {
+    ...status,
+    actions: [
+      {
+        label: "Update",
+        onClick: () => {
+          void (async () => {
+            const update = await check();
+            if (update?.available !== true) return;
+            await update.downloadAndInstall();
+            await relaunch();
+          })();
+        },
+      },
+    ],
+  };
+};
