3e7348f675e3b31e63c70a38b2e548d6bd527b37
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-05-20 16:41:29 -0400

    [console] - updated CI workflows

--- console/src-tauri/src/main.rs
+++ console/src-tauri/src/main.rs
@@ -2,18 +2,29 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 #[cfg(target_os = "macos")]
+#[macro_use]
 extern crate cocoa;
 
 #[cfg(target_os = "macos")]
+#[macro_use]
 extern crate objc;
+
+use objc::{msg_send, sel, sel_impl};
+use rand::{distributions::Alphanumeric, Rng};
+use tauri::{
+    plugin::{Builder, TauriPlugin},
+    Manager, Runtime, Window,
+}; // 0.8
 
 struct UnsafeWindowHandle(*mut std::ffi::c_void);
 unsafe impl Send for UnsafeWindowHandle {}
 unsafe impl Sync for UnsafeWindowHandle {}
 
 #[cfg(target_os = "macos")]
-fn position_traffic_lights(ns_window_handle: UnsafeWindowHandle, transparent: bool) {
+fn position_traffic_lights(win: Window, transparent: bool) {
+    let ns_window_handle = UnsafeWindowHandle(win.ns_window().expect("Failed to create window handle"));
     use cocoa::appkit::{NSView, NSWindow, NSWindowButton, NSWindowStyleMask, NSWindowTitleVisibility};
+    use cocoa::foundation::NSRect;
     let id = ns_window_handle.0 as cocoa::base::id;
     unsafe {
         let mut style_mask = id.styleMask();
@@ -22,14 +33,12 @@ fn position_traffic_lights(ns_window_handle: UnsafeWindowHandle, transparent: bo
             transparent,
         );
         id.setStyleMask_(style_mask);
-
         id.setTitleVisibility_(if transparent {
             NSWindowTitleVisibility::NSWindowTitleHidden
         } else {
             NSWindowTitleVisibility::NSWindowTitleVisible
         });
         id.setTitlebarAppearsTransparent_(cocoa::base::YES);
-
         let close = id.standardWindowButton_(NSWindowButton::NSWindowCloseButton);
         let miniaturize =
             id.standardWindowButton_(NSWindowButton::NSWindowMiniaturizeButton);
@@ -39,18 +48,6 @@ fn position_traffic_lights(ns_window_handle: UnsafeWindowHandle, transparent: bo
             button.removeFromSuperview()
         }
     }
-}
-
-
-
-    
-
-
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
@@ -65,36 +62,21 @@ fn main() {
         })
         .on_window_event(move |win, event| match event {
             tauri::WindowEvent::Focused {..} => {
-                position_traffic_lights(
-                    UnsafeWindowHandle(win.ns_window().expect("Failed to create window handle")),
-                    true,
-                );
+                position_traffic_lights(win, true);
             },
             tauri::WindowEvent::ThemeChanged {..} => {
-                position_traffic_lights(
-                    UnsafeWindowHandle(win.ns_window().expect("Failed to create window handle")),
-                    true,
-                );
+                position_traffic_lights(win, true);
             }
             tauri::WindowEvent::Resized(size) => {
                 let monitor = win.current_monitor().unwrap().unwrap();
                 let screen = monitor.size();
                 if size != screen {
-                    position_traffic_lights(
-                        UnsafeWindowHandle(win.ns_window().expect("Failed to create window handle")),
-                        true,
-                    );
+                    position_traffic_lights(win, true);
                 } 
-
-                
-                
             },
             tauri::WindowEvent::Moved(position)=> {
                 if position.x != 0 && position.y != 0 {
-                    position_traffic_lights(
-                        UnsafeWindowHandle(win.ns_window().expect("Failed to create window handle")),
-                        true,
-                    );
+                    position_traffic_lights(win, true);
                 }
            },
             _ => (),
@@ -102,7 +84,6 @@ fn main() {
         .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_store::Builder::default().build())
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
