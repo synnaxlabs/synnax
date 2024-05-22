eb95a7af0984d228c632f42bb88150402793e980
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-05-22 10:34:20 -0400

    [console] - fixed issues with workspaces

--- console/src/version/Updater.tsx
+++ console/src/version/Updater.tsx
@@ -7,17 +7,17 @@ import { NotificationAdapter } from "@/notifications/Notifications";
 
 export const useCheckForUpdates = () => {
   const addStatus = Status.useAggregator();
-  // useEffect(() => {
-  //   const i = setInterval(async () => {
-  //     const update = await check();
-  //     addStatus({
-  //       key: "update",
-  //       variant: "info",
-  //       message: `Update available: ${update != null}`,
-  //     });
-  //   }, TimeSpan.seconds(5).milliseconds);
-  //   return () => clearInterval(i);
-  // }, []);
+  useEffect(() => {
+    const i = setInterval(async () => {
+      const update = await check();
+      addStatus({
+        key: "update",
+        variant: "info",
+        message: `Update available: ${update != null}`,
+      });
+    }, TimeSpan.seconds(5).milliseconds);
+    return () => clearInterval(i);
+  }, []);
 };
 
 export const notificationAdapter: NotificationAdapter = (status) => {
