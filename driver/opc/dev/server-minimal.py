895600b95320e9bee04625b4f1ed82e7daa12eeb
Elham Islam <elham@synnaxlabs.com>
2024-05-24 09:51:16 -0400

    added opc development files into driver/opc/dev

---
+++ driver/opc/dev/server-minimal.py
@@ -0,0 +1,48 @@
+import asyncio
+import logging
+
+from asyncua import Server, ua
+from asyncua.common.methods import uamethod
+
+
+@uamethod
+def func(parent, value):
+    return value * 2
+
+
+async def main():
+    _logger = logging.getLogger(__name__)
+    # setup our server
+    server = Server()
+    await server.init()
+    server.set_endpoint("opc.tcp://localhost:4842/freeopcua/server/")
+
+    # set up our own namespace, not really necessary but should as spec
+    uri = "http://examples.freeopcua.github.io"
+    idx = await server.register_namespace(uri)
+
+    # populating our address space
+    # server.nodes, contains links to very common nodes like objects and root
+    myobj = await server.nodes.objects.add_object(idx, "MyObject")
+    myvar = await myobj.add_variable(idx, "MyVariable", 6.7)
+    # Set MyVariable to be writable by clients
+    await myvar.set_writable()
+    await server.nodes.objects.add_method(
+        ua.NodeId("ServerMethod", idx),
+        ua.QualifiedName("ServerMethod", idx),
+        func,
+        [ua.VariantType.Int64],
+        [ua.VariantType.Int64],
+    )
+    _logger.info("Starting server!")
+    async with server:
+        while True:
+            await asyncio.sleep(1)
+            new_val = await myvar.get_value() + 0.1
+            _logger.info("Set value of %s to %.1f", myvar, new_val)
+            await myvar.write_value(new_val)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.FATAL)
+    asyncio.run(main(), debug=True)
