6e68499c7e3f26152d1563cac919b5b9bc1e7aec
Elham Islam <elham@synnaxlabs.com>
2024-05-29 09:25:46 -0400

    checkpoint

---
+++ driver/vendor/mbedtls/build.sh
@@ -0,0 +1,9 @@
+curl -LO https://github.com/Mbed-TLS/mbedtls/releases/download/v3.6.0/mbedtls-3.6.0.tar.bz2
+tar -xjf mbedtls-3.6.0.tar.bz2
+mv mbedtls-3.6.0 mbedtls
+cd mbedtls
+./scripts/make_generated_files.bat
+cd ..
+cmake -G "Visual Studio 17 2022" -S mbedtls -B mbedtls-build
+cmake --build mbedtls-build --config Release
+cmake --install mbedtls-build --config Release
