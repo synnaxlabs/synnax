07421c834344c65f5deecb3a307086407e68e7ef
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-06-01 20:50:26 -0400

    [pluto] - removed unused deps

--- pluto/src/list/useSelect.spec.tsx
+++ pluto/src/list/useSelect.spec.tsx
@@ -7,13 +7,11 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-import { act, fireEvent, renderHook } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { act, renderHook } from "@testing-library/react";
 import { useState } from "react";
 import { describe, expect, it } from "vitest";
 
 import { useSelect, type UseSelectMultipleProps } from "@/list/useSelect";
-import { Triggers } from "@/triggers";
 
 interface Entry {
   key: string;
