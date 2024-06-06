39c95f0c9673373d5a31d2445c80845fccbf1814
pjdotson <123601024+pjdotson@users.noreply.github.com>
2024-06-06 13:01:31 -0400

    Pjdotson/sy 696 console deep linking: Workspaces, schematics, line plots, ranges (#596)
    * Improvements to deep linking being done asynchronously
    
    * Working more on an asynch function
    
    * Went from promises to async await in link handlers
    
    * Fixed module names
    
    * Got deep linking working
    
    * Uncommented schematic, range, and line plot link handler
    
    * [console] - fixed minor linting issues
    
    ---------
    
    Co-authored-by: Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>

--- console/src/link/link.tsx
+++ console/src/link/link.tsx
@@ -7,85 +7,128 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-import { Synnax } from "@synnaxlabs/client";
-import { Synnax as PSynnax } from "@synnaxlabs/pluto";
-import { useEffect } from "react";
-import { onOpenUrl } from "@tauri-apps/plugin-deep-link";
 import { Dispatch, UnknownAction } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
+import { Synnax } from "@synnaxlabs/client";
+import { Drift } from "@synnaxlabs/drift";
+import { Icon } from "@synnaxlabs/media";
+import {
+  Menu,
+  Status,
+  Synnax as PSynnax,
+  useAsyncEffect,
+  useSyncedRef,
+} from "@synnaxlabs/pluto";
+import { onOpenUrl } from "@tauri-apps/plugin-deep-link";
+import { ReactElement } from "react";
+import { useDispatch, useStore } from "react-redux";
 
 import { Cluster } from "@/cluster";
 import { Layout } from "@/layout";
-import { LinePlot } from "@/lineplot";
-import { Range } from "@/range";
-import { Schematic } from "@/schematic";
-import { Workspace } from "@/workspace";
 
 export interface HandlerProps {
-  // url is a string of two parts, the first part is the resource identifier and
-  // the second part is the resource key.
   resource: string;
   resourceKey: string;
   client: Synnax;
   dispatch: Dispatch<UnknownAction>;
   placer: Layout.Placer;
-  clusters: Cluster.Cluster[];
+  addStatus: (status: Status.CrudeSpec) => void;
 }
 
-export type Handler = (props: HandlerProps) => boolean;
+export type Handler = (props: HandlerProps) => Promise<boolean>;
 
 export interface UseDeepLinkProps {
   handlers: Handler[];
 }
 
-export const useDeepLink = ({ handlers }: UseDeepLinkProps): void => {
+export const useDeep = ({ handlers }: UseDeepLinkProps): void => {
   const client = PSynnax.use();
+  const clientRef = useSyncedRef(client);
+  const addStatus = Status.useAggregator();
   const dispatch = useDispatch();
   const placer = Layout.usePlacer();
-  const clusters = Cluster.useSelectMany();
+  const store = useStore();
+  const openUrlErrorMessage =
+    "Cannot open URL, URLs must be of the form synnax://cluster/<cluster-key> or synnax://cluster/<cluster-key>/<resource>/<resource-key>";
+  const addOpenUrlErrorStatus = () => {
+    addStatus({
+      variant: "error",
+      key: "openUrlError",
+      message: openUrlErrorMessage,
+    });
+  };
 
-  useEffect(() => {
-    onOpenUrl((urls) => {
-      if (client == null) {
-        console.error("Error: Cannot open URL, client is null");
-        return;
-      }
+  useAsyncEffect(async () => {
+    const unlisten = await onOpenUrl(async (urls) => {
+      dispatch(Drift.focusWindow({}));
+
+      // Processing URL, making sure is has valid form
       const scheme = "synnax://";
       if (urls.length === 0 || !urls[0].startsWith(scheme)) {
-        console.error("Error: Cannot open URL, URLs must start with synnax://");
+        addOpenUrlErrorStatus();
         return;
       }
       const urlParts = urls[0].slice(scheme.length).split("/");
-      if (
-        !Cluster.linkHandler({
-          resource: urlParts[0],
-          resourceKey: urlParts[1],
-          client,
-          dispatch,
-          placer,
-          clusters,
-        })
-      ) {
+      if (urlParts.length !== 2 && urlParts.length !== 4) {
+        addOpenUrlErrorStatus();
         return;
       }
-      handlers.find((h) =>
-        h({
-          resource: urlParts[2],
-          resourceKey: urlParts[3],
-          client,
-          dispatch,
-          placer,
-          clusters,
-        }),
-      );
+      if (urlParts[0] !== "cluster") {
+        addOpenUrlErrorStatus();
+        return;
+      }
+
+      // Connecting to the cluster
+      const clusterKey = urlParts[1];
+      const connParams = Cluster.select(
+        store.getState() as Cluster.StoreState,
+        clusterKey,
+      )?.props;
+      const addClusterErrorStatus = () => {
+        addStatus({
+          variant: "error",
+          key: "openUrlError-${clusterKey}",
+          message: `Cannot open URL, Cluster with key ${clusterKey} not found`,
+        });
+      };
+      if (connParams == null) {
+        addClusterErrorStatus();
+        return;
+      }
+      dispatch(Cluster.setActive(clusterKey));
+      clientRef.current = new Synnax(connParams);
+      if (clientRef.current == null) {
+        addClusterErrorStatus();
+        return;
+      }
+      if (urlParts.length === 2) return;
+
+      // Processing the resource part of URL
+      const resource = urlParts[2];
+      const resourceKey = urlParts[3];
+      for (const h of handlers)
+        if (
+          await h({
+            resource,
+            resourceKey,
+            client: clientRef.current,
+            dispatch,
+            placer,
+            addStatus,
+          })
+        )
+          return;
+      addStatus({
+        variant: "error",
+        key: "openUrlError-ResourceNotFound-",
+        message: `Cannot open link, ${resource} is unknown`,
+      });
     });
-  }, [client]);
+    return () => unlisten();
+  }, []);
 };
 
-export const HANDLERS: Handler[] = [
-  Cluster.linkHandler,
-  Schematic.linkHandler,
-  Range.linkHandler,
-  Workspace.linkHandler,
-  LinePlot.linkHandler,
-];
+export const CopyMenuItem = (): ReactElement => (
+  <Menu.Item itemKey="link" size="small" startIcon={<Icon.Link />}>
+    Copy link
+  </Menu.Item>
+);
