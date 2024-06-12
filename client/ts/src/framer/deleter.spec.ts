7e09b8dee75d2881821d8868d8c05767fa2ea884
Léo Liu <leoliuhy@gmail.com>
2024-06-11 20:35:00 -0400

    [client] - TS tests for delete failures. TS IS THE BANE OF MY EXISTENCE.

--- client/ts/src/framer/deleter.spec.ts
+++ client/ts/src/framer/deleter.spec.ts
@@ -7,13 +7,15 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-import { DataType, Rate, TimeRange, TimeStamp } from "@synnaxlabs/x/telem";
+import {DataType, Rate, TimeRange, TimeStamp } from "@synnaxlabs/x/telem";
 import { describe, expect, test } from "vitest";
 import { NotFoundError } from "@/errors"
 
 import { type channel } from "@/channel";
 import { newClient } from "@/setupspecs";
 import { randomSeries } from "@/util/telem";
+import { Frame } from "@/framer/frame";
+import {bigint} from "zod";
 
 const client = newClient();
 
@@ -25,53 +27,96 @@ const newChannel = async (): Promise<channel.Channel> =>
     dataType: DataType.FLOAT64,
   });
 
+const newIndexDataChannelPair = async (): Promise<channel.Channel[]> => {
+  const ind = await client.channels.create({
+    name: "index",
+    leaseholder: 1,
+    isIndex: true,
+    dataType: DataType.TIMESTAMP,
+  })
+  const data = await client.channels.create({
+    name: "data",
+    leaseholder: 1,
+    index: ind.key,
+    dataType: DataType.INT64,
+  })
+
+  return [ind, data]
+}
+
 describe("Deleter", () => {
-    test("Client - basic delete", async () => {
-      const ch = await newChannel();
-      const data = randomSeries(10, ch.dataType);
-      await client.write(TimeStamp.seconds(0), ch.key, data);
+  test("Client - basic delete", async () => {
+    const ch = await newChannel();
+    const data = randomSeries(10, ch.dataType);
+    await client.write(TimeStamp.seconds(0), ch.key, data);
 
-      await client.delete(ch.key, TimeStamp.seconds(2).range(TimeStamp.seconds(5)))
+    await client.delete(ch.key, TimeStamp.seconds(2).range(TimeStamp.seconds(5)))
 
-      const res = await client.read(TimeRange.MAX, ch.key);
-      expect(res.length).toEqual(data.length - 3);
-      expect(res.data.slice(0, 2)).toEqual(data.slice(0, 2))
-      expect(res.data.slice(2)).toEqual(data.slice(5))
+    const res = await client.read(TimeRange.MAX, ch.key);
+    expect(res.length).toEqual(data.length - 3);
+    expect(res.data.slice(0, 2)).toEqual(data.slice(0, 2))
+    expect(res.data.slice(2)).toEqual(data.slice(5))
+  });
+  test("Client - basic delete by name", async () => {
+    const ch = await newChannel()
+    const data = randomSeries(10, ch.dataType);
+    await client.write(TimeStamp.seconds(0), ch.key, data);
+
+    await client.delete(ch.name, TimeStamp.seconds(2).range(TimeStamp.seconds(5)))
+
+    const res = await client.read(TimeRange.MAX, ch.key);
+    expect(res.length).toEqual(data.length - 3);
+    expect(res.data.slice(0, 2)).toEqual(data.slice(0, 2))
+    expect(res.data.slice(2)).toEqual(data.slice(5))
+  })
+  test("Client - delete name not found", async () => {
+    const ch = await newChannel();
+    const data = randomSeries(10, ch.dataType);
+    await client.write(TimeStamp.seconds(0), ch.key, data);
+
+    await expect(
+      client.delete(["billy bob", ch.name], TimeRange.MAX)
+    ).rejects.toThrow(NotFoundError)
+
+    const res = await client.read(TimeRange.MAX, ch.key);
+    expect(res.data).toEqual(data);
+  })
+  test("Client - delete key not found", async () => {
+    const ch = await newChannel();
+    const data = randomSeries(10, ch.dataType);
+    await client.write(TimeStamp.seconds(0), ch.key, data);
+
+    await expect(
+      client.delete([ch.key, 1232], TimeRange.MAX)
+    ).rejects.toThrow(NotFoundError)
+
+    const res = await client.read(TimeRange.MAX, ch.key);
+    expect(res.data).toEqual(data);
+  })
+
+  test("Client - delete with writer", async () => {
+    const ch = await newChannel();
+
+    const writer = await client.openWriter({
+      start: TimeStamp.seconds(10),
+      channels: [ch.key],
     });
-    test("Client - basic delete by name", async () => {
-      const ch = await newChannel()
-      const data = randomSeries(10, ch.dataType);
-      await client.write(TimeStamp.seconds(0), ch.key, data);
 
-      await client.delete("test", TimeStamp.seconds(2).range(TimeStamp.seconds(5)))
+    await expect(
+      client.delete(
+      [ch.key], TimeStamp.seconds(12).range(TimeStamp.seconds(30)))
+    ).rejects.toThrow()
 
-      const res = await client.read(TimeRange.MAX, ch.key);
-      expect(res.length).toEqual(data.length - 3);
-      expect(res.data.slice(0, 2)).toEqual(data.slice(0, 2))
-      expect(res.data.slice(2)).toEqual(data.slice(5))
-    })
-    test("Client - delete name not found", async () => {
-      const ch = await newChannel();
-      const data = randomSeries(10, ch.dataType);
-      await client.write(TimeStamp.seconds(0), ch.key, data);
+    await writer.close()
+  })
 
-      await expect(
-        client.delete(["billy bob", ch.name], TimeRange.MAX)
-      ).rejects.toThrow(NotFoundError)
-
-      const res = await client.read(TimeRange.MAX, ch.key);
-      expect(res.data).toEqual(data);
-    })
-    test("Client - delete key not found", async () => {
-      const ch = await newChannel();
-      const data = randomSeries(10, ch.dataType);
-      await client.write(TimeStamp.seconds(0), ch.key, data);
-
-      await expect(
-        client.delete([ch.key, 1232], TimeRange.MAX)
-      ).rejects.toThrow(NotFoundError)
-
-      const res = await client.read(TimeRange.MAX, ch.key);
-      expect(res.data).toEqual(data);
-    })
+  // test("Client - delete index channel alone", async () => {
+  //   const chs = await newIndexDataChannelPair()
+  //   const index = chs[0]
+  //   const dat = chs[1]
+  //   const data = randomSeries(10, dat.dataType)
+  //
+  //   await index.write(0, BigUint64Array([0, 1000000000, 2000000000, 3000000000]))
+  //
+  // })
 });
