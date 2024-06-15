7b45aa7408f9a99c95d378b06fab282dc2fb66c8
Elham Islam <eislam@umich.edu>
2024-06-15 14:00:42 -0400

    merge issues

--- driver/task/task.h
+++ driver/task/task.h
@@ -1,3 +1,4 @@
+
 // Copyright 2024 Synnax Labs, Inc.
 //
 // Use of this software is governed by the Business Source License included in the file
@@ -22,39 +23,39 @@ using json = nlohmann::json;
 
 namespace task {
 /// @brief A command that can be executed on a task in order to change its state.
-    struct Command {
-        /// @brief the key of the task to be commanded.
-        TaskKey task = 0;
-        /// @brief the type of the command to execute.
-        std::string type = "";
-        /// @brief an optional key to assign to the command. This is useful for tracking
-        /// state updates related to the command.
-        std::string key = "";
-        /// @brief json arguments to the command.
-        json args = {};
+struct Command {
+    /// @brief the key of the task to be commanded.
+    TaskKey task = 0;
+    /// @brief the type of the command to execute.
+    std::string type = "";
+    /// @brief an optional key to assign to the command. This is useful for tracking
+    /// state updates related to the command.
+    std::string key = "";
+    /// @brief json arguments to the command.
+    json args = {};
 
-        Command() = default;
+    Command() = default;
 
-        /// @brief constructs the command from the provided configuration parser.
-        explicit Command(
-                config::Parser parser
-        ) : task(parser.required<TaskKey>("task")),
-            type(parser.required<std::string>("type")),
-            key(parser.optional<std::string>("key", "")),
-            args(parser.optional<json>("args", json{})) {
-        }
+    /// @brief constructs the command from the provided configuration parser.
+    explicit Command(
+        config::Parser parser
+    ): task(parser.required<TaskKey>("task")),
+       type(parser.required<std::string>("type")),
+       key(parser.optional<std::string>("key", "")),
+       args(parser.optional<json>("args", json{})) {
+    }
 
-        /// @brief Construct a new Task Command object
-        Command(const TaskKey task, std::string type, json args)
-                : task(task), type(std::move(type)), args(std::move(args)) {
-        }
-    };
+    /// @brief Construct a new Task Command object
+    Command(const TaskKey task, std::string type, json args)
+        : task(task), type(std::move(type)), args(std::move(args)) {
+    }
+};
 
 /// @brief interface for a task that can be executed by the driver. Tasks should be
 /// constructed by an @see Factory.
-    class Task {
-    public:
-        synnax::TaskKey key = 0;
+class Task {
+public:
+    synnax::TaskKey key = 0;
 
     virtual std::string name() { return ""; }
 
@@ -63,28 +64,28 @@ namespace task {
     virtual void exec(Command &cmd) {
     }
 
-        /// @brief stops the task, halting activities and freeing all resources. stop
-        /// is called when the task is no longer needed, and is typically followed by a
-        /// a call to the destructor.
-        virtual void stop() = 0;
+    /// @brief stops the task, halting activities and freeing all resources. stop
+    /// is called when the task is no longer needed, and is typically followed by a
+    /// a call to the destructor.
+    virtual void stop() = 0;
 
-        virtual ~Task() = default;
-    };
+    virtual ~Task() = default;
+};
 
-    const std::string TASK_FAILED = "error";
+const std::string TASK_FAILED = "error";
 
 /// @brief struct that represents the network portable state of a task. Used both
 /// internally by a task and externally by the driver to track its state.
-    struct State {
-        /// @brief the key of the task.
-        TaskKey task = 0;
-        /// @brief an optional key to assign to the state update. This is particularly
-        /// useful for identifying responses to commands.
-        std::string key = "";
-        /// @brief the type of the task.
-        std::string variant = "";
-        /// @brief relevant details about the current state of the task.
-        json details = {};
+struct State {
+    /// @brief the key of the task.
+    TaskKey task = 0;
+    /// @brief an optional key to assign to the state update. This is particularly
+    /// useful for identifying responses to commands.
+    std::string key = "";
+    /// @brief the type of the task.
+    std::string variant = "";
+    /// @brief relevant details about the current state of the task.
+    json details = {};
 
     [[nodiscard]] json toJSON() const {
         json j;
@@ -97,50 +98,50 @@ namespace task {
 };
 
 /// @brief name of the channel used in Synnax to communicate state updates.
-    const std::string TASK_STATE_CHANNEL = "sy_task_state";
+const std::string TASK_STATE_CHANNEL = "sy_task_state";
 
 /// @brief an interface for a standard context that is provided to every task in the
 /// driver. This context provides access to the Synnax client and alllows tasks to
 /// easily update their state.
-    class Context {
-    public:
-        /// @brief the client used to communicate with the Synnax server.
-        std::shared_ptr<Synnax> client;
+class Context {
+public:
+    /// @brief the client used to communicate with the Synnax server.
+    std::shared_ptr<Synnax> client;
 
-        Context() = default;
+    Context() = default;
 
-        virtual ~Context() = default;
+    virtual ~Context() = default;
 
     explicit Context(std::shared_ptr<Synnax> client): client(std::move(client)) {
     }
 
-        /// @brief updates the state of the task in the Synnax cluster.
-        virtual void setState(const State &state) = 0;
-    };
+    /// @brief updates the state of the task in the Synnax cluster.
+    virtual void setState(const State &state) = 0;
+};
 
 /// @brief a mock context that can be used for testing tasks.
-    class MockContext final : public Context {
-    public:
-        std::vector<State> states{};
+class MockContext final : public Context {
+public:
+    std::vector<State> states{};
 
-        explicit MockContext(std::shared_ptr<Synnax> client) : Context(client) {
-        }
+    explicit MockContext(std::shared_ptr<Synnax> client): Context(client) {
+    }
 
 
-        void setState(const State &state) override {
-            state_mutex.lock();
-            states.push_back(state);
-            state_mutex.unlock();
-        }
+    void setState(const State &state) override {
+        state_mutex.lock();
+        states.push_back(state);
+        state_mutex.unlock();
+    }
 
-    private:
-        std::mutex state_mutex;
-    };
+private:
+    std::mutex state_mutex;
+};
 
-    class SynnaxContext final : public Context {
-    public:
-        explicit SynnaxContext(std::shared_ptr<Synnax> client) : Context(client) {
-        }
+class SynnaxContext final : public Context {
+public:
+    explicit SynnaxContext(std::shared_ptr<Synnax> client): Context(client) {
+    }
 
     void setState(const State &state) override {
         std::unique_lock lock(state_mutex);
@@ -164,7 +165,7 @@ namespace task {
                         message();
                 return;
             }
-            state_mutex.unlock();
+            state_updater = std::make_unique<Writer>(std::move(su));
         }
         auto s = Series(to_string(state.toJSON()), JSON);
         auto fr = Frame(chan.key, std::move(s));
@@ -211,22 +212,24 @@ public:
             auto new_tasks = factory->configureInitialTasks(ctx, rack);
             for (auto &task: new_tasks) tasks.emplace_back(std::move(task));
         }
+        return tasks;
+    }
 
-        std::pair<std::unique_ptr<Task>, bool>
-        configureTask(
-                const std::shared_ptr<Context> &ctx,
-                const synnax::Task &task
-        ) override {
-            for (const auto &factory: factories) {
-                auto [t, ok] = factory->configureTask(ctx, task);
-                if (ok) return {std::move(t), true};
-            }
-            return {nullptr, false};
+    std::pair<std::unique_ptr<Task>, bool>
+    configureTask(
+        const std::shared_ptr<Context> &ctx,
+        const synnax::Task &task
+    ) override {
+        for (const auto &factory: factories) {
+            auto [t, ok] = factory->configureTask(ctx, task);
+            if (ok) return {std::move(t), true};
         }
+        return {nullptr, false};
+    }
 
-    private:
-        std::vector<std::shared_ptr<Factory> > factories;
-    };
+private:
+    std::vector<std::shared_ptr<Factory> > factories;
+};
 
 /// @brief TaskManager is responsible for configuring, executing, and commanding data
 /// acqusition and control tasks.
@@ -239,38 +242,38 @@ public:
         const breaker::Config& breaker
     );
 
-        freighter::Error start(std::atomic<bool> &done);
+    freighter::Error start(std::atomic<bool> &done);
 
-        freighter::Error stop();
+    freighter::Error stop();
 
-    private:
-        RackKey rack_key;
-        Rack internal;
-        std::shared_ptr<task::Context> ctx;
-        std::unique_ptr<Streamer> streamer;
-        std::unique_ptr<task::Factory> factory;
-        std::unordered_map<std::uint64_t, std::unique_ptr<task::Task> > tasks{};
+private:
+    RackKey rack_key;
+    Rack internal;
+    std::shared_ptr<task::Context> ctx;
+    std::unique_ptr<Streamer> streamer;
+    std::unique_ptr<task::Factory> factory;
+    std::unordered_map<std::uint64_t, std::unique_ptr<task::Task> > tasks{};
 
-        Channel task_set_channel;
-        Channel task_delete_channel;
-        Channel task_cmd_channel;
-        Channel task_state_channel;
+    Channel task_set_channel;
+    Channel task_delete_channel;
+    Channel task_cmd_channel;
+    Channel task_state_channel;
 
-        breaker::Breaker breaker;
+    breaker::Breaker breaker;
 
-        std::thread run_thread;
-        freighter::Error run_err;
+    std::thread run_thread;
+    freighter::Error run_err;
 
-        void run(std::atomic<bool> &done);
+    void run(std::atomic<bool> &done);
 
-        freighter::Error runGuarded();
+    freighter::Error runGuarded();
 
-        freighter::Error startGuarded();
+    freighter::Error startGuarded();
 
-        void processTaskSet(const Series &series);
+    void processTaskSet(const Series &series);
 
-        void processTaskDelete(const Series &series);
+    void processTaskDelete(const Series &series);
 
-        void processTaskCmd(const Series &series);
-    };
+    void processTaskCmd(const Series &series);
+};
 }
