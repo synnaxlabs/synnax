d65803462edb1278dbf1899cc8f853252ac7c2ba
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-06-24 07:46:47 -0700

    [console] - removed unused log statement

--- console/src/persist/state.ts
+++ console/src/persist/state.ts
@@ -98,7 +98,6 @@ export const open = async <S extends RequiredState>({
   let state = (await db.get<S>(persistedStateKey(version))) ?? undefined;
   if (state != null && migrator != null) {
     try {
-      console.log("applying migrations");
       state = migrator(state);
     } catch (e) {
       console.error("unable to apply migrations. continuing with undefined state.");
