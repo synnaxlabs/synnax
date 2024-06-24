0f5622050e4eae9856a9d4def9737977edd08294
Elham Islam <elham@synnaxlabs.com>
2024-06-24 15:58:21 -0700

    [driver] remove unncessary test helpers

--- driver/testutil/testutil.cpp
+++ driver/testutil/testutil.cpp
@@ -28,29 +28,6 @@ std::mt19937 random_generator(std::string suite_name) {
 }
 
 
-json add_index_channel_JSON(
-    json &config,
-    std::string name,
-    int key
-) {
-    json channel;
-    channel["name"] = name;
-    channel["cmd_key"] = 0;
-    channel["ack_key"] = 0;
-    channel["channel_type"] = "index";
-    channel["port"] = 0;
-    channel["line"] = 0;
-    channel["channel_key"] = key;
-
-    // now add json to the channels vector
-    // check if the channels array exists
-    if (config.find("channels") == config.end()) {
-        config["channels"] = json::array();
-    }
-    config["channels"].push_back(channel);
-    return channel;
-}
-
 
 json add_DI_channel_JSON(
     json &config,
@@ -65,7 +42,7 @@ json add_DI_channel_JSON(
     channel["channel_type"] = "digitalInput";
     channel["port"] = port;
     channel["line"] = line;
-    channel["channel_key"] = key;
+    channel["channel"] = key;
 
     // now add json to the channels vector
     // check if the channels array exists
@@ -126,27 +103,6 @@ json add_DO_channel_JSON(
     channel["channel_type"] = "digitalOutput";
     channel["port"] = port;
     channel["line"] = line;
-
-    // now add json to the channels vector
-    // check if the channels array exists
-    if (config.find("channels") == config.end()) {
-        config["channels"] = json::array();
-    }
-    config["channels"].push_back(channel);
-    return channel;
-}
-
-json add_drive_state_index_channel_JSON(
-    json &config,
-    std::string name,
-    int key
-) {
-    json channel;
-    channel["name"] = name;
-    channel["channel_key"] = key;
-    channel["channel_type"] = "driveStateIndex";
-    channel["port"] = 0;
-    channel["line"] = 0;
 
     // now add json to the channels vector
     // check if the channels array exists
