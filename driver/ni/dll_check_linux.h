cd32da9a0732f6a1116697a7997d4689f881db6f
Elham Islam <eislam@umich.edu>
2024-06-27 13:36:51 -0700

    [driver] typo

--- driver/ni/dll_check_linux.h
+++ driver/ni/dll_check_linux.h
@@ -7,7 +7,7 @@ bool does_dll_exist(const char* dll_path) {
 }
 
 void log_dll_error(const std::shared_ptr<task::Context> &ctx, const synnax::Task &task) {
-    LOG(ERROR) << "[ni] NI acquisitoion and control not supported on Linux or MacOS" << std::endl;
+    LOG(ERROR) << "[ni] NI acquisition and control not supported on Linux or MacOS" << std::endl;
     json j = {
         {"error", " NI acquisition and control not supported on Linux or MacOS"}
     };
