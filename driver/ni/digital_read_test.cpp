2dfc5f991865ea9421a75784b59e6397caf0e11f
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-07-02 08:22:40 -0700

    [driver] - updated copyright headers

--- driver/ni/digital_read_test.cpp
+++ driver/ni/digital_read_test.cpp
@@ -6,8 +6,6 @@
 // As of the Change Date specified in that file, in accordance with the Business Source
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
-//
-// Created by Synnax on 1/29/2024.
 
 #include <map>
 #include <stdio.h>
