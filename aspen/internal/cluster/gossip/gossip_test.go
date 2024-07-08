ba0b88890148689fba518058eb05f4222b395e9d
Léo Liu <leoliuhy@gmail.com>
2024-07-08 12:55:26 -0400

    [aspen] - fixed test case that DPanics

--- aspen/internal/cluster/gossip/gossip_test.go
+++ aspen/internal/cluster/gossip/gossip_test.go
@@ -89,9 +89,10 @@ var _ = Describe("OperationSender", func() {
 				g.Expect(sOne.CopyState().Nodes[2].Heartbeat.Version).To(Equal(uint32(0)))
 			}).Should(Succeed())
 		})
-		It("Should return an error when an invalid message is received", func() {
-			_, err := net.UnaryClient().Send(context.Background(), t2.Address, gossip.Message{})
-			Expect(err).To(HaveOccurred())
+		It("Should DPanic when an invalid message is received", func() {
+			Expect(func() {
+				net.UnaryClient().Send(context.Background(), t2.Address, gossip.Message{})
+			}).To(Panic())
 		})
 	})
 })
