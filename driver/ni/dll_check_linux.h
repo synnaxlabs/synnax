6d557c585a9462ec70cbc871de281fcd4775d444
Elham Islam <eislam@umich.edu>
2024-07-17 13:49:05 -0700

    [driver] fix duplicated symbols build error

--- driver/ni/dll_check_linux.h
+++ driver/ni/dll_check_linux.h
@@ -11,11 +11,11 @@
 #include <iostream>
 #include "glog/logging.h"
 
-bool does_dll_exist(const char *dll_path) {
+inline bool does_dll_exist(const char *dll_path) {
     return false;
 }
 
-void log_dll_error(const std::shared_ptr<task::Context> &ctx,
+inline void log_dll_error(const std::shared_ptr<task::Context> &ctx,
                    const synnax::Task &task) {
     LOG(ERROR) << "[ni] NI acquisition and control not supported on Linux or MacOS" <<
             std::endl;
