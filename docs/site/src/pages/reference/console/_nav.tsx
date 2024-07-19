2f6d0b3f477392d12b84c0ec493188d14bd62b90
pjdotson <patrick@synnaxlabs.com>
2024-07-19 14:59:46 -0700

    Refactored half of the console pages

--- docs/site/src/pages/reference/console/_nav.tsx
+++ docs/site/src/pages/reference/console/_nav.tsx
@@ -1,11 +1,11 @@
 // Copyright 2024 Synnax Labs, Inc.
 //
-// Use of this software is governed by the Business Source License included in the file
-// licenses/BSL.txt.
+// Use of this software is governed by the Business Source License included in
+// the file licenses/BSL.txt.
 //
-// As of the Change Date specified in that file, in accordance with the Business Source
-// License, use of this software will be governed by the Apache License, Version 2.0,
-// included in the file licenses/APL.txt.
+// As of the Change Date specified in that file, in accordance with the Business
+// Source License, use of this software will be governed by the Apache License,
+// Version 2.0, included in the file licenses/APL.txt.
 
 import { type PageNavNode } from "@/components/PageNav/PageNav";
 
@@ -41,12 +41,12 @@ export const consoleNav: PageNavNode = {
     {
       key: "/reference/console/line-plot",
       href: "/reference/console/line-plot",
-      name: "Line Plot Visualization",
+      name: "Line Plots",
     },
     {
       key: "/reference/console/schematic",
       href: "/reference/console/schematic",
-      name: "Schematic Visualization",
+      name: "Schematics",
     },
   ],
 };
