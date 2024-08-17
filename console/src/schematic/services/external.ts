cc3b9adb841642d27c3c1f2a9d01c20e8d5ab63c
pjdotson <patrick@synnaxlabs.com>
2024-08-16 17:14:16 -0700

    refactor(console): add icons for download/import schematic

--- console/src/schematic/services/external.ts
+++ console/src/schematic/services/external.ts
@@ -1,4 +1,14 @@
+// Copyright 2024 Synnax Labs, Inc.
+//
+// Use of this software is governed by the Business Source License included in
+// the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with the Business
+// Source License, use of this software will be governed by the Apache License,
+// Version 2.0, included in the file licenses/APL.txt.
+
 export * from "@/schematic/services/file";
+export * from "@/schematic/services/Icons";
 export * from "@/schematic/services/link";
 export * from "@/schematic/services/ontology";
 export * from "@/schematic/services/palette";
