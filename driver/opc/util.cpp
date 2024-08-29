52eda344847e8b40d3bb21807d5b036e2efa04cf
Elham Islam <eislam@umich.edu>
2024-08-29 07:31:53 -0700

    [driver] remove set auth

--- driver/opc/util.cpp
+++ driver/opc/util.cpp
@@ -269,27 +269,6 @@ void fetchEndpointDiagnosticInfo(
     }
 }
 
-//freighter::Error set_authentication(
-//        opc::ConnectionConfig &cfg,
-//        UA_ClientConfig *config
-//){
-//    UA_StatusCode status;
-//    if (!cfg.username.empty() || !cfg.password.empty()) {
-//        status = UA_ClientConfig_setAuthenticationUsername(
-//                config,
-//                cfg.username.c_str(),
-//                cfg.password.c_str()
-//        );
-//        if (status != UA_STATUSCODE_GOOD) {
-//            LOG(ERROR) << "[opc.scanner] Failed to set authentication: " << UA_StatusCode_name(status);
-//            return freighter::Error(freighter::TYPE_UNREACHABLE,
-//                                     "Failed to set authentication: " + std::string(
-//                                             UA_StatusCode_name(status)));
-//
-//        }
-//    }
-//    return freighter::NIL;
-//}
 
 ///@ connect returns a new UA_Client object which is connected to the specified endpoint
 std::pair<std::shared_ptr<UA_Client>, freighter::Error> opc::connect(
