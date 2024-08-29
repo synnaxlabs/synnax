d210f2dedf09b8c05da77ddd9fe419c1d5a3a188
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-08-29 16:23:19 -0700

    [client/py] - documented various internal methods

--- client/py/synnax/util/send_required.py
+++ client/py/synnax/util/send_required.py
@@ -9,19 +9,20 @@
 
 
 from typing import Type
-from freighter import UnaryClient, URL
+from freighter import UnaryClient, URL, send_required as fsend_required
 from freighter.exceptions import Unreachable
 from freighter.transport import RQ, RS, Transport
 
 
 def send_required(client: UnaryClient, target: str, req: RQ, res_t: Type[RS]) -> RS:
-    res, exc = client.send(target, req, res_t)
-    if exc is not None:
-        if isinstance(exc, Unreachable):
-            url = URL.parse(exc.target)
-            raise Unreachable(
-                target=url.stringify(),
-                message=f"Failed to reach cluster at {url.host}:{url.port}",
-            )
-        raise exc
-    return res
+    """Sends a request to a target and returns the response. Raises an exception if the
+    request returns an error.
+    """
+    try:
+        return fsend_required(client, target, req, res_t)
+    except Unreachable as exc:
+        url = URL.parse(exc.target)
+        raise Unreachable(
+            target=url.stringify(),
+            message=f"Failed to reach cluster at {url.host}:{url.port}",
+        ) from exc
--- freighter/py/freighter/unary.py
+++ freighter/py/freighter/unary.py
@@ -38,6 +38,8 @@ class UnaryClient(Transport, Protocol):
 
 
 def send_required(client: UnaryClient, target: str, req: RQ, res_t: Type[RS]) -> RS:
+    """Utility wrapper that throws an exception if the request returns an error.
+    """
     res, exc = client.send(target, req, res_t)
     if exc is not None:
         raise exc
