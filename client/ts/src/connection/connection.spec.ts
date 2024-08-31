c830863df5019ce254774a87e9232d083e583a9f
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-08-31 09:48:26 -0700

    [client/ts] - added version compatibility checks to client

--- client/ts/src/connection/connection.spec.ts
+++ client/ts/src/connection/connection.spec.ts
@@ -29,4 +29,30 @@ describe("connectivity", () => {
     expect(state.status).toEqual("connected");
     expect(z.string().uuid().safeParse(state.clusterKey).success).toBe(true);
   });
+  describe("version compatibility", () => {
+    it("should pull the server and client versions", async () => {
+      const transport = new Transport(new URL({ host: HOST, port: PORT }));
+      const client = new auth.Client(transport.unary, {
+        username: "synnax",
+        password: "seldon",
+      });
+      transport.use(client.middleware());
+      const connectivity = new Checker(transport.unary, undefined, __VERSION__);
+      const state = await connectivity.check();
+      expect(state.clientServerCompatible).toBe(true);
+      expect(state.clientVersion).toBe(__VERSION__);
+    });
+    it("should adjust state if the server is too old", async () => {
+      const transport = new Transport(new URL({ host: HOST, port: PORT }));
+      const client = new auth.Client(transport.unary, {
+        username: "synnax",
+        password: "seldon",
+      });
+      transport.use(client.middleware());
+      const connectivity = new Checker(transport.unary, undefined, "50000.0.0");
+      const state = await connectivity.check();
+      expect(state.clientServerCompatible).toBe(false);
+      expect(state.clientVersion).toBe("50000.0.0");
+    });
+  });
 });
