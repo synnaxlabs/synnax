614db1a5c0a8359eb57e7232b62590729a82a226
Elham Islam <eislam@umich.edu>
2024-09-06 11:39:15 -0700

    remove redundant steps

--- driver/vendor/open62541/build_windows.sh
+++ driver/vendor/open62541/build_windows.sh
@@ -11,10 +11,6 @@
 
 rm -r ./open62541/out
 rm -r ./open62541/build
-
-# Create build directory
-rm -r ./open62541/out
-rm -r ./open62541/build
 mkdir open62541/build && cd open62541/build
 MBEDTLS_DIR="C:\Program Files (x86)\Mbed TLS"
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUA_NAMESPACE_ZERO=FULL -DCMAKE_INSTALL_PREFIX=../out -DUA_ENABLE_ENCRYPTION=MBEDTLS -DMBEDTLS_LIBRARY="C:\Program Files (x86)\Mbed TLS\lib\mbedtls.lib" -DMBEDX509_LIBRARY="C:\Program Files (x86)\Mbed TLS\lib\mbedx509.lib" -DMBEDCRYPTO_LIBRARY="C:\Program Files (x86)\Mbed TLS\lib\mbedcrypto.lib" -DMBEDTLS_INCLUDE_DIRS="C:\Program Files (x86)\Mbed TLS\include" -DCMAKE_OSX_ARCHITECTURES=x86_64 ..
