d937b96a3a703cefa15bde56ecacd1085ec1152c
Elham Islam <eislam@umich.edu>
2024-09-06 11:38:43 -0700

    [driver] see what causes macos build to fail

--- driver/vendor/open62541/build_windows.sh
+++ driver/vendor/open62541/build_windows.sh
@@ -13,19 +13,9 @@ rm -r ./open62541/out
 rm -r ./open62541/build
 
 # Create build directory
-mkdir -p open62541/build && cd open62541/build
-
-# Define MBEDTLS_DIR and run cmake
-MBEDTLS_DIR="C:\\Program Files (x86)\\Mbed TLS"
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-      -DUA_NAMESPACE_ZERO=FULL \
-      -DCMAKE_INSTALL_PREFIX="../out" \
-      -DUA_ENABLE_ENCRYPTION=MBEDTLS \
-      -DMBEDTLS_LIBRARY="$MBEDTLS_DIR\\lib\\mbedtls.lib" \
-      -DMBEDX509_LIBRARY="$MBEDTLS_DIR\\lib\\mbedx509.lib" \
-      -DMBEDCRYPTO_LIBRARY="$MBEDTLS_DIR\\lib\\mbedcrypto.lib" \
-      -DMBEDTLS_INCLUDE_DIRS="$MBEDTLS_DIR\\include" \
-      -DCMAKE_OSX_ARCHITECTURES=x86_64 ..
-
-# Build and install with verbose output
-cmake --build . --config RelWithDebInfo --target install --verbose
+rm -r ./open62541/out
+rm -r ./open62541/build
+mkdir open62541/build && cd open62541/build
+MBEDTLS_DIR="C:\Program Files (x86)\Mbed TLS"
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUA_NAMESPACE_ZERO=FULL -DCMAKE_INSTALL_PREFIX=../out -DUA_ENABLE_ENCRYPTION=MBEDTLS -DMBEDTLS_LIBRARY="C:\Program Files (x86)\Mbed TLS\lib\mbedtls.lib" -DMBEDX509_LIBRARY="C:\Program Files (x86)\Mbed TLS\lib\mbedx509.lib" -DMBEDCRYPTO_LIBRARY="C:\Program Files (x86)\Mbed TLS\lib\mbedcrypto.lib" -DMBEDTLS_INCLUDE_DIRS="C:\Program Files (x86)\Mbed TLS\include" -DCMAKE_OSX_ARCHITECTURES=x86_64 ..
+cmake --build . --config RelWithDebInfo --target install
