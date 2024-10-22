c276e39b2ee4256e9da0aa01c034dcdb762b125c
Elham Islam <elham@synnaxlabs.com>
2024-10-22 13:27:14 -0700

    [driver/labjack] test simultaenous read and write

--- driver/labjack/reader_test.cpp
+++ driver/labjack/reader_test.cpp
@@ -12,6 +12,7 @@
 #include "client/cpp/synnax.h"
 #include "driver/labjack/task.h"
 #include "driver/labjack/reader.h"
+#include "driver/labjack/writer.h"
 #include "driver/labjack/scanner.h"
 #include "driver/testutil/testutil.h"
 
@@ -24,7 +25,6 @@ using json = nlohmann::json;
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                                   Basic Tests                                                //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 TEST(read_tests, labjack_t4){
     auto client = std::make_shared<synnax::Synnax>(new_test_client());
 
@@ -249,4 +249,95 @@ TEST(read_tests, labjack_scan_and_read){
         reader_task->exec(start_cmd);
         reader_task->exec(stop_cmd);
     }
+}
+
+TEST(read_tests, labjack_t4_read_and_write){
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////// READ TASK
+    auto client = std::make_shared<synnax::Synnax>(new_test_client());
+
+    auto [time, tErr] = client->channels.create("idx", synnax::TIMESTAMP, 0, true);
+    ASSERT_FALSE(tErr) << tErr.message();
+
+    auto [data, dErr] = client->channels.create("ai", synnax::FLOAT32, time.key, false);
+    ASSERT_FALSE(dErr) << dErr.message();
+
+    auto config = json{
+            {"sample_rate", 1000},
+            {"stream_rate", 30},
+            {"device_type", "T4"},
+            {"device_key", "440022190"},
+            {"serial_number", "440022190"},
+            {"connection_type", "USB"},
+            {"data_saving", true},
+            {"channels", json::array({
+                                             {
+                                                     {"location", "AIN0"},
+                                                     {"enabled", true},
+                                                     {"data_type", "float32"},
+                                                     {"channel_key", data.key},
+                                                     {"range", 10.0},
+                                                     {"channel_types", "AIN"}
+                                             }
+                                     })},
+            {"index_keys", json::array({time.key})},
+            {"channel_map", {
+                                    {"AIN0", data.key}
+                            }}
+    };
+
+    auto task = synnax::Task("my_task", "labjack_read", to_string(config));
+    auto mockCtx = std::make_shared<task::MockContext>(client);
+
+    auto reader_task = labjack::ReaderTask::configure(mockCtx, task);
+    // create commands
+    auto start_cmd = task::Command{task.key, "start", {}};
+    auto stop_cmd = task::Command{task.key, "stop", {}};
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////// WRITE TASK
+    auto [state_idx, tErr1] = client->channels.create("do_state_idx", synnax::TIMESTAMP, 0, true);
+    ASSERT_FALSE(tErr1) << tErr1.message();
+
+    auto [cmd_idx, tErr2] = client->channels.create("do_cmd_idx", synnax::TIMESTAMP,0, true);
+    ASSERT_FALSE(tErr2) << tErr2.message();
+
+    auto [state, aErr] = client->channels.create("do_state", synnax::SY_UINT8, state_idx.key, false);
+    ASSERT_FALSE(aErr) << aErr.message();
+
+    auto [cmd, cErr] = client->channels.create("do_cmd", synnax::SY_UINT8, cmd_idx.key, false);
+    ASSERT_FALSE(cErr) << cErr.message();
+
+    auto writer_config = json{
+        {"device_type", "T4"},
+        {"device_key", "440022190"},
+        {"serial_number", "440022190"},
+        {"connection_type", "USB"},
+        {"channels", json::array({
+                                         {
+                                                 {"location", "FIO4"},
+                                                 {"enabled", true},
+                                                 {"data_type", "uint8"},
+                                                 {"cmd_key", cmd.key},
+                                                 {"state_key", state.key},
+                                                 {"channel_types", "DIO"}
+                                         }
+                                 })},
+        {"data_saving", true},
+        {"state_rate", 10}
+    };
+
+    auto sy_task = synnax::Task("my_task", "labjack_write", to_string(writer_config));
+    auto writer_mock_ctx = std::make_shared<task::MockContext>(client);
+
+    auto writer_task = labjack::WriterTask::configure(writer_mock_ctx, sy_task);
+
+    auto writer_start_cmd = task::Command{sy_task.key, "start", {}};
+    auto writer_stop_cmd = task::Command{sy_task.key, "stop", {}};
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    reader_task->exec(start_cmd);
+    writer_task->exec(writer_start_cmd);
+    std::this_thread::sleep_for(std::chrono::seconds(30000));
+    writer_task->exec(writer_stop_cmd);
+    reader_task->exec(stop_cmd);
 }
