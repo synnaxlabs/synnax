9ea31099d3d65c30b5afb272752f7d46035e41a1
pjdotson <patrick@synnaxlabs.com>
2024-10-23 11:13:38 -0700

    fix log documentation

--- docs/site/src/pages/reference/console/_nav.tsx
+++ docs/site/src/pages/reference/console/_nav.tsx
@@ -54,14 +54,14 @@ export const consoleNav: PageNavNode = {
       name: "Schematics",
     },
     {
-      key: "/reference/console/users",
-      href: "/reference/console/users",
-      name: "Users",
-    },
-    {
       key: "/reference/console/logs",
       href: "/reference/console/logs",
       name: "Logs",
+    },
+    {
+      key: "/reference/console/users",
+      href: "/reference/console/users",
+      name: "Users",
     },
   ],
 };
