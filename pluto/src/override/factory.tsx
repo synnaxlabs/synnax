5d459cf2ec22a64079219b76977d5cf3dcbb6451
pjdotson <patrick@synnaxlabs.com>
2024-11-02 17:53:26 -0700

    fix ts complimation issues

--- pluto/src/override/factory.tsx
+++ pluto/src/override/factory.tsx
@@ -7,7 +7,7 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-import { type FC, PropsWithoutRef } from "react";
+import { type FC, type PropsWithoutRef } from "react";
 
 export const createComponent = <P extends PropsWithoutRef<any>>(
   Base: FC<P>,
