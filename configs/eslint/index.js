cb7c23e730307d5104ee53d31ae291d8bb3aa72f
pjdotson <patrick@synnaxlabs.com>
2024-11-10 18:18:54 -0800

    more changes

--- configs/eslint/index.js
+++ configs/eslint/index.js
@@ -9,7 +9,7 @@
 
 import { fixupConfigRules, includeIgnoreFile } from "@eslint/compat";
 import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import pluginReact from "eslint-plugin-react/configs/recommended.js";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import globals from "globals";
 import path from "path";
@@ -25,17 +25,61 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
+  ...fixupConfigRules(pluginReact),
   {
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
       globals: { ...globals.browser },
-      parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
     },
-
     plugins: { "simple-import-sort": simpleImportSort },
     rules: {
+      "no-constant-condition": ["error", { checkLoops: false }],
+      "no-duplicate-imports": "error",
+      "use-isnan": ["error", { enforceForIndexOf: true }],
+      "valid-typeof": ["error", { requireStringLiterals: true }],
+      "arrow-body-style": "error",
+      curly: ["error", "multi"],
+      "dot-notation": "error",
+      "logical-assignment-operators": [
+        "error",
+        "always",
+        { enforceForIfStatements: true },
+      ],
+      "no-array-constructor": "error",
+      "no-else-return": ["error", { allowElseIf: false }],
+      "no-extra-boolean-cast": ["error", { enforceForInnerExpressions: true }],
+      "no-lonely-if": "error",
+      "no-object-constructor": "error",
+      "no-undef-init": "error",
+      "no-unneeded-ternary": ["error", { defaultAssignment: false }],
+      "no-useless-computed-key": "error",
+      "no-useless-concat": "error",
+      "no-useless-constructor": "error",
+      "no-useless-rename": "error",
+      "no-useless-return": "error",
+      "object-shorthand": "error",
+      "operator-assignment": "error",
+      "prefer-arrow-callback": "error",
+      "prefer-const": "error",
+      "prefer-exponentiation-operator": "error",
+      "prefer-numeric-literals": "error",
+      "prefer-object-has-own": "error",
+      "prefer-object-spread": "error",
+      "prefer-regex-literals": ["error", { disallowRedundantWrapping: true }],
+      "prefer-spread": "error",
+      "prefer-template": "error",
+      radix: ["error", "as-needed"],
+      yoda: "error",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { fixStyle: "inline-type-imports" },
+      ],
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
       "react/react-in-jsx-scope": "off",
@@ -57,7 +101,6 @@ export default [
           ignoreRestSiblings: true,
         },
       ],
-      "no-constant-condition": "off",
     },
     settings: {
       "import/resolver": {
@@ -67,7 +110,5 @@ export default [
       react: { version: "^18.0.0" },
     },
   },
-  {
-    ignores: ["node_modules", "build", "dist", "release", "**/*.d.ts"],
-  },
+  { ignores: ["node_modules", "build", "dist", "release", "**/*.d.ts"] },
 ];
