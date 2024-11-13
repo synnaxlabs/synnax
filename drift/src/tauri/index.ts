7b24c30e0d623e1dd7e00441e19dab63e81a687e
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-11-13 08:45:06 -0800

    [drift/tauri] - fixed logical and physical position calculations

--- drift/src/tauri/index.ts
+++ drift/src/tauri/index.ts
@@ -8,7 +8,7 @@
 // included in the file licenses/APL.txt.
 
 import { type Action, type UnknownAction } from "@reduxjs/toolkit";
-import { debounce as debounceF, type dimensions, type xy } from "@synnaxlabs/x";
+import { debounce as debounceF, dimensions, xy } from "@synnaxlabs/x";
 import {
   emit,
   type Event as TauriEvent,
@@ -278,8 +278,8 @@ export class TauriRuntime<S extends StoreState, A extends Action = UnknownAction
     const scaleFactor = await this.win.scaleFactor();
     const visible = await this.win.isVisible();
     return {
-      position: await parsePosition(await this.win.innerPosition(), scaleFactor),
-      size: await parseSize(await this.win.innerSize(), scaleFactor),
+      position: parsePosition(await this.win.innerPosition(), scaleFactor),
+      size: parseSize(await this.win.innerSize(), scaleFactor),
       maximized: await this.win.isMaximized(),
       visible,
       fullscreen: await this.win.isFullscreen(),
@@ -306,8 +306,8 @@ const newWindowPropsHandlers = (): HandlerEntry[] => [
         maximized: await window.isMaximized(),
         visible,
         minimized: !visible,
-        position: await parsePosition(await window.innerPosition(), scaleFactor),
-        size: await parseSize(await window.innerSize(), scaleFactor),
+        position: parsePosition(await window.innerPosition(), scaleFactor),
+        size: parseSize(await window.innerSize(), scaleFactor),
       };
       return setWindowProps(nextProps);
     },
@@ -318,7 +318,7 @@ const newWindowPropsHandlers = (): HandlerEntry[] => [
     handler: async (window) => {
       const scaleFactor = await window?.scaleFactor();
       if (scaleFactor == null) return null;
-      const position = await parsePosition(await window.innerPosition(), scaleFactor);
+      const position = parsePosition(await window.innerPosition(), scaleFactor);
       const visible = await window.isVisible();
       const nextProps: SetWindowPropsPayload = {
         label: window.label,
@@ -346,18 +346,8 @@ const newWindowPropsHandlers = (): HandlerEntry[] => [
   },
 ];
 
-const parsePosition = async (
-  position: PhysicalPosition,
-  scaleFactor: number,
-): Promise<xy.XY> => {
-  const logical = position.toLogical(scaleFactor);
-  return { x: logical.x, y: logical.y };
-};
+const parsePosition = (position: PhysicalPosition, scaleFactor: number): xy.XY =>
+  xy.scale(position, 1 / scaleFactor);
 
-const parseSize = async (
-  size: PhysicalSize,
-  scaleFactor: number,
-): Promise<dimensions.Dimensions> => {
-  const logical = size.toLogical(scaleFactor);
-  return { width: logical.width, height: logical.height };
-};
+const parseSize = (size: PhysicalSize, scaleFactor: number): dimensions.Dimensions =>
+  dimensions.scale(size, 1 / scaleFactor);
