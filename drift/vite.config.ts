1080bc3ec8d42997579c001b8360657d07177a57
pjdotson <patrick@synnaxlabs.com>
2024-12-11 15:19:32 -0800

    ops: trim unneeded code

--- drift/vite.config.ts
+++ drift/vite.config.ts
@@ -7,7 +7,7 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-/// <reference types="vitest" />
+/// <reference types="vitest/config" />
 
 import { lib } from "@synnaxlabs/vite-plugin";
 import path from "path";
--- x/media/vite.config.ts
+++ x/media/vite.config.ts
@@ -7,8 +7,6 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-/// <reference types="vitest/config" />
-
 import { lib } from "@synnaxlabs/vite-plugin";
 import { defineConfig } from "vite";
 
@@ -20,5 +18,4 @@ export default defineConfig({
       output: { globals: { react: "React", "react-dom": "ReactDOM" } },
     },
   },
-  test: { globals: true, environment: "jsdom" },
 });
