33c4ce486054789cef8ef2dbc6916e83af580280
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-12-14 22:03:01 -0800

    [console] - added schematic and line plot import commands

--- console/src/lineplot/services/palette.tsx
+++ console/src/lineplot/services/palette.tsx
@@ -11,6 +11,9 @@ import { Icon } from "@synnaxlabs/media";
 
 import { create } from "@/lineplot/LinePlot";
 import { type Command } from "@/palette/Palette";
+import { importPlot } from "@/lineplot/file";
+import { Workspace } from "@/workspace";
+import { ImportIcon } from "@/lineplot/services/Icon";
 
 export const createLinePlotCommand: Command = {
   key: "create-line-plot",
@@ -19,4 +22,21 @@ export const createLinePlotCommand: Command = {
   onSelect: ({ placeLayout }) => placeLayout(create({})),
 };
 
-export const COMMANDS = [createLinePlotCommand];
+export const importLinePlotCommand: Command = {
+  key: "import-line-plot",
+  name: "Import Line Plot",
+  icon: <ImportIcon />,
+  onSelect: ({ placeLayout, ...props }) => {
+    const { store } = props;
+    const state = store.getState();
+    const activeWorkspaceKey = Workspace.selectActiveKey(state);
+    importPlot({
+      activeWorkspaceKey,
+      placer: placeLayout,
+      dispatch: store.dispatch,
+      ...props,
+    });
+  },
+};
+
+export const COMMANDS = [createLinePlotCommand, importLinePlotCommand];
