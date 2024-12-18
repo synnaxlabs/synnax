02f1c3388e9409cb0b28154cfcc34ed09b36ca12
pjdotson <patrick@synnaxlabs.com>
2024-12-18 15:06:27 -0800

    Add missing package to ESLint config

--- configs/eslint/eslint.config.js
+++ configs/eslint/eslint.config.js
@@ -7,6 +7,6 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-import synnaxConfig from "./index";
+import synnaxConfig from "eslint-config-synnaxlabs";
 
 export default synnaxConfig;
