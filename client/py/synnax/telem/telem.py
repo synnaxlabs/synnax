694d224a6153743848ab27c029f638779745fbb3
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-12-26 13:08:53 -0600

    [client/py] - adjusted data type constructor

--- client/py/synnax/telem/telem.py
+++ client/py/synnax/telem/telem.py
@@ -966,15 +966,12 @@ class DataType(str):
 
             raise TypeError(f"Cannot convert {type(value)} to DataType")
 
-        try:
-            value = DataType._FROM_NUMPY.get(np.dtype(value), None)
-            if value is not None:
-                return value
-        except Exception:
-            pass
-
         if isinstance(value, dict):
             return DataType.JSON
+
+        value = DataType._FROM_NUMPY.get(np.dtype(value), None)
+        if value is not None:
+            return value
 
         raise TypeError(f"Cannot convert {type(value)} to DataType")
 
