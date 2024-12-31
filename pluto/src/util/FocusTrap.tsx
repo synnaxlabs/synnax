ccb1b44e947d86cd0d90ba10b139a6ebee73d6ad
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-12-31 10:42:07 -0800

    [pluto] - removed focus trapping (#1026)

--- pluto/src/util/FocusTrap.tsx
+++
@@ -1,8 +0,0 @@
-import { FocusTrap as FocusTrapReact, type FocusTrapProps } from "focus-trap-react";
-import { type FC } from "react";
-
-import { canDisable } from "@/util/canDisable";
-
-export const FocusTrap = canDisable<FocusTrapProps>(
-  FocusTrapReact as unknown as FC<FocusTrapProps>,
-);
