a2a7ad292765305480828a32c888206b479aba41
pjdotson <patrick@synnaxlabs.com>
2024-12-31 11:35:39 -0600

    fix typing issues

--- console/src/hardware/ni/task/DigitalWrite.tsx
+++ console/src/hardware/ni/task/DigitalWrite.tsx
@@ -23,7 +23,6 @@ import { type Properties } from "@/hardware/ni/device/types";
 import { CopyButtons, SelectDevice } from "@/hardware/ni/task/common";
 import { createLayoutCreator } from "@/hardware/ni/task/createLayoutCreator";
 import {
-  type Chan,
   DIGITAL_WRITE_TYPE,
   type DigitalWrite,
   type DigitalWriteConfig,
@@ -385,13 +384,13 @@ const ChannelList = ({
         )}
         {...menuProps}
       >
-        <List.List<string, Chan>
+        <List.List<string, DOChan>
           data={value}
           emptyContent={
             <ChannelListEmptyContent onAdd={handleAdd} snapshot={snapshot} />
           }
         >
-          <List.Selector<string, Chan>
+          <List.Selector<string, DOChan>
             value={selected}
             allowNone={false}
             allowMultiple
@@ -400,7 +399,7 @@ const ChannelList = ({
             }
             replaceOnSingle
           >
-            <List.Core<string, Chan> grow>
+            <List.Core<string, DOChan> grow>
               {({ key, ...props }) => (
                 <ChannelListItem key={key} {...props} path={path} snapshot={snapshot} />
               )}
