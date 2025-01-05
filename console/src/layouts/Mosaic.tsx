8bb5c171e7db655747a18551615106a8c4118edf
pjdotson <patrick@synnaxlabs.com>
2025-01-05 08:58:54 -0600

    Format and lint console code

--- console/src/layouts/Mosaic.tsx
+++ console/src/layouts/Mosaic.tsx
@@ -7,8 +7,6 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-import "@/layouts/Mosaic.css";
-
 import { ontology } from "@synnaxlabs/client";
 import { selectWindowKey } from "@synnaxlabs/drift";
 import { Icon, Logo } from "@synnaxlabs/media";
