5eac7c7e6b8b841ef15da3072da7b0ed9591175c
Elham Islam <32990373+Lham42@users.noreply.github.com>
2025-01-11 14:18:36 -0800

    [computron ] update build for linux

--- computron/build-linux.sh
+++ computron/build-linux.sh
@@ -2,10 +2,19 @@
 
 set -e
 
-# Set up variables
-PYTHON_VERSION="3.9.13"
-NUMPY_VERSION="1.21.6"
+# Update versions to match macOS build
+PYTHON_VERSION="3.11.7"
+NUMPY_VERSION="1.24.3"
+PYTHON_VERSION_MAJOR_MINOR=$(echo $PYTHON_VERSION | cut -d. -f1-2)
+PYTHON_A_FILE="libpython${PYTHON_VERSION_MAJOR_MINOR}.a"
+COMBINED_PYTHON_A_FILE="libpython${PYTHON_VERSION_MAJOR_MINOR}-combined.a"
 PYTHON_INSTALL_DIR="$(pwd)/python_install"
+
+echo "Python Installation Starting"
+echo "Python Version: ${PYTHON_VERSION}"
+echo "NumPy Version: ${NUMPY_VERSION}"
+echo "Python Install Directory: ${PYTHON_INSTALL_DIR}"
+sleep 1
 
 # Download and build Python
 curl -O https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz
@@ -19,7 +28,7 @@ cd Python-${PYTHON_VERSION}
     --with-ensurepip=no \
     LDFLAGS="-Wl,-rpath,${PYTHON_INSTALL_DIR}/lib"
 
-# Build Python
+# Build Python using all available cores
 make -j$(nproc)
 
 # Install Python
@@ -39,17 +48,20 @@ mkdir -p ${PYTHON_INSTALL_DIR}/lib/combined
 cd ${PYTHON_INSTALL_DIR}/lib/combined
 
 # Extract object files from Python static library
-ar -x ../libpython3.9.a
+ar -x ../${PYTHON_A_FILE}
 
 # Extract object files from NumPy static libraries
-numpy_lib_path=$(find ${PYTHON_INSTALL_DIR}/lib/python3.9/site-packages/numpy -name '*.a')
+numpy_lib_path=$(find ${PYTHON_INSTALL_DIR}/lib/python${PYTHON_VERSION_MAJOR_MINOR}/site-packages/numpy -name '*.a')
 for lib in $numpy_lib_path; do
     ar -x $lib
 done
 
 # Create combined static library
-ar -qc libpython3.9-combined.a *.o
-ranlib libpython3.9-combined.a
+ar -qc ${COMBINED_PYTHON_A_FILE} *.o
+ranlib ${COMBINED_PYTHON_A_FILE}
+
+# Create VERSION file
+echo ${PYTHON_VERSION} > ${PYTHON_INSTALL_DIR}/VERSION
 
 cd ../../..
 
