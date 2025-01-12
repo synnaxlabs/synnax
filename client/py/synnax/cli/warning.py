387986bcfd9d1974c525ff831c873321b71f5fba
Patrick Dotson <123601024+pjdotson@users.noreply.github.com>
2025-01-11 19:28:51 -0800

    SY-1823 Add Deprecation Warning to `synnax` and `synnaxkit` Python CLI Names (#1051)

---
+++ client/py/synnax/cli/warning.py
@@ -0,0 +1,24 @@
+#  Copyright 2024 Synnax Labs, Inc.
+#
+#  Use of this software is governed by the Business Source License included in the file
+#  licenses/BSL.txt.
+#
+#  As of the Change Date specified in that file, in accordance with the Business Source
+#  License, use of this software will be governed by the Apache License, Version 2.0,
+#  included in the file licenses/APL.txt.
+
+from click import Context, echo, style
+
+CORRECT_NAME = "sy"
+STYLED_ERROR = style("DEPRECATION WARNING:", fg="red")
+STYLED_CORRECT_NAME = style(CORRECT_NAME, fg="cyan")
+
+
+def warning(ctx: Context) -> None:
+    """Warns the user if a deprecated command is used."""
+    name = ctx.find_root().info_name
+    if name != CORRECT_NAME:
+        styled_name = style(name, fg="cyan")
+        echo(
+            f"{STYLED_ERROR} The {styled_name} command has been deprecated and will be removed in a future release. Please use {STYLED_CORRECT_NAME} instead."
+        )
