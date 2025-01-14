d496a24bd2dff872ed727ddee96e6ff15a28c9a5
pjdotson <patrick@synnaxlabs.com>
2025-01-14 14:33:39 -0800

    move unique and uniqueBy functions to their own x folder

--- x/ts/src/unique/index.ts
+++ x/ts/src/unique/index.ts
@@ -7,4 +7,4 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-export const unique = <V>(values: V[] | readonly V[]): V[] => [...new Set(values)];
+export * as unique from "@/unique/unique";
