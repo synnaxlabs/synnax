51b981dda2479aa7276473572603c664ec8a18b7
Elham Islam <elham@synnaxlabs.com>
2025-01-17 15:48:22 -0500

    [driver] handle empty device key

--- driver/ni/ni.cpp
+++ driver/ni/ni.cpp
@@ -518,11 +518,11 @@ ni::Source::Source(
 void ni::Source::parse_config(config::Parser &parser) {
     this->reader_config.sample_rate.value = parser.required<uint64_t>("sample_rate");
     this->reader_config.stream_rate.value = parser.required<uint64_t>("stream_rate");
-    // TODO: If device is an empty string, default to "cross-device"
     this->reader_config.device_key = parser.optional<std::string>("device", "cross-device");
+    if (this->reader_config.device_key == "" ) this->reader_config.device_key = "cross-device";
     this->reader_config.timing_source = "none";
     // parser.required<std::string>("timing_source"); TODO: uncomment this when ui provides timing source
-    if(this->reader_config.device_key  != "cross-device") {
+    if (this->reader_config.device_key  != "cross-device") {
         auto [dev, err] = this->ctx->client->hardware.retrieveDevice(
                 this->reader_config.device_key);
         if (err)
