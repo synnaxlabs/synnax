ce30d85e44239f94c4e404efca1e710460c91a6c
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-02-03 19:12:39 -0800

    [ops] - updated copyrights for go, css, typescript, and javascript

--- driver/opc/dev/run-opc-encrypted-server.sh
+++ driver/opc/dev/run-opc-encrypted-server.sh
@@ -1,5 +1,4 @@
-#
-# Copyright 2024 Synnax Labs, Inc.
+# Copyright 2025 Synnax Labs, Inc.
 #
 # Use of this software is governed by the Business Source License included in the file
 # licenses/BSL.txt.
@@ -7,7 +6,6 @@
 # As of the Change Date specified in that file, in accordance with the Business Source
 # License, use of this software will be governed by the Apache License, Version 2.0,
 # included in the file licenses/APL.txt.
-#
 
 realpath () (
     if [[ -d $1 ]]; then
