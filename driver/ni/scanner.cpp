9559a6a88c4cc91aa8e9cd4fb7764018b63b0233
emilbon99 <ebonilla@synnaxlabs.com>
2025-02-05 14:44:47 -0800

    [driver] - adjusted chassis filter properties

--- driver/ni/scanner.cpp
+++ driver/ni/scanner.cpp
@@ -55,7 +55,7 @@ ni::Scanner::Scanner(
     this->syscfg->SetFilterProperty(
         this->filter,
         NISysCfgFilterPropertyIsChassis,
-        NISysCfgBoolTrue
+        NISysCfgBoolFalse
     );
     VLOG(1) << "[ni.scanner] successfully configured scanner for task " << this->task.
             name;
