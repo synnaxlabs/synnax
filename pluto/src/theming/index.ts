713abcef336e8eb870d624410bb9eabac9a3ba6a
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-02-25 20:17:26 -0800

    SY-2100 - Make Sure Geist Mono Gets Correctly Imported into Windows Text Editor (#1118)
    * [console] - maded sure geist mono gets correctly imported on windows
    
    * [console/ops] - added lsp mocking on pull request
    
    * [console/ops] - added lsp mocking on pull request

--- pluto/src/theming/index.ts
+++ pluto/src/theming/index.ts
@@ -7,4 +7,7 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
+import "@fontsource-variable/inter";
+import "@fontsource/geist-mono";
+
 export * as Theming from "@/theming/external";
