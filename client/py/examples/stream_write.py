b3f92f9ad7a10f7accb47a053724b254c29070fe
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-03-14 12:37:49 -0700

    [client/py] - fixed stream writing example

--- client/py/examples/stream_write.py
+++ client/py/examples/stream_write.py
@@ -19,12 +19,7 @@ import synnax as sy
 
 # We've logged in via the command-line interface, so there's no need to provide
 # credentials here. See https://docs.synnaxlabs.com/reference/python-client/get-started.
-client = sy.Synnax(
-    host="localhost",
-    port=9091,
-    username="synnax",
-    password="seldon",
-)
+client = sy.Synnax()
 
 # Create an index channel that will be used to store our timestamps.
 time_channel = client.channels.create(
