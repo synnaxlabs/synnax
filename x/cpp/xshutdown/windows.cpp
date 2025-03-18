8568f63bbec7efbc456847e09cca98dd094926ed
Elham Islam <elham@synnaxlabs.com>
2025-03-18 12:47:47 -0700

    [x/cpp] - fixed windows shutdown

--- x/cpp/xshutdown/windows.cpp
+++ x/cpp/xshutdown/windows.cpp
@@ -16,16 +16,19 @@
 #include "x/cpp/xshutdown/xshutdown.h"
 
 namespace xshutdown::priv {
-std::mutex shutdown_mutex;
-std::condition_variable shutdown_cv;
-bool should_stop = false;
+    std::mutex shutdown_mutex;
+    std::condition_variable shutdown_cv;
+    bool should_stop = false;
 
-BOOL WINAPI console_ctrl_handler(DWORD ctrl_type) {
+    BOOL WINAPI console_ctrl_handler(DWORD ctrl_type) {
     if (ctrl_type == CTRL_C_EVENT) {
-        signal_shutdown();
-        return TRUE;
-    }
-    return FALSE;
+    // Flush stdout and stderr before signaling shutdown
+    fflush(stdout);
+    fflush(stderr);
+    signal_shutdown();
+    return TRUE;
+}
+return FALSE;
 }
 
 void listen_signal() {
@@ -34,26 +37,18 @@ void listen_signal() {
 
 void listen_stdin() {
     HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
-    DWORD mode;
-    GetConsoleMode(hStdin, &mode);
-    SetConsoleMode(hStdin, mode & ~ENABLE_MOUSE_INPUT & ~ENABLE_WINDOW_INPUT);
-    
     char buffer[256];
     std::string input;
-    
+
     while (true) {
         if (should_shutdown()) break;
-        
-        DWORD available;
-        INPUT_RECORD record;
-        PeekConsoleInput(hStdin, &record, 1, &available);
-        
-        if (available > 0) {
-            DWORD read;
-            if (ReadConsole(hStdin, buffer, sizeof(buffer) - 1, &read, nullptr)) {
-                buffer[read] = '\0';
+
+        DWORD bytesRead;
+        if (ReadFile(hStdin, buffer, sizeof(buffer) - 1, &bytesRead, nullptr)) {
+            if (bytesRead > 0) {
+                buffer[bytesRead] = '\0';
                 input += buffer;
-                
+
                 size_t pos;
                 while ((pos = input.find('\n')) != std::string::npos) {
                     std::string line = input.substr(0, pos);
@@ -61,7 +56,7 @@ void listen_stdin() {
                         line = line.substr(0, line.length()-1);
                     }
                     input.erase(0, pos + 1);
-                    
+
                     if (line == "STOP") {
                         signal_shutdown();
                         return;
@@ -69,7 +64,7 @@ void listen_stdin() {
                 }
             }
         }
-        
+
         Sleep(100);
     }
 }
