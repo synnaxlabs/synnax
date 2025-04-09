f662d1fd2d6aa3ad6f4c0aa7f606473b7db36fcf
pjdotson <patrick@synnaxlabs.com>
2025-04-08 21:56:14 -0700

    device Toolbar

--- console/src/hardware/device/Toolbar.tsx
+++ console/src/hardware/device/Toolbar.tsx
@@ -9,7 +9,7 @@
 
 import { ontology } from "@synnaxlabs/client";
 import { Icon } from "@synnaxlabs/media";
-import { Align, Header, Synnax } from "@synnaxlabs/pluto";
+import { Align, Synnax } from "@synnaxlabs/pluto";
 import { useQuery } from "@tanstack/react-query";
 import { type ReactElement } from "react";
 
@@ -36,7 +36,6 @@ const Content = (): ReactElement => {
       <Align.Space empty style={{ height: "100%" }}>
         <Toolbar.Header>
           <Toolbar.Title icon={<Icon.Device />}>Devices</Toolbar.Title>
-          <Header.Actions />
         </Toolbar.Header>
         <Ontology.Tree root={group.data} />
       </Align.Space>
