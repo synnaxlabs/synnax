31b639ae78073a3d9d10091d5d415aaef91f5262
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-04-15 09:40:43 -0700

    [x/go] - improved enum validation and validation test suite

--- cesium/internal/migrate/migrate.go
+++ cesium/internal/migrate/migrate.go
@@ -10,13 +10,17 @@
 package version
 
 import (
+	"github.com/synnaxlabs/cesium/internal/meta"
+	"github.com/synnaxlabs/x/binary"
 	"github.com/synnaxlabs/x/errors"
 	xfs "github.com/synnaxlabs/x/io/fs"
+	"github.com/synnaxlabs/x/validate"
 	"strconv"
 )
 
 var migrations = map[string]func(fs xfs.FS) error{
 	"01": migrate01,
+	"02": migrate02,
 }
 
 func Migrate(fs xfs.FS, oldVersion Version, newVersion Version) error {
@@ -34,5 +38,36 @@ func Migrate(fs xfs.FS, oldVersion Version, newVersion Version) error {
 
 // migrate01 is a migration from unversioned to v1 (which is the same as unversioned)
 func migrate01(_ xfs.FS) error {
+	return nil
+}
+
+func migrate02(fs xfs.FS) error {
+	dirs, err := fs.List(".")
+	if err != nil {
+		return err
+	}
+	for _, dir := range dirs {
+		if !dir.IsDir() {
+			continue
+		}
+		subFS, err := fs.Sub(dir.Name())
+		if err != nil {
+			return err
+		}
+		_, err = meta.Read(subFS, &binary.JSONCodec{})
+		if err == nil {
+			continue
+		}
+		var fieldErr validate.FieldError
+		if errors.As(err, &fieldErr) {
+			if fieldErr.Field == "index" && fieldErr.Message == "non-indexed channel must have an index" {
+				if err := fs.Remove(dir.Name()); err != nil {
+					return err
+				}
+			}
+		} else {
+			return err
+		}
+	}
 	return nil
 }
