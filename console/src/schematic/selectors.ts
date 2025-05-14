1c4939a6c02e206a271b03115be19b9163ac3d57
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-05-14 09:38:02 -0700

    [console] - fixed schematic edge selection

--- console/src/schematic/selectors.ts
+++ console/src/schematic/selectors.ts
@@ -52,9 +52,14 @@ export const selectSelectedElementDigests = (
   layoutKey: string,
 ): ElementDigest[] => {
   const schematic = select(state, layoutKey);
-  return schematic.nodes
-    .filter((node) => node.selected)
-    .map((node) => ({ key: node.key, type: "node" }));
+  return [
+    ...schematic.nodes
+      .filter((node) => node.selected)
+      .map((node) => ({ key: node.key, type: "node" as const })),
+    ...schematic.edges
+      .filter((edge) => edge.selected)
+      .map((edge) => ({ key: edge.key, type: "edge" as const })),
+  ];
 };
 
 export const useSelectSelectedElementDigests = (layoutKey: string): ElementDigest[] =>
