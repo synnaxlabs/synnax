b14523aa4f96c0cad18aa97b7950404b82406f51
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-05-29 21:05:54 -0500

    SY-2454 - Refactored Color Utilities to Use Functional Patterns (#1238)
    * Auto Update JSON File
    
    * [ops] - bumped RC version
    
    * Auto Update JSON File
    
    * Auto Update JSON File
    
    * Auto Update JSON File
    
    * Auto Update JSON File
    
    * Fix  CLI flag
    
    * Fix spacing in log
    
    * [x/color] - refactored color to use functional patterns
    
    * checkpoint
    
    * [color] - improved module
    
    * [ops] - addressed pull request comments
    
    ---------
    
    Co-authored-by: pjdotson <pjdotson@users.noreply.github.com>
    Co-authored-by: emilbon99 <emilbon99@users.noreply.github.com>
    Co-authored-by: pjdotson <patrick@synnaxlabs.com>

--- x/ts/src/color/external.ts
+++ x/ts/src/color/external.ts
@@ -7,22 +7,6 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-import { z } from "zod";
-
-import { Color } from "@/color/core/color";
-
-const styleZ = z.object({
-  key: z.string(),
-  name: z.string(),
-  color: Color.z,
-});
-
-export type Style = z.infer<typeof styleZ>;
-
-export const paletteZ = z.object({
-  key: z.string(),
-  name: z.string(),
-  swatches: z.array(styleZ),
-});
-
-export type Palette = z.infer<typeof paletteZ>;
+export * from "@/color/color";
+export * from "@/color/gradient";
+export * from "@/color/palette";
