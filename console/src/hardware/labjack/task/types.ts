fc4eb6cbdae79a4e640cf8d3b2581d6183c1ec94
pjdotson <patrick@synnaxlabs.com>
2025-06-02 17:27:41 -0600

    Update zod parsing for task types

--- console/src/hardware/labjack/task/types.ts
+++ console/src/hardware/labjack/task/types.ts
@@ -8,7 +8,7 @@
 // included in the file licenses/APL.txt.
 
 import { channel, type task } from "@synnaxlabs/client";
-import { type core, z } from "zod";
+import { z } from "zod";
 
 import { Common } from "@/hardware/common";
 import { Device } from "@/hardware/labjack/device";
@@ -224,7 +224,10 @@ export const ZERO_OUTPUT_CHANNEL: OutputChannel = ZERO_OUTPUT_CHANNELS[DO_CHANNE
 export type Channel = InputChannel | OutputChannel;
 export type ChannelType = Channel["type"];
 
-const validateUniquePorts: core.CheckFn<Channel[]> = ({ value: channels, issues }) => {
+const validateUniquePorts: z.core.CheckFn<Channel[]> = ({
+  value: channels,
+  issues,
+}) => {
   const portToIndexMap = new Map<string, number>();
   channels.forEach(({ port }, i) => {
     if (!portToIndexMap.has(port)) {
@@ -232,11 +235,10 @@ const validateUniquePorts: core.CheckFn<Channel[]> = ({ value: channels, issues 
       return;
     }
     const index = portToIndexMap.get(port) as number;
-
     const code = "custom";
-    const msg = `Port ${port} has already been used on another channel`;
-    issues.push({ code, message: msg, path: [index, "port"], input: channels });
-    issues.push({ code, message: msg, path: [i, "port"], input: channels });
+    const message = `Port ${port} has already been used on another channel`;
+    issues.push({ code, message, path: [index, "port"], input: channels });
+    issues.push({ code, message, path: [i, "port"], input: channels });
   });
 };
 
