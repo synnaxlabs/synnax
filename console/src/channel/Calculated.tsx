2822363e09e502da8e455bb379900bcfa1e63751
pjdotson <patrick@synnaxlabs.com>
2025-06-02 14:20:31 -0600

    use react query properly

--- console/src/channel/Calculated.tsx
+++ console/src/channel/Calculated.tsx
@@ -154,7 +154,7 @@ export const Calculated: Layout.Renderer = ({ layoutKey, onClose }) => {
     },
   });
 
-  if (res.isLoading) return <Text.Text level="p">Loading...</Text.Text>;
+  if (res.isPending) return <Text.Text level="p">Loading...</Text.Text>;
   if (res.isError)
     return (
       <Align.Space y grow style={{ height: "100%" }}>
@@ -162,7 +162,7 @@ export const Calculated: Layout.Renderer = ({ layoutKey, onClose }) => {
       </Align.Space>
     );
 
-  return <Internal onClose={onClose} initialValues={res.data as FormValues} />;
+  return <Internal onClose={onClose} initialValues={res.data} />;
 };
 
 interface InternalProps extends Pick<Layout.RendererProps, "onClose"> {
