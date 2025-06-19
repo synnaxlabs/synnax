fefd5d84666d21a677390284c37a45fca9925cb8
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-06-19 13:34:44 -0600

    [synnax] - adjusted python client to match new status implementation

--- client/py/synnax/status/status.py
+++ client/py/synnax/status/status.py
@@ -7,7 +7,14 @@
 #  License, use of this software will be governed by the Apache License, Version 2.0,
 #  included in the file licenses/APL.txt.
 
-from typing import Literal
+from typing import Literal, TypeVar, Generic
+
+
+from pydantic import Field
+from uuid import uuid4
+
+from freighter import Payload
+from synnax.telem import TimeStamp
 
 SUCCESS_VARIANT = "success"
 INFO_VARIANT = "info"
@@ -25,3 +32,13 @@ Variant = Literal[
     "loading",
 ]
 """Represents the variant of a status message."""
+
+D = TypeVar("D", bound=Payload, default=Payload)
+
+class Status(Payload, Generic[D]):
+    key: str = Field(default=str(uuid4()))
+    variant: Variant
+    message: str
+    description: str = ""
+    time: TimeStamp = Field(default=TimeStamp.now())
+    details: D
