36183846b46a6089cc1da8335b56dd2f637008ae
Patrick Dotson <--local>
2025-06-23 12:11:20 -0600

    Move fallback and passthrough codecs to separate files

--- x/go/binary/codec.go
+++ x/go/binary/codec.go
@@ -10,7 +10,6 @@
 package binary
 
 import (
-	"bytes"
 	"context"
 	"io"
 	"reflect"
@@ -61,37 +60,6 @@ type Decoder interface {
 	DecodeStream(ctx context.Context, r io.Reader, value any) error
 }
 
-// PassThroughCodec wraps a Codec and checks for values that are already encoded
-// ([]byte) and returns them as is.
-type PassThroughCodec struct{ Codec }
-
-// Encode implements the Encoder interface.
-func (enc *PassThroughCodec) Encode(ctx context.Context, value any) ([]byte, error) {
-	if bv, ok := value.([]byte); ok {
-		return bv, nil
-	}
-	return enc.Codec.Encode(ctx, value)
-}
-
-// Decode implements the Decoder interface.
-func (enc *PassThroughCodec) Decode(ctx context.Context, data []byte, value any) error {
-	return enc.DecodeStream(ctx, bytes.NewReader(data), value)
-}
-
-// DecodeStream implements the Decoder interface.
-func (enc *PassThroughCodec) DecodeStream(ctx context.Context, r io.Reader, value any) error {
-	if bv, ok := value.(*[]byte); ok {
-		*bv, _ = io.ReadAll(r)
-		return nil
-	}
-	return enc.Codec.DecodeStream(ctx, r, value)
-}
-
-// EncodeStream implements the Encoder interface.
-func (enc *PassThroughCodec) EncodeStream(ctx context.Context, w io.Writer, value any) error {
-	return enc.Codec.EncodeStream(ctx, w, value)
-}
-
 // MarshalStringInt64 marshals the int64 value to a UTF-8 string.
 func MarshalStringInt64(n int64) ([]byte, error) {
 	return []byte(`"` + strconv.Itoa(int(n)) + `"`), nil
@@ -100,59 +68,4 @@ func MarshalStringInt64(n int64) ([]byte, error) {
 // MarshalStringUint64 marshals the uint64 value to a UTF-8 string.
 func MarshalStringUint64(n uint64) ([]byte, error) {
 	return []byte(`"` + strconv.FormatUint(n, 10) + `"`), nil
-}
-
-// decodeFallbackCodec wraps a set of Codecs. When the first Codec in the chain fails to
-// decode a value, it falls back to the next Codec in the chain.
-type decodeFallbackCodec struct {
-	// Codecs is the list of codecs to fallback on.
-	Codecs []Codec
-}
-
-func NewDecodeFallbackCodec(base Codec, codecs ...Codec) Codec {
-	return &decodeFallbackCodec{Codecs: append([]Codec{base}, codecs...)}
-}
-
-var _ Codec = (*decodeFallbackCodec)(nil)
-
-// Encode implements the Encoder interface.
-func (f *decodeFallbackCodec) Encode(ctx context.Context, value any) ([]byte, error) {
-	return f.Codecs[0].Encode(ctx, value)
-}
-
-func (f *decodeFallbackCodec) EncodeStream(ctx context.Context, w io.Writer, value any) error {
-	return f.Codecs[0].EncodeStream(ctx, w, value)
-}
-
-// Decode implements the Decoder interface.
-func (f *decodeFallbackCodec) Decode(ctx context.Context, data []byte, value any) error {
-	for _, c := range f.Codecs {
-		if err := c.Decode(ctx, data, value); err == nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// DecodeStream implements the Decoder interface.
-func (f *decodeFallbackCodec) DecodeStream(
-	ctx context.Context,
-	r io.Reader,
-	value any,
-) error {
-	if len(f.Codecs) == 0 {
-		panic("[binary] - no codecs provided to decodeFallbackCodec")
-	}
-	// We need to read out all the data here, otherwise an initial codec that fails will
-	// leave the reader in a bad state. It's not ideal, but we need to do it.
-	data, err := io.ReadAll(r)
-	if err != nil {
-		return err
-	}
-	for _, c := range f.Codecs {
-		if err = c.DecodeStream(ctx, bytes.NewReader(data), value); err == nil {
-			return err
-		}
-	}
-	return err
 }
---
+++ x/go/binary/passthrough.go
@@ -0,0 +1,47 @@
+// Copyright 2025 Synnax Labs, Inc.
+//
+// Use of this software is governed by the Business Source License included in the file
+// licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with the Business Source
+// License, use of this software will be governed by the Apache License, Version 2.0,
+// included in the file licenses/APL.txt.
+
+package binary
+
+import (
+	"bytes"
+	"context"
+	"io"
+)
+
+// PassThroughCodec wraps a Codec and checks for values that are already encoded
+// ([]byte) and returns them as is.
+type PassThroughCodec struct{ Codec }
+
+// Encode implements the Encoder interface.
+func (p *PassThroughCodec) Encode(ctx context.Context, value any) ([]byte, error) {
+	if bv, ok := value.([]byte); ok {
+		return bv, nil
+	}
+	return p.Codec.Encode(ctx, value)
+}
+
+// Decode implements the Decoder interface.
+func (p *PassThroughCodec) Decode(ctx context.Context, data []byte, value any) error {
+	return p.DecodeStream(ctx, bytes.NewReader(data), value)
+}
+
+// DecodeStream implements the Decoder interface.
+func (p *PassThroughCodec) DecodeStream(ctx context.Context, r io.Reader, value any) error {
+	if bv, ok := value.(*[]byte); ok {
+		*bv, _ = io.ReadAll(r)
+		return nil
+	}
+	return p.Codec.DecodeStream(ctx, r, value)
+}
+
+// EncodeStream implements the Encoder interface.
+func (p *PassThroughCodec) EncodeStream(ctx context.Context, w io.Writer, value any) error {
+	return p.Codec.EncodeStream(ctx, w, value)
+}
