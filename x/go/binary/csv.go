ab76cd1f9eb8a8a210176851869da03a582c734d
Patrick Dotson <--local>
2025-06-23 18:32:36 -0600

    Add docs to csv

--- x/go/binary/csv.go
+++ x/go/binary/csv.go
@@ -18,25 +18,40 @@ import (
 	"github.com/synnaxlabs/x/errors"
 )
 
-var _ Codec = (*CSVCodec)(nil)
-
-type CSVCodec struct{}
-
+// CSVMarshaler is a type that can marshal itself to a CSV representation.
 type CSVMarshaler interface {
 	MarshalCSV() ([][]string, error)
 }
 
+// MarshalCSV marshals a value to a CSV representation. If each row in the CSV
+// representation has a different length, an error will be returned.
+func MarshalCSV(value any) ([][]string, error) {
+	m, ok := value.(CSVMarshaler)
+	if !ok {
+		return nil, errors.Newf("%T does not implement CSVMarshaler", value)
+	}
+	records, err := m.MarshalCSV()
+	if err != nil {
+		return nil, err
+	}
+	if len(records) == 0 || len(records[0]) == 0 {
+		return records, nil
+	}
+	rowLength := len(records[0])
+	for _, row := range records {
+		if len(row) != rowLength {
+			return nil, errors.Newf("all rows must have the same length. Row %d has length %d, expected %d", i, len(row), rowLength)
+		}
+	}
+	return records, nil
+}
+
+// CSVUnmarshaler is a type that can unmarshal itself from a CSV representation.
 type CSVUnmarshaler interface {
 	UnmarshalCSV([][]string) error
 }
 
-func MarshalCSV(value any) ([][]string, error) {
-	if m, ok := value.(CSVMarshaler); ok {
-		return m.MarshalCSV()
-	}
-	return nil, errors.Newf("%T does not implement CSVMarshaler", value)
-}
-
+// UnmarshalCSV unmarshals a value from a CSV representation.
 func UnmarshalCSV(data [][]string, value any) error {
 	if u, ok := value.(CSVUnmarshaler); ok {
 		return u.UnmarshalCSV(data)
@@ -44,16 +59,13 @@ func UnmarshalCSV(data [][]string, value any) error {
 	return errors.Newf("%T does not implement CSVUnmarshaler", value)
 }
 
-func validateCSV(records [][]string) error {
-	rowLengths := len(records[0])
-	for i, row := range records {
-		if len(row) != rowLengths {
-			return errors.Newf("all rows must have the same length. Row %d has length %d, expected %d", i, len(row), rowLengths)
-		}
-	}
-	return nil
-}
+// CSVCodec is a codec that encodes and decodes data to and from CSV format. CSVCodec
+// implements the Codec interface.
+type CSVCodec struct{}
 
+var _ Codec = (*CSVCodec)(nil)
+
+// Encode encodes a value to its CSV representation in bytes.
 func (c *CSVCodec) Encode(ctx context.Context, value any) ([]byte, error) {
 	records, err := MarshalCSV(value)
 	if err != nil {
@@ -61,9 +73,6 @@ func (c *CSVCodec) Encode(ctx context.Context, value any) ([]byte, error) {
 	}
 	if len(records) == 0 {
 		return []byte{}, nil
-	}
-	if err := validateCSV(records); err != nil {
-		return nil, sugarEncodingErr(value, err)
 	}
 	buf := bytes.NewBuffer(nil)
 	csvWriter := csv.NewWriter(buf)
@@ -78,6 +87,8 @@ func (c *CSVCodec) Encode(ctx context.Context, value any) ([]byte, error) {
 	return buf.Bytes()[:buf.Len()-2], nil
 }
 
+// EncodeStream encodes a value to a CSV representation in bytes and writes it to a
+// writer.
 func (c *CSVCodec) EncodeStream(ctx context.Context, w io.Writer, value any) error {
 	b, err := c.Encode(ctx, value)
 	if err != nil {
@@ -87,6 +98,7 @@ func (c *CSVCodec) EncodeStream(ctx context.Context, w io.Writer, value any) err
 	return sugarEncodingErr(value, err)
 }
 
+// Decode decodes a value from a CSV representation in bytes.
 func (c *CSVCodec) Decode(ctx context.Context, data []byte, value any) error {
 	csvReader := csv.NewReader(bytes.NewReader(data))
 	csvReader.ReuseRecord = true
@@ -103,6 +115,8 @@ func (c *CSVCodec) Decode(ctx context.Context, data []byte, value any) error {
 	return nil
 }
 
+// DecodeStream decodes a value from a CSV representation in bytes and reads it from a
+// reader.
 func (c *CSVCodec) DecodeStream(ctx context.Context, r io.Reader, value any) error {
 	data, err := io.ReadAll(r)
 	if err != nil {
