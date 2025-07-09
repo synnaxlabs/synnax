4e3f9b1764e1dd05c9d28155fa0fe88c71bdc547
Patrick Dotson <--local>
2025-07-09 14:36:33 -0600

    add export client

---
+++ client/ts/src/export/client.ts
@@ -0,0 +1,36 @@
+// Copyright 2025 Synnax Labs, Inc.
+//
+// Use of this software is governed by the Business Source License included in the file
+// licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with the Business Source
+// License, use of this software will be governed by the Apache License, Version 2.0,
+// included in the file licenses/APL.txt.
+
+import { type UnaryClient } from "@synnaxlabs/freighter";
+import { TimeRange } from "@synnaxlabs/x";
+import { z } from "zod/v4";
+
+import { channel } from "@/channel";
+
+const csvRequestZ = z.object({
+  keys: channel.keyZ.array(),
+  timeRange: TimeRange.z,
+  channelNames: z.record(channel.keyZ, z.string()).optional(),
+});
+
+type CSVRequest = z.infer<typeof csvRequestZ>;
+
+export class Client {
+  private readonly client: UnaryClient;
+
+  constructor(client: UnaryClient) {
+    this.client = client;
+  }
+
+  async csv(request: CSVRequest): Promise<Response> {
+    const [res, err] = await this.client.send("/export/csv", request, csvRequestZ);
+    if (err != null) throw err;
+    return res;
+  }
+}
