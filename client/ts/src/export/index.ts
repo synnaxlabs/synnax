2cae58d4efcbbb58613959594d1c7db906fbd1ea
Patrick Dotson <--local>
2025-07-11 11:34:58 -0600

    Fix import file path

--- client/ts/src/export/index.ts
+++ client/ts/src/export/index.ts
@@ -7,4 +7,4 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-export * as export from "./client";
+export * as export from "@/export/client";
