d4b00e6954319d9e5fc23f6b6380817113034933
Patrick Dotson <--local>
2025-07-17 12:43:38 -0600

    Update x/go/middleware package

--- x/go/middleware/middleware.go
+++ x/go/middleware/middleware.go
@@ -9,47 +9,63 @@
 
 package middleware
 
+// Handler is a function that can process a request of type I and returns a response of
+// type O or an error.
+type Handler[I, O any] func(I) (O, error)
+
 // Middleware is a function that can act on a request of type I, pass it on to the next
 // middleware, and process a response of type O.
 type Middleware[I, O any] interface {
-	// Exec executes the middleware. It receives a context and a request. It also
-	// receives a function that can be used to execute the next middleware in the chain.
-	Exec(I, func(I) (O, error)) (O, error)
-}
-
-// Finalizer is the final middleware in a chain, and is expected to execute the request.
-type Finalizer[I, O any] interface {
-	// Finalize is a special middleware that is executed after the middleware chain has
-	// completed.
-	Finalize(I) (O, error)
+	// Exec executes the middleware. It receives an input value and a function that can
+	// be used to execute the next middleware in the chain.
+	Exec(I, Handler[I, O]) (O, error)
 }
 
 // Chain is a chain of middleware that can be executed sequentially.
 type Chain[I, O any] []Middleware[I, O]
 
+var _ Middleware[any, any] = Chain[any, any]{}
+
 // Exec executes the middleware chain sequentially given a value. The first middleware
 // will be the first to receive the incoming, and the last to receive the outgoing
 // value.
-func (c Chain[I, O]) Exec(in I, finalizer Finalizer[I, O]) (O, error) {
+func (c Chain[I, O]) Exec(in I, finalizer Handler[I, O]) (O, error) {
 	var (
-		i    = 0
-		next func(I) (O, error)
+		i    int
+		next Handler[I, O]
 	)
-	next = func(_in I) (O, error) {
+	next = func(in I) (O, error) {
 		if i == len(c) {
-			return finalizer.Finalize(_in)
+			return finalizer(in)
 		}
-		_m := c[i]
+		m := c[i]
 		i++
-		return _m.Exec(_in, next)
+		return m.Exec(in, next)
 	}
 	return next(in)
 }
 
-// Collector allows middleware to be collected and executed in a chain.
-type Collector[I, O any] struct{ Chain[I, O] }
+// Collector provides a utility for constructing middleware chains.
+type Collector[I, O any] struct{ chain Chain[I, O] }
 
-// Use adds middleware to the collector.
-func (c *Collector[I, O]) Use(middlewares ...Middleware[I, O]) {
-	c.Chain = append(c.Chain, middlewares...)
+var _ Middleware[any, any] = (*Collector[any, any])(nil)
+
+// Exec executes the middleware chain with the provided input and finalizer.
+func (c *Collector[I, O]) Exec(in I, finalizer Handler[I, O]) (O, error) {
+	return c.chain.Exec(in, finalizer)
+}
+
+// Use adds the provided middleware to the chain.
+func (c *Collector[I, O]) Use(middleware ...Middleware[I, O]) {
+	c.chain = append(c.chain, middleware...)
+}
+
+// Func is an adapter that allows ordinary functions to be used as Middleware.
+type Func[I, O any] func(I, Handler[I, O]) (O, error)
+
+var _ Middleware[any, any] = (*Func[any, any])(nil)
+
+// Exec calls f(in, next)
+func (f Func[I, O]) Exec(in I, next Handler[I, O]) (O, error) {
+	return f(in, next)
 }
--- x/go/middleware/middleware_test.go
+++ x/go/middleware/middleware_test.go
@@ -10,58 +10,126 @@
 package middleware_test
 
 import (
+	"fmt"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/synnaxlabs/x/errors"
 	"github.com/synnaxlabs/x/middleware"
 )
 
-type request struct {
-	value string
-}
+type (
+	input          = string
+	output         = string
+	handler        = middleware.Handler[input, output]
+	testMiddleware = middleware.Middleware[input, output]
+	chain          = middleware.Chain[input, output]
+	collector      = middleware.Collector[input, output]
+	testFunc       = middleware.Func[input, output]
+)
 
-type response struct {
-	value string
-}
-
-type myFirstMiddleware struct{}
-
-var _ middleware.Middleware[*request, *response] = &myFirstMiddleware{}
-
-func (m *myFirstMiddleware) Exec(
-	req *request,
-	next func(*request) (*response, error),
-) (*response, error) {
-	req.value = "request"
-	return next(req)
-}
-
-type myFinalizer struct{}
-
-var _ middleware.Finalizer[*request, *response] = &myFinalizer{}
-
-func (m *myFinalizer) Finalize(req *request) (*response, error) {
-	return nil, nil
+func newStepper(steps *[]string) func(string) testMiddleware {
+	return func(name string) testMiddleware {
+		return testFunc(func(in input, next handler) (output, error) {
+			*steps = append(*steps, fmt.Sprintf("enter %s", name))
+			out, err := next(in)
+			*steps = append(*steps, fmt.Sprintf("exit %s", name))
+			return out, err
+		})
+	}
 }
 
 var _ = Describe("Middleware", func() {
-	It("Should execute middleware in the correct order", func() {
-		chain := &middleware.Chain[*request, *response]{
-			&myFirstMiddleware{},
-			&myFirstMiddleware{},
-		}
-		req := &request{}
-		_, err := chain.Exec(req, &myFinalizer{})
-		Expect(err).To(BeNil())
-		Expect(req.value).To(Equal("request"))
+	Describe("Chain", func() {
+		var (
+			steps   []string
+			stepper func(string) testMiddleware
+		)
+		BeforeEach(func() {
+			steps = []string{}
+			stepper = newStepper(&steps)
+		})
+		It("executes middleware in order and finalizer last", func() {
+			ch := chain{stepper("A"), stepper("B")}
+			finalizer := func(input) (output, error) {
+				steps = append(steps, "finalizer")
+				return "final output", nil
+			}
+			Expect(ch.Exec("hello", finalizer)).To(Equal("final output"))
+			Expect(steps).To(Equal([]string{
+				"enter A",
+				"enter B",
+				"finalizer",
+				"exit B",
+				"exit A",
+			}))
+		})
+		It("can short-circuit and skip remaining middleware", func() {
+			ch := chain{
+				testFunc(func(string, handler) (string, error) {
+					steps = append(steps, "short-circuit")
+					return "shorted", nil
+				}),
+				stepper("should not run"),
+			}
+			Expect(ch.Exec("hi", func(input) (output, error) {
+				Fail("finalizer should not be called")
+				return "", nil
+			})).To(Equal("shorted"))
+			Expect(steps).To(Equal([]string{"short-circuit"}))
+		})
+		It("propagates errors from middleware", func() {
+			ch := chain{
+				stepper("X"),
+				testFunc(func(input, handler) (output, error) {
+					return "", errors.New("middleware error")
+				}),
+				stepper("Z"),
+			}
+			Expect(ch.Exec("abc", func(input) (output, error) {
+				Fail("finalizer should not be called")
+				return "", nil
+			})).Error().To(MatchError("middleware error"))
+			Expect(steps).To(Equal([]string{"enter X", "exit X"}))
+		})
+		It("works with an empty chain", func() {
+			ch := chain{}
+			Expect(ch.Exec("foo", func(input) (output, error) {
+				return "no middleware", nil
+			})).To(Equal("no middleware"))
+		})
 	})
-
-	It("Should collect middleware correctly", func() {
-		collector := &middleware.Collector[*request, *response]{}
-		collector.Use(&myFirstMiddleware{})
-		collector.Use(&myFirstMiddleware{})
-		req := &request{}
-		_, err := collector.Chain.Exec(req, &myFinalizer{})
-		Expect(err).To(BeNil())
-		Expect(req.value).To(Equal("request"))
+	Describe("Collector", func() {
+		It("uses and executes middleware in correct order", func() {
+			c := collector{}
+			steps := []string{}
+			stepper := newStepper(&steps)
+			c.Use(stepper("A"), stepper("B"))
+			finalizer := func(input) (output, error) {
+				steps = append(steps, "finalizer")
+				return "collector output", nil
+			}
+			Expect(c.Exec("hello", finalizer)).To(Equal("collector output"))
+			Expect(steps).To(Equal([]string{
+				"enter A",
+				"enter B",
+				"finalizer",
+				"exit B",
+				"exit A",
+			}))
+		})
+	})
+	Describe("Func", func() {
+		It("adapts a plain function to Middleware and executes correctly", func() {
+			var called bool
+			fn := testFunc(func(in input, next handler) (output, error) {
+				called = true
+				return next("wrapped:" + in)
+			})
+			Expect(fn.Exec("data", func(in input) (output, error) {
+				return "handled:" + in, nil
+			})).To(Equal("handled:wrapped:data"))
+			Expect(called).To(BeTrue())
+		})
 	})
 })
