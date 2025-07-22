f36bfb2fb4dd5fa6aeae60d2d107d2498b7d803b
Patrick Dotson <--local>
2025-07-21 22:57:27 -0600

    add base64 encoding package

---
+++ x/go/encoding/base64/base64.go
@@ -0,0 +1,24 @@
+// Copyright 2025 Synnax Labs, Inc.
+//
+// Use of this software is governed by the Business Source License included in the file
+// licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with the Business Source
+// License, use of this software will be governed by the Apache License, Version 2.0,
+// included in the file licenses/APL.txt.
+
+// Package base64 provides utilities for encoding and decoding to Base64
+package base64
+
+import (
+	"encoding/base64"
+
+	"github.com/samber/lo"
+)
+
+// MustDecode decodes the given Base64-encoded string and panics if it fails. MustDecode
+// is intended for initialization of static variables.
+func MustDecode(s string) string {
+	msg := lo.Must(base64.StdEncoding.DecodeString(s))
+	return string(msg)
+}
