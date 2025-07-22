a2189c8860bd0d12e0c0fba8d1576773ee98f463
Patrick Dotson <--local>
2025-07-22 09:05:38 -0600

    Update documentation on types in numeric.go

--- x/go/types/numeric.go
+++ x/go/types/numeric.go
@@ -14,30 +14,35 @@ import (
 	"strconv"
 )
 
-// Numeric represents a generic numeric value.
+// Numeric represents any numeric value.
 type Numeric interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
 		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
 		~float32 | ~float64
 }
 
+// Integer represents any integer value.
 type Integer interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
 		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 
+// Signed represents any numeric value that can be negative.
 type Signed interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~float32 | ~float64
 }
 
+// SignedInteger represents any integer value that can be negative.
 type SignedInteger interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64
 }
 
+// UnsignedInteger represents any integer value that can only be positive.
 type UnsignedInteger interface {
 	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 
+// Floating represents any floating-point value.
 type Floating interface {
 	~float32 | ~float64
 }
