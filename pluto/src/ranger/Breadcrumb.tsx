a3ac6aad3a3b3b316372f1082ce6ccac9cfa3d2e
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-08-04 19:55:30 -0600

    [pluto] - breadcrumb tuning

---
+++ pluto/src/ranger/Breadcrumb.tsx
@@ -0,0 +1,27 @@
+import { type ranger } from "@synnaxlabs/client";
+
+import { Breadcrumb as Core } from "@/breadcrumb";
+
+export interface BreadcrumbProps extends Omit<Core.BreadcrumbProps, "children"> {
+  name: string;
+  showParent?: boolean;
+  parent?: Pick<ranger.Payload, "name"> | null;
+}
+
+export const Breadcrumb = ({
+  name,
+  parent,
+  showParent = true,
+  ...rest
+}: BreadcrumbProps) => (
+  <Core.Breadcrumb {...rest}>
+    <Core.Segment weight={450} color={10}>
+      {name}
+    </Core.Segment>
+    {parent != null && showParent && (
+      <Core.Segment weight={400} color={9}>
+        {parent.name}
+      </Core.Segment>
+    )}
+  </Core.Breadcrumb>
+);
