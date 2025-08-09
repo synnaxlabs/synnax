639c274c3dad6db63517477da03b4c157ad93c58
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-08-09 08:48:43 -0600

    [pluto] - fixed schematic orientation control

--- pluto/src/hooks/useRequiredContext.ts
+++ pluto/src/hooks/useRequiredContext.ts
@@ -7,12 +7,14 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
+import { NotFoundError } from "@synnaxlabs/client";
 import { use } from "react";
 
 export const useRequiredContext = <T>(
   context: React.Context<T | null>,
 ): NonNullable<T> => {
   const value = use(context);
-  if (value == null) throw new Error(`useRequiredContext: context value is null`);
+  if (value == null)
+    throw new NotFoundError(`useRequiredContext: context value is null`);
   return value;
 };
