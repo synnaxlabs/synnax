83fe3bea906c605cf773a23ff64107111e383afd
nico-synnax <nico@synnaxlabs.com>
2025-08-19 21:18:19 -0500

    SY-2746: DISABLE ALL NI SCANNER FILTERS

--- driver/ni/scan_task.cpp
+++ driver/ni/scan_task.cpp
@@ -156,18 +156,18 @@ xerrors::Error ni::Scanner::start() {
 
     if (const auto err = this->syscfg->CreateFilter(this->session, &this->filter))
         return err;
-    if (const auto err = this->syscfg->SetFilterProperty(
-            this->filter,
-            NISysCfgFilterPropertyIsDevice,
-            NISysCfgBoolTrue
-        ))
-        return err;
-    if (const auto err = this->syscfg->SetFilterProperty(
-            this->filter,
-            NISysCfgFilterPropertyIsPresent,
-            NISysCfgIsPresentTypePresent
-        ))
-        return err;
+    //if (const auto err = this->syscfg->SetFilterProperty(
+    //        this->filter,
+    //        NISysCfgFilterPropertyIsDevice,
+    //        NISysCfgBoolTrue
+    //    ))
+    //    return err;
+    //if (const auto err = this->syscfg->SetFilterProperty(
+    //       this->filter,
+    //        NISysCfgFilterPropertyIsPresent,
+    //        NISysCfgIsPresentTypePresent
+    //     ))
+    //    return err;
     //if (const auto err = this->syscfg->SetFilterProperty(
     //        this->filter,
     //        NISysCfgFilterPropertyIsChassis,
@@ -181,4 +181,5 @@ xerrors::Error ni::Scanner::start() {
     //    ))
     //     return err;
     //return xerrors::NIL;
+
 }
