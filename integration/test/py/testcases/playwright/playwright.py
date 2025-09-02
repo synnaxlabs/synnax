5168bfb304921d301e7eba54fecce32ff0be98ba
nico-synnax <nico@synnaxlabs.com>
2025-09-02 15:52:27 -0500

    SY-2780: Control or Meta Shift

--- integration/test/py/testcases/playwright/playwright.py
+++ integration/test/py/testcases/playwright/playwright.py
@@ -41,9 +41,9 @@ class Playwright(TestCase):
         self.page.wait_for_load_state('networkidle')
 
         # Toggle theme
-        time.sleep(0.1)
+        time.sleep(0.5)
         self.page.keyboard.press("ControlOrMeta+Shift+p")
-        time.sleep(0.1)
+        time.sleep(0.5)
         self.page.wait_for_selector("#toggle-theme", timeout=5000)
         self.page.locator("#toggle-theme").click()
 
