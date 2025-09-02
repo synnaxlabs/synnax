41ab114f6484532a1f72fffdb814c5c145d9dd98
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-09-01 20:59:21 -0600

    [console] - fixed line plot lines styles

--- console/src/lineplot/toolbar/Lines.tsx
+++ console/src/lineplot/toolbar/Lines.tsx
@@ -128,7 +128,7 @@ const Line = ({
   } = typedLineKeyFromString(line.key);
 
   return (
-    <List.Item itemKey={itemKey} index={0} key={itemKey} bordered gap="large">
+    <List.Item itemKey={itemKey} index={0} key={itemKey} gap="large">
       <Channel.AliasInput
         channelKey={yChannel}
         variant="shadow"
