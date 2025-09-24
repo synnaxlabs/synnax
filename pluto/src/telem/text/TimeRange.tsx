266f5c25cff1ce7e12846b09dd004cb02ea1a086
Patrick Dotson <--local>
2025-09-24 09:51:20 -0600

    Format and regenerate theme

--- pluto/src/telem/text/TimeRange.tsx
+++ pluto/src/telem/text/TimeRange.tsx
@@ -7,7 +7,6 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-
 import { type CrudeTimeRange, TimeRange, TimeSpan, TimeStamp } from "@synnaxlabs/x";
 import { type ReactElement } from "react";
 
