e5f9c3cf8669a40fd0c2c82a29fe197d261542f3
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-09-29 14:41:28 -0700

    [driver/opc] - fixed boolean writes

--- driver/opc/dev/server_extended.py
+++ driver/opc/dev/server_extended.py
@@ -68,6 +68,12 @@ async def main():
             idx, f"my_float_{i}", i, ua.VariantType.Float
         )
         await my_float.set_writable()
+    for i in range(5):
+        # add 30 float variables t OPC
+        my_float = await myobj.add_variable(
+            idx, f"my_bool_{i}", i, ua.VariantType.Boolean
+        )
+        await my_float.set_writable()
 
     i = 0
     start_ref = datetime.datetime.now(datetime.timezone.utc)
