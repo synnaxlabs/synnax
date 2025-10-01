52e22a48c6c6805286033c9d18c887fbeecd2059
Patrick Dotson <123601024+pjdotson@users.noreply.github.com>
2025-10-01 07:20:23 -0700

    SY-3040: Add copy properties context menu item to ontology toolbars (#1523)

--- console/src/services.tsx
+++ console/src/services.tsx
@@ -8,11 +8,10 @@
 // included in the file licenses/APL.txt.
 
 import { type ontology } from "@synnaxlabs/client";
+import { Icon } from "@synnaxlabs/pluto";
 
 import { ArcServices } from "@/arc/services";
 import { ChannelServices } from "@/channel/services";
-import { Node } from "@/cluster/node";
-import { ClusterServices } from "@/cluster/services";
 import { GroupServices } from "@/group/services";
 import { Hardware } from "@/hardware";
 import { LinePlotServices } from "@/lineplot/services";
@@ -24,33 +23,37 @@ import { TableServices } from "@/table/services";
 import { UserServices } from "@/user/services";
 import { WorkspaceServices } from "@/workspace/services";
 
-const createEmptyService = (type: ontology.ResourceType): Ontology.Service => ({
-  ...Ontology.NOOP_SERVICE,
-  type,
-});
+const createEmptyService = (
+  type: ontology.ResourceType,
+  icon?: Icon.ReactElement,
+): Ontology.Service => {
+  const service = { ...Ontology.NOOP_SERVICE, type };
+  if (icon != null) service.icon = icon;
+  return service;
+};
 
 export const SERVICES: Ontology.Services = {
   schematic: SchematicServices.ONTOLOGY_SERVICE,
-  schematic_symbol: createEmptyService("schematic_symbol"),
-  cluster: ClusterServices.ONTOLOGY_SERVICE,
+  schematic_symbol: createEmptyService("schematic_symbol", <Icon.Schematic />),
+  cluster: createEmptyService("cluster", <Icon.Cluster />),
   user: UserServices.ONTOLOGY_SERVICE,
   builtin: createEmptyService("builtin"),
-  node: Node.ONTOLOGY_SERVICE,
+  node: createEmptyService("node", <Icon.Node />),
   group: GroupServices.ONTOLOGY_SERVICE,
   range: RangeServices.ONTOLOGY_SERVICE,
   workspace: WorkspaceServices.ONTOLOGY_SERVICE,
   lineplot: LinePlotServices.ONTOLOGY_SERVICE,
   "range-alias": createEmptyService("range-alias"),
-  label: createEmptyService("label"),
+  label: createEmptyService("label", <Icon.Label />),
   rack: Hardware.Rack.ONTOLOGY_SERVICE,
   task: Hardware.Task.ONTOLOGY_SERVICE,
   device: Hardware.Device.ONTOLOGY_SERVICE,
   channel: ChannelServices.ONTOLOGY_SERVICE,
   framer: createEmptyService("framer"),
-  policy: createEmptyService("policy"),
+  policy: createEmptyService("policy", <Icon.Access />),
   allow_all: createEmptyService("allow_all"),
   log: LogServices.ONTOLOGY_SERVICE,
   table: TableServices.ONTOLOGY_SERVICE,
-  status: createEmptyService("status"),
+  status: createEmptyService("status", <Icon.Status />),
   arc: ArcServices.ONTOLOGY_SERVICE,
 };
