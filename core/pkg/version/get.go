6ce73c9718a63d3f9243391f03136002bbd57737
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-10-04 10:06:51 -0600

    [ops] - removed earthfiles, improved docker builds and version outputs

--- core/pkg/version/get.go
+++ core/pkg/version/get.go
@@ -11,6 +11,7 @@ package version
 
 import (
 	"embed"
+	"fmt"
 	"go.uber.org/zap"
 	"io"
 	"strings"
@@ -22,8 +23,24 @@ var fs embed.FS
 const unknown = "unknown"
 const errorMsg = "unexpected failure to resolve version"
 
+// These variables can be set at build time using -ldflags:
+// -X github.com/synnaxlabs/synnax/pkg/version.Version=1.0.0
+// -X github.com/synnaxlabs/synnax/pkg/version.GitCommit=abc123
+// -X github.com/synnaxlabs/synnax/pkg/version.BuildDate=2025-01-01T00:00:00Z
+var (
+	Version   string = ""
+	GitCommit string = ""
+	BuildDate string = ""
+)
+
 // Prod returns the production version of Synnax.
 func Prod() string {
+	// If version was injected at build time, use it
+	if Version != "" {
+		return Version
+	}
+
+	// Otherwise fall back to embedded VERSION file
 	f, err := fs.Open("VERSION")
 	if err != nil {
 		zap.S().Errorw(errorMsg, "error", err)
@@ -42,3 +59,33 @@ func Prod() string {
 
 // Get returns the production version of Synnax.
 func Get() string { return Prod() }
+
+// Commit returns the git commit hash.
+func Commit() string {
+	if GitCommit != "" {
+		return GitCommit
+	}
+	return unknown
+}
+
+// Date returns the build date.
+func Date() string {
+	if BuildDate != "" {
+		return BuildDate
+	}
+	return unknown
+}
+
+// Full returns the full version string with commit and build date.
+func Full() string {
+	v := Get()
+	commit := Commit()
+	date := Date()
+
+	if commit != unknown && date != unknown {
+		return fmt.Sprintf("%s (commit: %s, built: %s)", v, commit[:7], date)
+	} else if commit != unknown {
+		return fmt.Sprintf("%s (commit: %s)", v, commit[:7])
+	}
+	return v
+}
