e7277d2e07e88f88917727a27754acc4f3c8ad2d
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-10-07 06:37:56 -0600

    [console] - improved diagnostics for task dialog status changes

--- console/src/hardware/common/task/ChannelName.tsx
+++ console/src/hardware/common/task/ChannelName.tsx
@@ -7,9 +7,9 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-import { type channel } from "@synnaxlabs/client";
-import { Channel, Text } from "@synnaxlabs/pluto";
-import { type Optional, primitive } from "@synnaxlabs/x";
+import { type channel, NotFoundError } from "@synnaxlabs/client";
+import { Channel, Flex, Text, Tooltip } from "@synnaxlabs/pluto";
+import { location, type Optional, primitive, status, uuid } from "@synnaxlabs/x";
 import { useCallback, useEffect } from "react";
 
 import { CSS } from "@/css";
@@ -28,7 +28,7 @@ export const ChannelName = ({
   ...rest
 }: ChannelNameProps) => {
   const range = useSelectActiveRangeKey();
-  const { data, retrieve } = Channel.useRetrieveStateful();
+  const { data, retrieve, ...restResult } = Channel.useRetrieveStateful();
   useEffect(() => {
     if (primitive.isZero(channel)) return;
     retrieve({ key: channel, rangeKey: range ?? undefined });
@@ -39,10 +39,27 @@ export const ChannelName = ({
     (name: string) => update({ key: channel, name }),
     [channel, update],
   );
-  return (
+  let stat: Pick<status.Status, "variant" | "message" | "description"> =
+    restResult.status;
+  if (primitive.isZero(channel))
+    stat = { variant: "warning", message: "No channel selected" };
+  else if (
+    restResult.status.variant === "error" &&
+    NotFoundError.matches(restResult.status.details.error) &&
+    restResult.status.details.error.message.includes("Channel")
+  )
+    stat = {
+      variant: "error",
+      message: "Channel not found. Was it deleted?",
+      description:
+        "If it was deleted, a new channel will be created when the task is configured.",
+    };
+
+  const variant = status.removeVariants(stat.variant, ["success"]);
+  const text = (
     <Text.MaybeEditable
       className={CSS(className, CSS.BE("task", "channel-name"))}
-      status={primitive.isZero(channel) ? "warning" : undefined}
+      status={variant}
       level="small"
       value={name}
       onChange={handleRename}
@@ -50,5 +67,23 @@ export const ChannelName = ({
       overflow="ellipsis"
       {...rest}
     />
+  );
+
+  if (stat.variant !== "success" && stat.variant !== "error" && variant !== "warning")
+    return text;
+  return (
+    <Tooltip.Dialog location={location.CENTER_RIGHT}>
+      <Flex.Box y gap="small">
+        <Text.Text status={variant} level="p" color={10} weight={500}>
+          {stat.message}
+        </Text.Text>
+        {stat.description != null && (
+          <Text.Text level="small" color={9} weight={450}>
+            {stat.description}
+          </Text.Text>
+        )}
+      </Flex.Box>
+      {text}
+    </Tooltip.Dialog>
   );
 };
