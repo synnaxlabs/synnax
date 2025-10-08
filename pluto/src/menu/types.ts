bff7020eaebcadd5f744d350232d9368f1b5e66f
Patrick Dotson <patrick@synnaxlabs.com>
2025-10-08 16:43:41 -0600

    Final circular dependency fixes

--- pluto/src/menu/types.ts
+++ pluto/src/menu/types.ts
@@ -7,9 +7,8 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-export * from "@/status/core/Aggregator";
-export * from "@/status/core/colors";
-export * from "@/status/core/CreateIcon";
-export * from "@/status/core/Indicator";
-export * from "@/status/core/Notification";
-export * from "@/status/core/Summary";
+import { CSS } from "@/css";
+
+export const CONTEXT_SELECTED = CSS.BM("context", "selected");
+export const CONTEXT_TARGET = CSS.BE("context", "target");
+export const CONTEXT_MENU_CLASS = CSS.B("menu-context");
