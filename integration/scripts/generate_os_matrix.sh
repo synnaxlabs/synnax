83ad5c84b03e717fd47ed266d3aafd1e5270d1fa
nico-synnax <nico@synnaxlabs.com>
2025-10-08 18:56:50 -0500

    SY-3114: Formatting

--- integration/scripts/generate_os_matrix.sh
+++ integration/scripts/generate_os_matrix.sh
@@ -8,17 +8,17 @@ BUILD_MACOS=$3
 OS_LIST=""
 
 if [ "$BUILD_UBUNTU" = "true" ]; then
-  OS_LIST="\"ubuntu-latest\""
+    OS_LIST="\"ubuntu-latest\""
 fi
 
 if [ "$BUILD_WINDOWS" = "true" ]; then
-  [ -n "$OS_LIST" ] && OS_LIST="$OS_LIST,"
-  OS_LIST="${OS_LIST}\"windows-latest\""
+    [ -n "$OS_LIST" ] && OS_LIST="$OS_LIST,"
+    OS_LIST="${OS_LIST}\"windows-latest\""
 fi
 
 if [ "$BUILD_MACOS" = "true" ]; then
-  [ -n "$OS_LIST" ] && OS_LIST="$OS_LIST,"
-  OS_LIST="${OS_LIST}\"macos-latest\""
+    [ -n "$OS_LIST" ] && OS_LIST="$OS_LIST,"
+    OS_LIST="${OS_LIST}\"macos-latest\""
 fi
 
 echo "matrix=[${OS_LIST}]"
