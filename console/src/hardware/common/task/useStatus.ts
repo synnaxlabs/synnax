d6b0ddaafbc43b297338d2b3a19a3d6bde70f120
Patrick Dotson <patrick@synnaxlabs.com>
2025-10-08 16:46:03 -0600

    Remove duplicate copyright header

--- console/src/hardware/common/task/useStatus.ts
+++ console/src/hardware/common/task/useStatus.ts
@@ -7,15 +7,6 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-// Copyright 2025 Synnax Labs, Inc.
-//
-// Use of this software is governed by the Business Source License included in the file
-// licenses/BSL.txt.
-//
-// As of the Change Date specified in that file, in accordance with the Business Source
-// License, use of this software will be governed by the Apache License, Version 2.0,
-// included in the file licenses/APL.txt.
-
 import { type task } from "@synnaxlabs/client";
 import { Form } from "@synnaxlabs/pluto";
 import { id, TimeStamp } from "@synnaxlabs/x";
