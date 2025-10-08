95d94c0c8b8ff969d77b5a45c37a0157454a176e
nico-synnax <nico@synnaxlabs.com>
2025-10-08 18:49:17 -0500

    SY-3114: Add selection OS and binary selection logic

---
+++ integration/scripts/generate_os_matrix.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Generate OS matrix for GitHub Actions workflow based on input flags
+
+BUILD_UBUNTU=$1
+BUILD_WINDOWS=$2
+BUILD_MACOS=$3
+
+OS_LIST=""
+
+if [ "$BUILD_UBUNTU" = "true" ]; then
+  OS_LIST="\"ubuntu-latest\""
+fi
+
+if [ "$BUILD_WINDOWS" = "true" ]; then
+  [ -n "$OS_LIST" ] && OS_LIST="$OS_LIST,"
+  OS_LIST="${OS_LIST}\"windows-latest\""
+fi
+
+if [ "$BUILD_MACOS" = "true" ]; then
+  [ -n "$OS_LIST" ] && OS_LIST="$OS_LIST,"
+  OS_LIST="${OS_LIST}\"macos-latest\""
+fi
+
+echo "matrix=[${OS_LIST}]"
