e8672071e3911ae9baaf48e36fbed72cadf345f6
nico-synnax <nico@synnaxlabs.com>
2025-10-14 14:12:22 -0500

    SY-3024: Feedback

--- docs/site/src/pages/reference/device-drivers/modbus/_nav.ts
+++ docs/site/src/pages/reference/device-drivers/modbus/_nav.ts
@@ -11,22 +11,22 @@ import { type PageNavNode } from "@/components/PageNav/PageNav";
 
 export const MODBUS_NAV: PageNavNode = {
   key: "modbus",
-  name: "Modbus TCP",
+  name: "Modbus",
   children: [
     {
-      key: "/reference/device-drivers/modbus/connect-device",
-      href: "/reference/device-drivers/modbus/connect-device",
+      key: "/reference/device-drivers/modbus/connect-server",
+      href: "/reference/device-drivers/modbus/connect-server",
       name: "Connect to a Server",
     },
     {
       key: "/reference/device-drivers/modbus/read-task",
       href: "/reference/device-drivers/modbus/read-task",
-      name: "Configure a Read Task",
+      name: "Read Task",
     },
     {
       key: "/reference/device-drivers/modbus/write-task",
       href: "/reference/device-drivers/modbus/write-task",
-      name: "Configure a Write Task",
+      name: "Write Task",
     },
   ],
 };
