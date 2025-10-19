450264d719662dc0dc79fe78a6bf93f821a32369
Patrick Dotson <patrick@synnaxlabs.com>
2025-10-19 12:42:11 -0600

    Add showBottomDivider prop to ContextMenu.Item

--- pluto/src/context-menu/Item.tsx
+++ pluto/src/context-menu/Item.tsx
@@ -8,18 +8,24 @@
 // included in the file licenses/APL.txt.
 
 import { Button } from "@/button";
+import { Divider } from "@/context-menu/Divider";
 
 export interface ItemProps
   extends Omit<Button.ButtonProps, "children" | "onClick">,
-    Required<Pick<Button.ButtonProps, "children" | "onClick">> {}
+    Required<Pick<Button.ButtonProps, "children" | "onClick">> {
+  showBottomDivider?: boolean;
+}
 
-export const Item = (props: ItemProps) => (
-  <Button.Button
-    level="small"
-    overflow="nowrap"
-    variant="text"
-    background={1}
-    gap="small"
-    {...props}
-  />
+export const Item = ({ showBottomDivider = false, ...props }: ItemProps) => (
+  <>
+    <Button.Button
+      level="small"
+      overflow="nowrap"
+      variant="text"
+      background={1}
+      gap="small"
+      {...props}
+    />
+    {showBottomDivider && <Divider />}
+  </>
 );
