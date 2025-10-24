6ceb46c3d7d0515f40504214a7f4187541404b5e
nico-synnax <nico@synnaxlabs.com>
2025-10-24 10:40:26 -0500

    SY-3151: Remove curl tests

--- driver/errors/errors_test.cpp
+++ driver/errors/errors_test.cpp
@@ -10,7 +10,6 @@
 #include <vector>
 
 #include "gtest/gtest.h"
-#include <curl/curl.h>
 
 #include "x/cpp/xtest/xtest.h"
 
@@ -41,46 +40,6 @@ TEST(ErrorsTest, URLsAreWellFormed) {
     for (const auto &lib: ALL_LIBS) {
         EXPECT_TRUE(is_valid_url(lib.url)) << "URL is not well-formed for " << lib.name;
     }
-}
-
-size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp) {
-    return size * nmemb;
-}
-
-TEST(ErrorsTest, URLsAreReachable) {
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-
-    for (const auto &lib: ALL_LIBS) {
-        CURL *curl = curl_easy_init();
-        ASSERT_NE(curl, nullptr) << "Failed to initialize curl for " << lib.name;
-
-        curl_easy_setopt(curl, CURLOPT_URL, lib.url.c_str());
-        curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-
-        CURLcode res = curl_easy_perform(curl);
-
-        long response_code = 0;
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-
-        curl_easy_cleanup(curl);
-
-        EXPECT_EQ(res, CURLE_OK) << "Failed to reach " << lib.name << " at " << lib.url
-                                 << " (curl error: " << curl_easy_strerror(res) << ")";
-
-        if (res == CURLE_OK) {
-            EXPECT_NE(response_code, 404)
-                << lib.name << " URL returned 404: " << lib.url;
-            EXPECT_GE(response_code, 200)
-                << lib.name << " URL returned error code: " << response_code;
-            EXPECT_LT(response_code, 400)
-                << lib.name << " URL returned error code: " << response_code;
-        }
-    }
-
-    curl_global_cleanup();
 }
 
 TEST(ErrorsTest, LibraryInfoWithoutURL) {
