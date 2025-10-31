1735a667817429ded6ed8f097cee15c572e3d156
nico-synnax <nico@synnaxlabs.com>
2025-10-31 10:57:31 -0500

    SY-3205: assert_nil_p

--- x/cpp/xtest/xtest_test.cpp
+++ x/cpp/xtest/xtest_test.cpp
@@ -107,8 +107,9 @@ TEST_F(XTestTest, TestMustSucceedSuccess) {
     auto successful_op = []() -> std::pair<int, xerrors::Error> {
         return {42, xerrors::NIL};
     };
-    auto result = ASSERT_NIL_P(successful_op());
-    EXPECT_EQ(result, 42);
+    auto [value, err] = successful_op();
+    ASSERT_FALSE(err) << "Expected operation to succeed, but got error: " << err;
+    EXPECT_EQ(value, 42);
 }
 
 TEST_F(XTestTest, TestEventuallyTrue) {
