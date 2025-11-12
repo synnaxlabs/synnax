60c9a27d2b4667a16578006fd3dfa592c9741c5c
nico-synnax <nico@synnaxlabs.com>
2025-11-11 19:26:30 -0600

    [log] formatting

--- driver/modbus/mock/slave.h
+++ driver/modbus/mock/slave.h
@@ -138,8 +138,10 @@ class Slave {
         for (const auto &[addr, value]: config_.holding_registers)
             if (addr < nb_registers) {
                 auto dt = telem::DataType::infer(value);
-                LOG(INFO) << "Holding register[" << addr << "]: inferred type=" << dt.name()
-                          << " density=" << dt.density() << " dest_size=" << ((dt.density() + 1) / 2);
+                LOG(INFO) << "Holding register[" << addr
+                          << "]: inferred type=" << dt.name()
+                          << " density=" << dt.density()
+                          << " dest_size=" << ((dt.density() + 1) / 2);
                 std::vector<uint16_t> dest((dt.density() + 1) / 2);
                 if (const auto err = util::format_register(
                         value,
@@ -156,8 +158,10 @@ class Slave {
         for (const auto &[addr, value]: config_.input_registers)
             if (addr < nb_input_registers) {
                 auto dt = telem::DataType::infer(value);
-                LOG(INFO) << "Input register[" << addr << "]: inferred type=" << dt.name()
-                          << " density=" << dt.density() << " dest_size=" << ((dt.density() + 1) / 2);
+                LOG(INFO) << "Input register[" << addr
+                          << "]: inferred type=" << dt.name()
+                          << " density=" << dt.density()
+                          << " dest_size=" << ((dt.density() + 1) / 2);
                 std::vector<uint16_t> dest((dt.density() + 1) / 2);
                 if (const auto err = util::format_register(
                         value,
