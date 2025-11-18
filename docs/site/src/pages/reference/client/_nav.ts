529ef1a7903f373e7772011e018d0a143e1a932f
nico-synnax <nico@synnaxlabs.com>
2025-11-18 00:13:36 -0600

    [DRAFT] - Client docs structure

--- docs/site/src/pages/reference/client/_nav.ts
+++ docs/site/src/pages/reference/client/_nav.ts
@@ -14,24 +14,93 @@ export const CLIENT_NAV: PageNavNode = {
   name: "Client",
   children: [
     {
-      key: "/reference/client/get-started",
-      href: "/reference/client/get-started",
-      name: "Get Started",
+      key: "/reference/client/quick-start",
+      href: "/reference/client/quick-start",
+      name: "Quick Start",
     },
     {
-      key: "/reference/client/channels",
-      href: "/reference/client/channels",
-      name: "Channels",
+      key: "/reference/client/complete-setup",
+      href: "/reference/client/complete-setup",
+      name: "Complete Setup",
     },
     {
-      key: "/reference/client/read-data",
-      href: "/reference/client/read-data",
-      name: "Read Data",
+      key: "fundamentals",
+      name: "Fundamentals",
+      children: [
+        {
+          key: "/reference/client/fundamentals/channels",
+          href: "/reference/client/fundamentals/channels",
+          name: "Channels",
+        },
+        {
+          key: "/reference/client/fundamentals/read-data",
+          href: "/reference/client/fundamentals/read-data",
+          name: "Reading Data",
+        },
+        {
+          key: "/reference/client/fundamentals/write-data",
+          href: "/reference/client/fundamentals/write-data",
+          name: "Writing Data",
+        },
+      ],
     },
     {
-      key: "/reference/client/write-data",
-      href: "/reference/client/write-data",
-      name: "Write Data",
+      key: "working-with-data",
+      name: "Working with Data",
+      children: [
+        {
+          key: "/reference/client/working-with-data/series-and-frames",
+          href: "/reference/client/working-with-data/series-and-frames",
+          name: "Series & Frames",
+        },
+        {
+          key: "/reference/client/working-with-data/ranges",
+          href: "/reference/client/working-with-data/ranges",
+          name: "Ranges",
+        },
+        {
+          key: "/reference/client/working-with-data/streaming-data",
+          href: "/reference/client/working-with-data/streaming-data",
+          name: "Streaming Data",
+        },
+        {
+          key: "/reference/client/working-with-data/iterators",
+          href: "/reference/client/working-with-data/iterators",
+          name: "Iterators",
+        },
+      ],
+    },
+    {
+      key: "advanced",
+      name: "Advanced Topics",
+      children: [
+        {
+          key: "/reference/client/advanced/writers",
+          href: "/reference/client/advanced/writers",
+          name: "Writers",
+        },
+        {
+          key: "/reference/client/advanced/delete-data",
+          href: "/reference/client/advanced/delete-data",
+          name: "Delete Data",
+        },
+        {
+          key: "/reference/client/advanced/timestamps",
+          href: "/reference/client/advanced/timestamps",
+          name: "Timestamps",
+        },
+      ],
+    },
+    {
+      key: "resources",
+      name: "Resources",
+      children: [
+        {
+          key: "/reference/client/resources/build-device-driver",
+          href: "/reference/client/resources/build-device-driver",
+          name: "Build a Device Driver",
+        },
+      ],
     },
   ],
 };
