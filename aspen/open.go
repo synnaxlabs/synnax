4f85c599c771a6cc1c1618d8cfbf8df409a1726d
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-11-20 11:11:15 -0700

    SY-3309 - X/Go - Fixed Pebble Logging Event Listener (#1672)

--- aspen/open.go
+++ aspen/open.go
@@ -95,7 +95,12 @@ func configureTransport(ctx context.Context, o *options) (io.Closer, error) {
 }
 
 func openKV(o *options) (xkv.DB, error) {
-	pebbleDB, err := pebble.Open(o.dirname, &pebble.Options{FS: o.fs})
+	var (
+		log           = pebblekv.NewLogger(o.Child("kv"))
+		ev            = pebble.MakeLoggingEventListener(log)
+		opts          = &pebble.Options{FS: o.fs, Logger: log, EventListener: &ev}
+		pebbleDB, err = pebble.Open(o.dirname, opts)
+	)
 	if err != nil {
 		return nil, err
 	}
