72318338f2995584c665cf2b83f63086a5ba8cf2
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-11-21 13:15:20 -0700

    [console] - implemented policy and role access control mechanisms

--- console/src/access/role/ontology.tsx
+++ console/src/access/role/ontology.tsx
@@ -7,13 +7,62 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-import { Icon } from "@synnaxlabs/pluto";
+import { access } from "@synnaxlabs/client";
+import { Access, Icon, Menu as PMenu } from "@synnaxlabs/pluto";
 
+import { Menu } from "@/components";
 import { Ontology } from "@/ontology";
+import { createUseDelete } from "@/ontology/createUseDelete";
+import { createUseRename } from "@/ontology/createUseRename";
+
+const useDelete = createUseDelete({
+  type: "Role",
+  query: Access.Role.useDelete,
+  convertKey: String,
+});
+
+const useRename = createUseRename({
+  query: Access.Role.useRename,
+  ontologyID: access.role.ontologyID,
+  convertKey: String,
+});
+
+const TreeContextMenu: Ontology.TreeContextMenu = (props) => {
+  const {
+    selection: { ids },
+  } = props;
+  const handleDelete = useDelete(props);
+  const handleRename = useRename(props);
+  const handleSelect = {
+    rename: handleRename,
+    delete: handleDelete,
+  };
+  const singleResource = ids.length === 1;
+  return (
+    <PMenu.Menu onChange={handleSelect} level="small" gap="small">
+      {singleResource && (
+        <>
+          <Menu.RenameItem />
+          <PMenu.Divider />
+        </>
+      )}
+      <Menu.DeleteItem />
+      <PMenu.Divider />
+      {singleResource && (
+        <>
+          <Ontology.CopyMenuItem {...props} />
+          <PMenu.Divider />
+        </>
+      )}
+      <Menu.ReloadConsoleItem />
+    </PMenu.Menu>
+  );
+};
 
 export const ONTOLOGY_SERVICE: Ontology.Service = {
   ...Ontology.NOOP_SERVICE,
   type: "role",
   icon: <Icon.Role />,
+  TreeContextMenu,
   hasChildren: true,
 };
