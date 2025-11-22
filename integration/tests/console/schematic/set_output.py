7a570ef0c92019723b9fbbf1142542ba77d8814f
sy-nico <nico@synnaxlabs.com>
2025-11-21 20:07:02 -0600

    SY-3078: Console Integration Test Improvements (#1681)
    * SY-3078: Schematic Control Tests & Page Refactor (#1676)
    
    * Added new tests (in edit_props.py) for Schematic properties and show/hide legend

--- integration/tests/console/schematic/set_output.py
+++ integration/tests/console/schematic/set_output.py
@@ -10,6 +10,7 @@
 import synnax as sy
 
 from console.case import ConsoleCase
+from console.schematic.schematic import Schematic
 
 
 class SetOutput(ConsoleCase):
@@ -19,50 +20,42 @@ class SetOutput(ConsoleCase):
 
     def run(self) -> None:
         console = self.console
-        console.schematic.new()
+        client = self.client
 
         self.log("Creating channels")
         CHANNEL_NAME = "command_channel"
         INDEX_NAME = "idx_channel"
 
-        console.channels.create(
+        index_ch = client.channels.create(
             name=INDEX_NAME,
             is_index=True,
+            retrieve_if_name_exists=True,
         )
-        console.channels.create(
+        cmd_ch = client.channels.create(
             name=CHANNEL_NAME,
             data_type=sy.DataType.FLOAT64,
             is_index=False,
-            index=INDEX_NAME,
+            index=index_ch.key,
+            retrieve_if_name_exists=True,
         )
 
         self.log("Creating schematic symbols")
-        setpoint_symbol = console.schematic.create_setpoint(CHANNEL_NAME)
+        schematic = Schematic(client, console, "set_output_schematic")
+
+        setpoint_symbol = schematic.create_setpoint(CHANNEL_NAME)
         setpoint_symbol.move(-200, 0)
 
-        value_symbol = console.schematic.create_value(CHANNEL_NAME)
+        value_symbol = schematic.create_value(CHANNEL_NAME)
         value_symbol.move(200, 0)
 
-        console.schematic.connect_symbols(
-            setpoint_symbol, "right", value_symbol, "left"
-        )
+        schematic.connect_symbols(setpoint_symbol, "right", value_symbol, "left")
 
         set_p_value = 47.23
         self.log(f"Verifying setpoint value: {set_p_value}")
-        setpoint_symbol.set_value(set_p_value)
-        actual_value = self.get_value(CHANNEL_NAME)
-
-        assert (
-            actual_value == set_p_value
-        ), f"Setpoint value mismatch!\nActual: {actual_value}\nExpected: {set_p_value}"
+        schematic.assert_setpoint(setpoint_symbol, CHANNEL_NAME, set_p_value)
 
         set_p_value = 1.0101
         self.log(f"Verifying setpoint value: {set_p_value}")
-        setpoint_symbol.set_value(set_p_value)
-        actual_value = self.get_value(CHANNEL_NAME)
+        schematic.assert_setpoint(setpoint_symbol, CHANNEL_NAME, set_p_value)
 
-        assert (
-            actual_value == set_p_value
-        ), f"Setpoint value mismatch!\nActual: {actual_value}\nExpected: {set_p_value}"
-
-        console.schematic.screenshot()
+        schematic.screenshot()
