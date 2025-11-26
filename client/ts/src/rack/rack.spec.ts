24bcc8b8d49a62f719a9d57d635671b1e13da6b6
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-11-26 11:42:44 -0700

    [pluto] - addressed pull request comments

--- client/ts/src/rack/rack.spec.ts
+++ client/ts/src/rack/rack.spec.ts
@@ -7,6 +7,7 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
+import { TimeStamp } from "@synnaxlabs/x";
 import { describe, expect, it } from "vitest";
 import { ZodError } from "zod";
 
@@ -46,7 +47,7 @@ describe("Rack", () => {
       expect(retrieved.name).toBe("test");
     });
     it("should retrieve a rack by its name", async () => {
-      const name = `TimeStamp.now().toString()}-${Math.random()}`;
+      const name = `${TimeStamp.now().toString()}-${Math.random()}`;
       const r = await client.racks.create({ name });
       const retrieved = await client.racks.retrieve({ name });
       expect(retrieved.key).toBe(r.key);
