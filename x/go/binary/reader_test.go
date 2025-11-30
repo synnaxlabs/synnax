5ba181369d1ba2b897463cccaa1341eb28442080
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-11-29 23:39:47 -0700

    [x/go] - cleaned up binary reader tests

--- x/go/binary/reader_test.go
+++ x/go/binary/reader_test.go
@@ -17,100 +17,62 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	xbinary "github.com/synnaxlabs/x/binary"
+	. "github.com/synnaxlabs/x/testutil"
 )
 
 var _ = Describe("Reader", func() {
 	It("Should correctly read values from the buffer", func() {
-		// Write data using Writer, then read it back with Reader
 		w := xbinary.NewWriter(13, binary.LittleEndian)
 		w.Uint8(1)
 		w.Uint32(256)
 		w.Uint64(1024)
-
 		r := xbinary.NewReader(bytes.NewReader(w.Bytes()), binary.LittleEndian)
-		v8, err := r.Uint8()
-		Expect(err).To(BeNil())
-		Expect(v8).To(Equal(uint8(1)))
-		v32, err := r.Uint32()
-		Expect(err).To(BeNil())
-		Expect(v32).To(Equal(uint32(256)))
-		v64, err := r.Uint64()
-		Expect(err).To(BeNil())
-		Expect(v64).To(Equal(uint64(1024)))
+		Expect(MustSucceed(r.Uint8())).To(Equal(uint8(1)))
+		Expect(MustSucceed(r.Uint32())).To(Equal(uint32(256)))
+		Expect(MustSucceed(r.Uint64())).To(Equal(uint64(1024)))
 	})
-
 	It("Should return error on EOF", func() {
 		r := xbinary.NewReader(bytes.NewReader([]byte{1, 2}), binary.LittleEndian)
-		v8, err := r.Uint8()
-		Expect(err).To(BeNil())
-		Expect(v8).To(Equal(uint8(1)))
-		// Try to read uint32 with only 1 byte remaining
-		_, err = r.Uint32()
+		Expect(MustSucceed(r.Uint8())).To(Equal(uint8(1)))
+		_, err := r.Uint32()
 		Expect(err).To(MatchError(io.ErrUnexpectedEOF))
 	})
-
 	It("Should return EOF when no data remains", func() {
 		r := xbinary.NewReader(bytes.NewReader([]byte{1}), binary.LittleEndian)
-		v8, err := r.Uint8()
-		Expect(err).To(BeNil())
-		Expect(v8).To(Equal(uint8(1)))
-		// Now EOF
-		_, err = r.Uint8()
+		Expect(MustSucceed(r.Uint8())).To(Equal(uint8(1)))
+		_, err := r.Uint8()
 		Expect(err).To(MatchError(io.EOF))
 	})
-
 	It("Should read arbitrary bytes", func() {
 		data := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 		r := xbinary.NewReader(bytes.NewReader(data), binary.LittleEndian)
 		buf := make([]byte, 4)
-		n, err := r.Read(buf)
-		Expect(err).To(BeNil())
-		Expect(n).To(Equal(4))
+		Expect(MustSucceed(r.Read(buf))).To(Equal(4))
 		Expect(buf).To(Equal([]byte{1, 2, 3, 4}))
-		n, err = r.Read(buf)
-		Expect(err).To(BeNil())
-		Expect(n).To(Equal(4))
+		Expect(MustSucceed(r.Read(buf))).To(Equal(4))
 		Expect(buf).To(Equal([]byte{5, 6, 7, 8}))
 	})
-
 	It("Should work with big-endian data", func() {
 		w := xbinary.NewWriter(13, binary.BigEndian)
 		w.Uint8(1)
 		w.Uint32(256)
 		w.Uint64(1024)
-
 		r := xbinary.NewReader(bytes.NewReader(w.Bytes()), binary.BigEndian)
-		v8, err := r.Uint8()
-		Expect(err).To(BeNil())
-		Expect(v8).To(Equal(uint8(1)))
-		v32, err := r.Uint32()
-		Expect(err).To(BeNil())
-		Expect(v32).To(Equal(uint32(256)))
-		v64, err := r.Uint64()
-		Expect(err).To(BeNil())
-		Expect(v64).To(Equal(uint64(1024)))
+		Expect(MustSucceed(r.Uint8())).To(Equal(uint8(1)))
+		Expect(MustSucceed(r.Uint32())).To(Equal(uint32(256)))
+		Expect(MustSucceed(r.Uint64())).To(Equal(uint64(1024)))
 	})
-
 	Describe("Reset", func() {
 		It("Should reset to use a new reader", func() {
 			r := xbinary.NewReader(bytes.NewReader([]byte{1}), binary.LittleEndian)
-			v8, err := r.Uint8()
-			Expect(err).To(BeNil())
-			Expect(v8).To(Equal(uint8(1)))
-			_, err = r.Uint8() // EOF
+			Expect(MustSucceed(r.Uint8())).To(Equal(uint8(1)))
+			_, err := r.Uint8()
 			Expect(err).To(MatchError(io.EOF))
-
-			// Reset with new data (little-endian: uint8(42), uint32(1) = {1, 0, 0, 0})
 			r.Reset(bytes.NewReader([]byte{42, 1, 0, 0, 0}))
-			v8, err = r.Uint8()
-			Expect(err).To(BeNil())
-			Expect(v8).To(Equal(uint8(42)))
-			v32, err := r.Uint32()
-			Expect(err).To(BeNil())
-			Expect(v32).To(Equal(uint32(1)))
+			Expect(MustSucceed(r.Uint8())).To(Equal(uint8(42)))
+			Expect(MustSucceed(r.Uint32())).To(Equal(uint32(1)))
 		})
 	})
-
 	Describe("Round-trip with Writer", func() {
 		It("Should correctly round-trip complex data", func() {
 			w := xbinary.NewWriter(100, binary.LittleEndian)
@@ -119,24 +81,14 @@ var _ = Describe("Reader", func() {
 			w.Uint64(0x123456789ABCDEF0)
 			w.Write([]byte("hello"))
 			w.Uint32(42)
-
 			r := xbinary.NewReader(bytes.NewReader(w.Bytes()), binary.LittleEndian)
-			v8, err := r.Uint8()
-			Expect(err).To(BeNil())
-			Expect(v8).To(Equal(uint8(255)))
-			v32, err := r.Uint32()
-			Expect(err).To(BeNil())
-			Expect(v32).To(Equal(uint32(0xDEADBEEF)))
-			v64, err := r.Uint64()
-			Expect(err).To(BeNil())
-			Expect(v64).To(Equal(uint64(0x123456789ABCDEF0)))
+			Expect(MustSucceed(r.Uint8())).To(Equal(uint8(255)))
+			Expect(MustSucceed(r.Uint32())).To(Equal(uint32(0xDEADBEEF)))
+			Expect(MustSucceed(r.Uint64())).To(Equal(uint64(0x123456789ABCDEF0)))
 			buf := make([]byte, 5)
-			_, err = r.Read(buf)
-			Expect(err).To(BeNil())
+			MustSucceed(r.Read(buf))
 			Expect(string(buf)).To(Equal("hello"))
-			v32, err = r.Uint32()
-			Expect(err).To(BeNil())
-			Expect(v32).To(Equal(uint32(42)))
+			Expect(MustSucceed(r.Uint32())).To(Equal(uint32(42)))
 		})
 	})
 })
