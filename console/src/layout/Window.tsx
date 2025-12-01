d5a812ee3d786e2e61a89492a4c1c243a6ec75ce
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-12-01 12:23:37 -0700

    [console] - removed debug log

--- console/src/layout/Window.tsx
+++ console/src/layout/Window.tsx
@@ -39,7 +39,6 @@ const WindowInternal = (): ReactElement | null => {
   const dispatch = useDispatch();
   useEffect(() => {
     if (layout?.key == null) return;
-    console.log("setting window props", layout?.key);
     dispatch(
       setWindowProps({
         key: layout?.key,
