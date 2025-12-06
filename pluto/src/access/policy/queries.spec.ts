8f02e6795776396240e41cf7abb06794de7a4110
Patrick Dotson <patrick@synnaxlabs.com>
2025-12-06 15:37:54 -0700

    Fix renaming of all policy

--- pluto/src/access/policy/queries.spec.ts
+++ pluto/src/access/policy/queries.spec.ts
@@ -77,7 +77,7 @@ describe("queries", () => {
       const newPolicy = await client.access.policies.create({
         name: "newPolicy",
         objects: [{ type: "channel", key: "new" }],
-        actions: ["*"],
+        actions: ["all"],
       });
 
       await waitFor(() => {
