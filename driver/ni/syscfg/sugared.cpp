7101d003153bd0835104bac8b38d0518ba8500c2
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-12-08 14:22:18 -0700

    [driver] - clang format

--- driver/ni/syscfg/sugared.cpp
+++ driver/ni/syscfg/sugared.cpp
@@ -7,9 +7,8 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-#include "driver/ni/syscfg/sugared.h"
-
 #include "driver/ni/errors.h"
+#include "driver/ni/syscfg/sugared.h"
 
 namespace syscfg {
 xerrors::Error SugaredAPI::process_error(NISysCfgStatus status) const {
