9426c04ddfe5f8e3858a5f3a3d132acf4b00493e
Patrick Dotson <patrick@synnaxlabs.com>
2025-12-17 15:46:45 -0700

    Fix channel deletion limit issue

--- core/pkg/distribution/channel/lease_proxy.go
+++ core/pkg/distribution/channel/lease_proxy.go
@@ -640,12 +640,15 @@ func (lp *leaseProxy) deleteGateway(ctx context.Context, tx gorp.Tx, keys Keys) 
 	if err := lp.maybeDeleteResources(ctx, tx, keys); err != nil {
 		return err
 	}
+	// It's very important that this goes last, as it's the only operation that can fail
+	// without an atomic guarantee.
+	if err := lp.TSChannel.DeleteChannels(keys.Storage()); err != nil {
+		return err
+	}
 	lp.mu.Lock()
 	lp.mu.externalNonVirtualSet.Remove(keys...)
 	lp.mu.Unlock()
-	// It's very important that this goes last, as it's the only operation that can fail
-	// without an atomic guarantee.
-	return lp.TSChannel.DeleteChannels(keys.Storage())
+	return nil
 }
 
 func (lp *leaseProxy) maybeDeleteResources(
