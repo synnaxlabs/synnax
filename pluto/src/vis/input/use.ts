50076d9dc9019e59fa1041e3783a5f8662816077
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2025-12-23 14:33:41 -0600

    SY-3440: Implement Method Invoking in Aether (#1776)

--- pluto/src/vis/input/use.ts
+++ pluto/src/vis/input/use.ts
@@ -7,10 +7,7 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-import { useCallback, useEffect } from "react";
-
 import { Aether } from "@/aether";
-import { useMemoDeepEqual } from "@/memo";
 import { input } from "@/vis/input/aether";
 
 export interface UseProps extends Pick<input.State, "sink"> {
@@ -22,27 +19,13 @@ export interface UseReturn {
 }
 
 export const use = ({ aetherKey, sink }: UseProps): UseReturn => {
-  const memoProps = useMemoDeepEqual({ sink });
-  const [, , setState] = Aether.use({
+  const { methods } = Aether.useUnidirectional({
     aetherKey,
     type: input.Input.TYPE,
     schema: input.stateZ,
-    initialState: { trigger: 0, ...memoProps },
+    state: { sink },
+    methods: input.methodsZ,
   });
 
-  useEffect(() => {
-    setState((state) => ({ ...state, ...memoProps }));
-  }, [memoProps, setState]);
-
-  const handleSet = useCallback(
-    (value: string) =>
-      setState((state) => ({
-        ...state,
-        trigger: state.trigger + 1,
-        command: value,
-      })),
-    [setState],
-  );
-
-  return { set: handleSet };
+  return { set: methods.set };
 };
