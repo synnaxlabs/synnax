9caeeeccf4082d32dd0739679d234a5c90f95e97
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2026-01-19 14:16:06 -0700

    [ops] - updated embedded assets location

--- driver/scripts/install_embedded.sh
+++ driver/scripts/install_embedded.sh
@@ -1,2 +1,2 @@
 bazel build -c opt --config=hide_symbols //driver:driver
-mv ./bazel-bin/driver/driver ./core/pkg/driver/cpp/assets/driver
+mv ./bazel-bin/driver/driver ./core/pkg/driver/assets/driver
