d206a9524b5fec0f38a16cee0fef72391ce85411
Patrick Dotson <123601024+pjdotson@users.noreply.github.com>
2026-01-19 16:36:50 -0700

    SY-3039:  Fix ontology relationships of node metrics channels (#1828)
    Fix ontology relationships with node metrics channels by creating a metrics group to group all channels within.

--- core/pkg/service/metrics/metrics_suite_test.go
+++ core/pkg/service/metrics/metrics_suite_test.go
@@ -10,13 +10,72 @@
 package metrics_test
 
 import (
+	"context"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/synnaxlabs/synnax/pkg/distribution/mock"
+	"github.com/synnaxlabs/synnax/pkg/service/arc"
+	"github.com/synnaxlabs/synnax/pkg/service/framer"
+	"github.com/synnaxlabs/synnax/pkg/service/label"
+	"github.com/synnaxlabs/synnax/pkg/service/status"
+	. "github.com/synnaxlabs/x/testutil"
+)
+
+var (
+	builder   *mock.Cluster
+	dist      mock.Node
+	framerSvc *framer.Service
 )
 
 func TestMetrics(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Metrics Suite")
 }
+
+var _ = BeforeSuite(func() {
+	builder = mock.NewCluster()
+	ctx := context.Background()
+	dist = builder.Provision(ctx)
+	labelSvc := MustSucceed(label.OpenService(ctx, label.ServiceConfig{
+		DB:       dist.DB,
+		Ontology: dist.Ontology,
+		Group:    dist.Group,
+		Signals:  dist.Signals,
+	}))
+	statusSvc := MustSucceed(status.OpenService(ctx, status.ServiceConfig{
+		DB:       dist.DB,
+		Label:    labelSvc,
+		Ontology: dist.Ontology,
+		Group:    dist.Group,
+		Signals:  dist.Signals,
+	}))
+	arcSvc := MustSucceed(arc.OpenService(ctx, arc.ServiceConfig{
+		Channel:  dist.Channel,
+		Ontology: dist.Ontology,
+		DB:       dist.DB,
+		Framer:   dist.Framer,
+		Status:   statusSvc,
+		Signals:  dist.Signals,
+	}))
+	statusSvc = MustSucceed(status.OpenService(ctx, status.ServiceConfig{
+		DB:       dist.DB,
+		Label:    labelSvc,
+		Ontology: dist.Ontology,
+		Group:    dist.Group,
+		Signals:  dist.Signals,
+	}))
+	framerSvc = MustSucceed(framer.OpenService(ctx, framer.ServiceConfig{
+		Framer:  dist.Framer,
+		Channel: dist.Channel,
+		Arc:     arcSvc,
+		Status:  statusSvc,
+		DB:      dist.DB,
+	}))
+})
+
+var _ = AfterSuite(func() {
+	Expect(framerSvc.Close()).To(Succeed())
+	Expect(builder.Close()).To(Succeed())
+})
