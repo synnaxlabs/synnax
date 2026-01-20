84043336930aad250c9eedc896576401aad50f6c
Patrick Dotson <123601024+pjdotson@users.noreply.github.com>
2026-01-20 10:20:38 -0700

    SY-355: Fix `synnax --version` usage (#1846)

--- core/cmd/cmd_test.go
+++ core/cmd/cmd_test.go
@@ -7,20 +7,22 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-//go:build !windows
-
-package service_test
+package cmd_test
 
 import (
+	"bytes"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/synnaxlabs/synnax/cmd/service"
+	"github.com/synnaxlabs/synnax/cmd"
 )
 
-var _ = Describe("Service", func() {
-	Describe("RegisterCommands", func() {
-		It("should be a no-op on non-Windows platforms", func() {
-			Expect(service.AddCommand(nil)).To(Succeed())
+var _ = Describe("Cmd", func() {
+	Describe("Version", func() {
+		It("Should print the version when using the --version flag", func() {
+			var buf bytes.Buffer
+			Expect(cmd.ExecuteWithArgs([]string{"--version"}, &buf)).To(Succeed())
+			Expect(buf.String()).To(Equal(version))
 		})
 	})
 })
