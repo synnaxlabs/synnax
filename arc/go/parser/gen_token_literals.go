784c4c37f2976a0155a73c0dd1ad5dcd62b96cf7
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2026-01-23 15:24:43 -0700

    [arc/go] - fixed linting isseus

--- arc/go/parser/gen_token_literals.go
+++ arc/go/parser/gen_token_literals.go
@@ -28,10 +28,23 @@ func main() {
 	re := regexp.MustCompile(`(\w+)\s*:\s*'([^']+)'\s*;`)
 	matches := re.FindAllStringSubmatch(string(content), -1)
 	var b strings.Builder
-	b.WriteString("package parser\n\nconst (\n")
+	b.WriteString(`// Copyright 2026 Synnax Labs, Inc.
+//
+// Use of this software is governed by the Business Source License included in the file
+// licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with the Business Source
+// License, use of this software will be governed by the Apache License, Version 2.0,
+// included in the file licenses/APL.txt.
+
+package parser
+
+const (
+`)
 	for _, m := range matches {
 		name, literal := m[1], m[2]
-		b.WriteString(fmt.Sprintf("\tLiteral%s = %q\n", name, literal))
+		goName := strings.ReplaceAll(name, "_", "")
+		b.WriteString(fmt.Sprintf("\tLiteral%s = %q\n", goName, literal))
 	}
 	b.WriteString(")\n")
 	if err := os.WriteFile("token_literals.go", []byte(b.String()), 0644); err != nil {
