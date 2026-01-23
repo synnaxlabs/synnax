7a0033bf2115993fa33325d08266a65d79ca866d
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2026-01-23 15:26:39 -0700

    [arc/go] - updated gen token literals

--- arc/go/parser/gen_token_literals.go
+++ arc/go/parser/gen_token_literals.go
@@ -28,15 +28,7 @@ func main() {
 	re := regexp.MustCompile(`(\w+)\s*:\s*'([^']+)'\s*;`)
 	matches := re.FindAllStringSubmatch(string(content), -1)
 	var b strings.Builder
-	b.WriteString(`// Copyright 2026 Synnax Labs, Inc.
-//
-// Use of this software is governed by the Business Source License included in the file
-// licenses/BSL.txt.
-//
-// As of the Change Date specified in that file, in accordance with the Business Source
-// License, use of this software will be governed by the Apache License, Version 2.0,
-// included in the file licenses/APL.txt.
-
+	b.WriteString(`
 package parser
 
 const (
