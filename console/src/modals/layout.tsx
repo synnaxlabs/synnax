f468d441928f2dfa3db4f74fc21167c5243fd226
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2026-01-23 21:54:42 -0700

    [console] - moved layout modal into css files

--- console/src/modals/layout.tsx
+++ console/src/modals/layout.tsx
@@ -7,9 +7,12 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
+import "@/modals/layout.css";
+
 import { Flex } from "@synnaxlabs/pluto";
 import { type PropsWithChildren, type ReactElement } from "react";
 
+import { CSS } from "@/css";
 import { BottomNavBar } from "@/modals/BottomNavBar";
 
 export interface ModalContentLayoutProps extends PropsWithChildren, Flex.BoxProps {
@@ -19,11 +22,17 @@ export interface ModalContentLayoutProps extends PropsWithChildren, Flex.BoxProp
 export const ModalContentLayout = ({
   children,
   footer,
-  style,
+  className,
   ...rest
 }: ModalContentLayoutProps): ReactElement => (
   <Flex.Box y grow justify="center">
-    <Flex.Box y grow justify="center" style={{ padding: "5rem", ...style }} {...rest}>
+    <Flex.Box
+      y
+      grow
+      justify="center"
+      className={CSS(CSS.B("modal-content-layout"), className)}
+      {...rest}
+    >
       {children}
     </Flex.Box>
     <BottomNavBar>{footer}</BottomNavBar>
