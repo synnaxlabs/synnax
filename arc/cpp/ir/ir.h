cd3cfdfb0e70ef18bf8655bc294c121a055c83f0
sy-nico <nico@synnaxlabs.com>
2026-02-04 11:43:44 -0700

    [ir.h] change `constexpr std::string` to `constexpr std::string_view`
    MSVC doesn't support `constexpr std::string` well because `std::string `allocates heap memory, which isn't allowed in constant evaluation. `std::string_view` doesn't allocate and works correctly.

--- arc/cpp/ir/ir.h
+++ arc/cpp/ir/ir.h
@@ -16,6 +16,7 @@
 #include <ostream>
 #include <sstream>
 #include <string>
+#include <string_view>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -28,10 +29,10 @@
 #include "arc/go/symbol/arc/go/symbol/symbol.pb.h"
 
 namespace arc::ir {
-constexpr std::string default_output_param = "output";
-constexpr std::string default_input_param = "input";
-constexpr std::string lhs_input_param = "lhs_input";
-constexpr std::string rhs_input_param = "rhs_input";
+constexpr std::string_view default_output_param = "output";
+constexpr std::string_view default_input_param = "input";
+constexpr std::string_view lhs_input_param = "lhs_input";
+constexpr std::string_view rhs_input_param = "rhs_input";
 
 enum class EdgeKind { Unspecified = 0, Continuous = 1, OneShot = 2 };
 
