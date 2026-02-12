096730138353bab5b97264703167a3ae3666a5ae
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2026-02-12 11:47:51 -0700

    [x/go/os] - updated code generation suite

---
+++ x/go/os/stdio_test.go
@@ -0,0 +1,63 @@
+// Copyright 2026 Synnax Labs, Inc.
+//
+// Use of this software is governed by the Business Source License included in the file
+// licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with the Business Source
+// License, use of this software will be governed by the Apache License, Version 2.0,
+// included in the file licenses/APL.txt.
+
+package os_test
+
+import (
+	"io"
+	"os"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	xos "github.com/synnaxlabs/x/os"
+	. "github.com/synnaxlabs/x/testutil"
+)
+
+var _ io.ReadWriteCloser = xos.StdIO
+
+var _ = Describe("StdIO", func() {
+	It("Should return nil on Close", func() {
+		Expect(xos.StdIO.Close()).To(Succeed())
+	})
+
+	It("Should delegate writes to os.Stdout", func() {
+		r, w := MustSucceed2(os.Pipe())
+		defer r.Close()
+
+		origStdout := os.Stdout
+		os.Stdout = w
+		defer func() { os.Stdout = origStdout }()
+
+		data := []byte("hello stdout")
+		n := MustSucceed(xos.StdIO.Write(data))
+		Expect(n).To(Equal(len(data)))
+
+		w.Close()
+		buf := make([]byte, 64)
+		n = MustSucceed(r.Read(buf))
+		Expect(string(buf[:n])).To(Equal("hello stdout"))
+	})
+
+	It("Should delegate reads to os.Stdin", func() {
+		r, w := MustSucceed2(os.Pipe())
+		defer w.Close()
+
+		origStdin := os.Stdin
+		os.Stdin = r
+		defer func() { os.Stdin = origStdin }()
+
+		data := []byte("hello stdin")
+		MustSucceed(w.Write(data))
+
+		buf := make([]byte, 64)
+		n := MustSucceed(xos.StdIO.Read(buf))
+		Expect(n).To(Equal(len(data)))
+		Expect(string(buf[:n])).To(Equal("hello stdin"))
+	})
+})
