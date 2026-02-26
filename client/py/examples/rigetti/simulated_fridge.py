920759cde08d78d4327fc4c841436524d542d9d8
Patrick Dotson <patrick@synnaxlabs.com>
2026-02-26 04:24:31 -0800

    update state

--- client/py/examples/rigetti/simulated_fridge.py
+++ client/py/examples/rigetti/simulated_fridge.py
@@ -1,26 +1,5 @@
+from typing import Literal
 import synnax as sy
-from typing import TypedDict
-
-
-class Sensors(TypedDict):
-    p1: float
-    p6: float
-
-
-class Valves(TypedDict):
-    v14: bool
-    v16: bool
-    v19: bool
-    v20: bool
-    v21: bool
-    v22: bool
-    v23: bool
-    turbo_pump: bool
-    scroll_2_pump_state: bool
-
-
-# Create channels for the sensors, valves, and pumps.
-
 
 client = sy.Synnax(
     host="localhost",
@@ -29,6 +8,34 @@ client = sy.Synnax(
     password="seldon",
 )
 
+# Declare pieces of hardware that we will use to create channels.
+SENSORS: dict[str, float] = {
+    "p1": 1013.25,
+    "p2": 1013.25,
+    "p6": 1013.25,
+    "t4k": 4,
+    "tstill": 2,
+    "aux": 1013.25,
+    "vent": 1013.25,
+}
+
+ACTUATORS = (
+    "v14",
+    "v15",
+    "v16",
+    "v18",
+    "v19",
+    "v20",
+    "v21",
+    "heater_wu",
+    "heater_4k",
+    "turbo_pump",
+    "scroll_2_pump",
+)
+
+
+# Create channels for the sensors, valves, and pumps.
+
 time_channel = client.channels.create(
     name="fridge_simulation_time",
     is_index=True,
@@ -36,120 +43,129 @@ time_channel = client.channels.create(
     retrieve_if_name_exists=True,
 )
 
-sensor_channels = [
-    sy.Channel(
-        name=sensor,
-        data_type=sy.DataType.FLOAT32,
-        index=time_channel.key,
-    )
-    for sensor in Sensors.keys()
-]
 
-valve_state_channels = [
-    sy.Channel(
-        name=valve,
-        data_type=sy.DataType.UINT8,
-        index=time_channel.key,
-    )
-    for valve in Valves.keys()
-]
-
-client.channels.create(
-    [*sensor_channels, *valve_state_channels], retrieve_if_name_exists=True
+sensor_channels = client.channels.create(
+    [
+        sy.Channel(name=sensor, data_type=sy.DataType.FLOAT32, index=time_channel.key)
+        for sensor in SENSORS.keys()
+    ],
+    retrieve_if_name_exists=True,
 )
 
-valve_state_channels = client.channels.create(
-    valve_state_channels, retrieve_if_name_exists=True
-)
-
-## Create a cmd_time and cmd channel for each valve and pump.
-valve_cmd_time_channels = [
-    sy.Channel(
-        name=f"{valve}_cmd_time",
-        data_type=sy.DataType.TIMESTAMP,
-        is_index=True,
-    )
-    for valve in Valves.keys()
-]
-
-valve_cmd_time_channels = client.channels.create(
-    valve_cmd_time_channels, retrieve_if_name_exists=True
-)
-
-
-client.channels.create(
+sensor_override_channels = client.channels.create(
     [
         sy.Channel(
-            name=f"{valve}_cmd",
-            data_type=sy.DataType.UINT8,
-            index=valve_cmd_time_channels[i].key,
+            name=f"{sensor}_override", data_type=sy.DataType.FLOAT32, virtual=True
         )
-        for i, valve in enumerate(Valves.keys())
+        for sensor in SENSORS.keys()
     ],
     retrieve_if_name_exists=True,
 )
 
 
-class DaqState:
-    sensors: Sensors
-    valves: Valves
+actuator_state_channels = client.channels.create(
+    [
+        sy.Channel(name=actuator, data_type=sy.DataType.UINT8, index=time_channel.key)
+        for actuator in ACTUATORS
+    ],
+    retrieve_if_name_exists=True,
+)
 
-    def to_frame(self) -> sy.Frame:
-        return sy.Frame(
-            {
-                time_channel.name: sy.TimeStamp.now(),
-                **self.sensors,
-                **{f"{valve}_state": value for valve, value in self.valves.items()},
-            }
-        )
-
+actuator_cmd_channels = client.channels.create(
+    [
+        sy.Channel(name=f"{actuator}_cmd", data_type=sy.DataType.UINT8, virtual=True)
+        for actuator in ACTUATORS
+    ],
+    retrieve_if_name_exists=True,
+)
 
 # Now we have all of the channels, we will want to have a loop that reads from the
 # command channels and updates the state and sensor values as appropriate.
 
-loop = sy.Loop(sy.Rate.HZ * 100)
+loop = sy.Loop(sy.Rate.HZ * 10)
 
 
-atmospheric_pressure = 1013.25  # mBar
+atmospheric_pressure_mbar = 1013.25
+atmospheric_temp_k = 320
 
 
 def update_state(
-    daq_state: dict[str, float | bool], frame: sy.Frame | None
+    state: dict[str, float | bool], frame: sy.Frame | None
 ) -> dict[str, float | bool]:
-    ### Implement physics logic in this function.
-    daq_state[time_channel.name] = sy.TimeStamp.now()
+    state[time_channel.name] = sy.TimeStamp.now()
     if frame is not None:
         for channel in frame.channels:
             series = frame[channel]
             val = series[-1]
-            print(f"Received command: {channel}={val}")
-            state_channel_name = str(channel).replace("_cmd", "_state")
-            daq_state[state_channel_name] = val
-    ## determine p1 and p2 pressure based on v14, v16, and v19 states
-    v14_and_v16_open = bool(daq_state["V14_state"]) and bool(daq_state["V16_state"])
-    v19_open = bool(daq_state["V19_state"])
-    if v14_and_v16_open and v19_open:
-        daq_state["p1"] += (atmospheric_pressure - daq_state["p1"]) * 0.01
-        daq_state["p6"] += (atmospheric_pressure - daq_state["p6"]) * 0.01
-    elif v14_and_v16_open:
-        # make p1 and p6 move towards each other
-        daq_state["p1"] += (daq_state["p6"] - daq_state["p1"]) * 0.01
-        daq_state["p6"] += (daq_state["p1"] - daq_state["p6"]) * 0.01
-    elif v19_open:
-        # make p6 move towards atmospheric pressure
-        daq_state["p6"] += (atmospheric_pressure - daq_state["p6"]) * 0.01
-    else:
-        # do nothing, everything is closed
-        pass
-    return daq_state
+            if channel.endswith("_override"):
+                sensor_name = channel.removesuffix("_override")
+                print(f"Setting {sensor_name} to {float(val)}")
+                state[sensor_name] = val
+                continue
+            actuator_name = channel.removesuffix("_cmd")
+            is_open = bool(val)
+            is_valve = channel.startswith("v")
+            if is_valve:
+                print(f"{'Opening' if is_open else 'Closing'} {actuator_name}")
+            else:
+                print(f"{'Activating' if is_open else 'De-activating'} {actuator_name}")
+            state[actuator_name] = val
+
+    next_state = state.copy()
+
+    def decrease_sensor(sensor: str):
+        next_state[sensor] -= 0.01 * state[sensor]
+
+    def move_towards(sensor1: str, sensor2: str):
+        delta = 0.01 * (next_state[sensor2] - next_state[sensor1])
+        next_state[sensor1] += delta
+        next_state[sensor2] -= delta
+
+    def move_from(sensor1: str, sensor2: str):
+        amount = 0.01 * state[sensor1]
+        next_state[sensor1] -= amount
+        next_state[sensor2] += amount
+
+    def move_towards_target(sensor: str, target: float, amount: float = 0.01):
+        delta = amount * (target - next_state[sensor])
+        next_state[sensor] += delta
+
+    ## Implement physics logic here
+
+    if state["heater_4k"]:
+        move_towards_target("t4k", atmospheric_temp_k)
+        move_towards_target("tstill", atmospheric_temp_k)
+    if state["heater_wu"]:
+        move_towards_target("t4k", atmospheric_temp_k, 0.001)
+        move_towards_target("tstill", atmospheric_temp_k, 0.001)
+
+    if state["v14"] and state["v16"]:
+        move_towards("p1", "p6")
+    if state["v15"] and state["v16"]:
+        move_towards("p2", "p6")
+    if state["v14"] and state["v15"]:
+        move_towards("p1", "p2")
+    if state["v19"]:
+        move_towards_target("p6", state["vent"])
+    if state["v20"]:
+        move_towards_target("p6", state["aux"])
+    if state["v18"] and state["turbo_pump"]:
+        move_from("p2", "p6")
+    if state["v21"] and state["scroll_2_pump"]:
+        decrease_sensor("p6")
+
+    return next_state
 
 
-streaming_channels = [ch.name for ch in valve_cmd_channels] + [
-    ch.name for ch in pump_cmd_channels
+streaming_channels = [ch.name for ch in actuator_cmd_channels] + [
+    ch.name for ch in sensor_override_channels
 ]
 
-writing_channels: list[str] = [*Sensors.keys(), *Valves.keys()]
-
+writing_channels = (
+    [ch.name for ch in actuator_state_channels]
+    + [time_channel.name]
+    + [ch.name for ch in sensor_channels]
+)
 
 with client.open_streamer(streaming_channels) as streamer:
     with client.open_writer(
@@ -157,14 +173,12 @@ with client.open_streamer(streaming_channels) as streamer:
         channels=writing_channels,
         name="fridge_simulation",
     ) as writer:
-        daq_state: dict[str, float | bool] = {
-            "p1": 3,
-            "p6": 0.4,
-            **{f"{valve}_state": False for valve in VALVES},
-            **{f"{pump}_state": False for pump in PUMPS},
+        state: dict[str, float | bool | sy.TimeStamp] = {
+            **SENSORS,
+            **{actuator: False for actuator in ACTUATORS},
             **{time_channel.name: sy.TimeStamp.now()},
         }
         while loop.wait():
             frame = streamer.read(timeout=0)
-            daq_state = update_state(daq_state, frame)
-            writer.write(daq_state)
+            state = update_state(state, frame)
+            writer.write(state)
