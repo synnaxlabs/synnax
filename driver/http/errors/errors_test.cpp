193640ed01688e070e530fce9e090ac5c9a9cf18
Patrick Dotson <patrick@synnaxlabs.com>
2026-03-09 10:33:29 -0600

    Use macros for `from_status` tests

--- driver/http/errors/errors_test.cpp
+++ driver/http/errors/errors_test.cpp
@@ -15,88 +15,38 @@
 
 namespace driver::http::errors {
 
-TEST(FromStatus, Status000ReturnsCriticalError) {
-    ASSERT_OCCURRED_AS(from_status(000), CRITICAL_ERROR);
-}
+#define STATUS_TEST(code, expected_err) \
+    TEST(FromStatus, Status##code##Returns##expected_err) { \
+        ASSERT_OCCURRED_AS(from_status(code), expected_err); \
+    }
 
-TEST(FromStatus, Status100ReturnsCriticalError) {
-    ASSERT_OCCURRED_AS(from_status(100), CRITICAL_ERROR);
-}
+#define STATUS_TEST_NIL(code) \
+    TEST(FromStatus, Status##code##ReturnsNil) { \
+        ASSERT_NIL(from_status(code)); \
+    }
 
-TEST(FromStatus, Status199ReturnsCriticalError) {
-    ASSERT_OCCURRED_AS(from_status(199), CRITICAL_ERROR);
-}
+STATUS_TEST(000, CRITICAL_ERROR)
+STATUS_TEST(100, CRITICAL_ERROR)
+STATUS_TEST(199, CRITICAL_ERROR)
 
-TEST(FromStatus, Status200ReturnsNil) {
-    ASSERT_NIL(from_status(200));
-}
+STATUS_TEST_NIL(200)
+STATUS_TEST_NIL(201)
+STATUS_TEST_NIL(204)
+STATUS_TEST_NIL(299)
 
-TEST(FromStatus, Status201ReturnsNil) {
-    ASSERT_NIL(from_status(201));
-}
-
-TEST(FromStatus, Status204ReturnsNil) {
-    ASSERT_NIL(from_status(204));
-}
-
-TEST(FromStatus, Status299ReturnsNil) {
-    ASSERT_NIL(from_status(299));
-}
-
-TEST(FromStatus, Status300ReturnsCriticalError) {
-    ASSERT_OCCURRED_AS(from_status(300), CRITICAL_ERROR);
-}
-
-TEST(FromStatus, Status399ReturnsCriticalError) {
-    ASSERT_OCCURRED_AS(from_status(399), CRITICAL_ERROR);
-}
-
-TEST(FromStatus, Status400ReturnsCriticalError) {
-    ASSERT_OCCURRED_AS(from_status(400), CRITICAL_ERROR);
-}
-
-TEST(FromStatus, Status404ReturnsTemporaryError) {
-    ASSERT_OCCURRED_AS(from_status(404), TEMPORARY_ERROR);
-}
-
-TEST(FromStatus, Status408ReturnsTemporaryError) {
-    ASSERT_OCCURRED_AS(from_status(408), TEMPORARY_ERROR);
-}
-
-TEST(FromStatus, Status429ReturnsTemporaryError) {
-    ASSERT_OCCURRED_AS(from_status(429), TEMPORARY_ERROR);
-}
-
-TEST(FromStatus, Status499ReturnsCriticalError) {
-    ASSERT_OCCURRED_AS(from_status(499), CRITICAL_ERROR);
-}
-
-TEST(FromStatus, Status500ReturnsTemporaryError) {
-    ASSERT_OCCURRED_AS(from_status(500), TEMPORARY_ERROR);
-}
-
-TEST(FromStatus, Status503ReturnsTemporaryError) {
-    ASSERT_OCCURRED_AS(from_status(503), TEMPORARY_ERROR);
-}
-
-TEST(FromStatus, Status599ReturnsTemporaryError) {
-    ASSERT_OCCURRED_AS(from_status(599), TEMPORARY_ERROR);
-}
-
-TEST(FromStatus, Status600ReturnsCriticalError) {
-    ASSERT_OCCURRED_AS(from_status(600), CRITICAL_ERROR);
-}
-
-TEST(FromStatus, Status700ReturnsCriticalError) {
-    ASSERT_OCCURRED_AS(from_status(700), CRITICAL_ERROR);
-}
-
-TEST(FromStatus, Status800ReturnsCriticalError) {
-    ASSERT_OCCURRED_AS(from_status(800), CRITICAL_ERROR);
-}
-
-TEST(FromStatus, Status900ReturnsCriticalError) {
-    ASSERT_OCCURRED_AS(from_status(900), CRITICAL_ERROR);
-}
+STATUS_TEST(300, CRITICAL_ERROR)
+STATUS_TEST(399, CRITICAL_ERROR)
+STATUS_TEST(400, CRITICAL_ERROR)
+STATUS_TEST(404, TEMPORARY_ERROR)
+STATUS_TEST(408, TEMPORARY_ERROR)
+STATUS_TEST(429, TEMPORARY_ERROR)
+STATUS_TEST(499, CRITICAL_ERROR)
+STATUS_TEST(500, TEMPORARY_ERROR)
+STATUS_TEST(503, TEMPORARY_ERROR)
+STATUS_TEST(599, TEMPORARY_ERROR)
+STATUS_TEST(600, CRITICAL_ERROR)
+STATUS_TEST(700, CRITICAL_ERROR)
+STATUS_TEST(800, CRITICAL_ERROR)
+STATUS_TEST(900, CRITICAL_ERROR)
 
 }
