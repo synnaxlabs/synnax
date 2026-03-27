481c450ed4fef3b64c180abaca7b5967a6fa77a9
Patrick Dotson <patrick@synnaxlabs.com>
2026-03-26 21:23:43 -0600

    More TypeScript upgrades

--- pluto/src/side-effects.d.ts
+++ pluto/src/side-effects.d.ts
@@ -7,6 +7,6 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-/// <reference types="astro/client" />
-
+declare module "@fontsource-variable/inter";
 declare module "@fontsource/geist-mono";
+declare module "@xyflow/react/dist/base.css";
