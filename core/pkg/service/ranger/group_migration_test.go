80a12fc84be7c29be3b9f2b5bdc59dc744f2bd8b
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2026-04-03 12:14:15 -0600

    SY-3816 [7/8]: Ranger Migration Update (#2161)

--- core/pkg/service/ranger/group_migration_test.go
+++ core/pkg/service/ranger/group_migration_test.go
@@ -12,6 +12,7 @@ package ranger_test
 import (
 	"io"
 
+	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/synnaxlabs/synnax/pkg/distribution/group"
@@ -29,17 +30,18 @@ import (
 
 var _ = Describe("Migrate", func() {
 	var (
-		db     *gorp.DB
-		svc    *ranger.Service
-		lab    *label.Service
-		otg    *ontology.Ontology
-		closer io.Closer
-		gSvc   *group.Service
+		db        *gorp.DB
+		svc       *ranger.Service
+		otg       *ontology.Ontology
+		closer    io.Closer
+		gSvc      *group.Service
+		searchIdx *search.Index
+		lab       *label.Service
 	)
 	BeforeEach(func(ctx SpecContext) {
 		db = gorp.Wrap(memkv.New())
 		otg = MustSucceed(ontology.Open(ctx, ontology.Config{DB: db}))
-		searchIdx := MustSucceed(search.Open())
+		searchIdx = MustSucceed(search.Open())
 		DeferCleanup(func() {
 			Expect(searchIdx.Close()).To(Succeed())
 		})
@@ -54,30 +56,30 @@ var _ = Describe("Migrate", func() {
 			Group:    gSvc,
 			Search:   searchIdx,
 		}))
-		svc = MustSucceed(ranger.OpenService(ctx, ranger.ServiceConfig{
-			DB:       db,
-			Ontology: otg,
-			Group:    gSvc,
-			Label:    lab,
-			Search:   searchIdx,
-		}))
-		closer = xio.MultiCloser{db, otg, gSvc}
+		closer = xio.MultiCloser{db, otg, gSvc, lab}
 	})
 	AfterEach(func() {
 		Expect(closer.Close()).To(Succeed())
 	})
 	It("should migrate subgroups to parent ranges and delete groups", func(ctx SpecContext) {
-		// Manually create the "Ranges" group
-		tx := db.OpenTx()
-		tlg := MustSucceed(gSvc.NewWriter(tx).Create(ctx, "Ranges", ontology.RootID))
+		// Open a bare Range table with only the codec transition migration.
+		// This simulates the state of the DB before the range_groups migration
+		// was added.
+		bareTable := MustSucceed(gorp.OpenTable[uuid.UUID, ranger.Range](
+			ctx, gorp.TableConfig[ranger.Range]{DB: db},
+		))
 
-		// Create a subgroup under "Ranges"
+		tx := db.OpenTx()
+
+		// Create the "Ranges" group and a subgroup.
+		tlg := MustSucceed(gSvc.NewWriter(tx).Create(ctx, "Ranges", ontology.RootID))
 		subGroup := MustSucceed(
 			gSvc.NewWriter(tx).Create(ctx, "Subgroup", tlg.OntologyID()),
 		)
 
-		// Create two ranges under the subgroup
+		// Create two ranges and their ontology resources under the subgroup.
 		r1 := ranger.Range{
+			Key:  uuid.New(),
 			Name: "Range1",
 			TimeRange: telem.TimeRange{
 				Start: telem.TimeStamp(10 * telem.Second),
@@ -85,41 +87,54 @@ var _ = Describe("Migrate", func() {
 			},
 		}
 		r2 := ranger.Range{
+			Key:  uuid.New(),
 			Name: "Range2",
 			TimeRange: telem.TimeRange{
 				Start: telem.TimeStamp(15 * telem.Second),
 				End:   telem.TimeStamp(25 * telem.Second),
 			},
 		}
-		ranges := []ranger.Range{r1, r2}
-		Expect(
-			svc.NewWriter(tx).CreateManyWithParent(ctx, &ranges, subGroup.OntologyID()),
-		).To(Succeed())
+		Expect(bareTable.NewCreate().Entry(&r1).Exec(ctx, tx)).To(Succeed())
+		Expect(bareTable.NewCreate().Entry(&r2).Exec(ctx, tx)).To(Succeed())
+
+		otgWriter := otg.NewWriter(tx)
+		Expect(otgWriter.DefineResource(ctx, ranger.OntologyID(r1.Key))).To(Succeed())
+		Expect(otgWriter.DefineResource(ctx, ranger.OntologyID(r2.Key))).To(Succeed())
+		Expect(otgWriter.DefineRelationship(
+			ctx,
+			subGroup.OntologyID(),
+			ontology.RelationshipTypeParentOf,
+			ranger.OntologyID(r1.Key),
+		)).To(Succeed())
+		Expect(otgWriter.DefineRelationship(
+			ctx,
+			subGroup.OntologyID(),
+			ontology.RelationshipTypeParentOf,
+			ranger.OntologyID(r2.Key),
+		)).To(Succeed())
+
 		Expect(tx.Commit(ctx)).To(Succeed())
 		Expect(tx.Close()).To(Succeed())
-		Expect(svc.Close()).To(Succeed())
+		Expect(bareTable.Close()).To(Succeed())
 
-		// Reopen the service to run the migration
-		searchIdx2 := MustSucceed(search.Open())
 		svc = MustSucceed(ranger.OpenService(ctx, ranger.ServiceConfig{
 			DB:             db,
 			Ontology:       otg,
 			Group:          gSvc,
 			Label:          lab,
 			ForceMigration: new(true),
-			Search:         searchIdx2,
+			Search:         searchIdx,
 		}))
 
-		// The "Ranges" group and "Subgroup" should be deleted
+		// The "Ranges" group and "Subgroup" should be deleted.
 		var g group.Group
 		Expect(gSvc.NewRetrieve().WhereNames("Ranges").Entry(&g).Exec(ctx, nil)).
 			To(MatchError(query.ErrNotFound))
-
 		Expect(gSvc.NewRetrieve().WhereNames("Subgroup").Entry(&g).Exec(ctx, nil)).
 			To(MatchError(query.ErrNotFound))
 
-		// There should be a new parent range named "Subgroup" whose time range is the
-		// union of r1 and r2
+		// There should be a new parent range named "Subgroup" whose time range
+		// is the union of r1 and r2.
 		var parentRange ranger.Range
 		Expect(svc.NewRetrieve().WhereNames("Subgroup").
 			Entry(&parentRange).Exec(ctx, nil)).To(Succeed())
@@ -128,7 +143,7 @@ var _ = Describe("Migrate", func() {
 			End:   telem.TimeStamp(25 * telem.Second),
 		}))
 
-		// The parent range should have r1 and r2 as children in the ontology
+		// The parent range should have r1 and r2 as children in the ontology.
 		var children []ontology.Resource
 		Expect(otg.NewRetrieve().
 			WhereIDs(parentRange.OntologyID()).
