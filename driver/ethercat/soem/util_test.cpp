ac434aa8f7b60eadcd4dae8538396ee9c982eb50
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2026-04-05 20:24:56 -0600

    fixed issues with macos driver tests

--- driver/ethercat/soem/util_test.cpp
+++ driver/ethercat/soem/util_test.cpp
@@ -7,7 +7,7 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
 
 #include "driver/ethercat/soem/util.h"
 
