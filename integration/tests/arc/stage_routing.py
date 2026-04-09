0ea78240b201d69553a55a54b72bd30c718a28c6
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2026-04-09 07:36:36 -0600

    fised integration stage routing tests

--- integration/tests/arc/stage_routing.py
+++ integration/tests/arc/stage_routing.py
@@ -36,11 +36,11 @@ sequence main {
     }
     stage select_on {
         "select_on" -> routing_stage_log,
-        wait{50ms} => select_hold
+        next_cmd => select_hold
     }
     stage select_off {
         "select_off" -> routing_stage_log,
-        wait{50ms} => decide_stage_hold
+        next_cmd => decide_stage_hold
     }
 
     stage decide_stage_hold {
@@ -53,15 +53,15 @@ sequence main {
     }
     stage decide_stage_vent {
         "decide_stage_vent" -> routing_stage_log,
-        wait{50ms} => decide_stage_hold
+        next_cmd => decide_stage_hold
     }
     stage decide_stage_press {
         "decide_stage_press" -> routing_stage_log,
-        wait{50ms} => decide_stage_hold
+        next_cmd => decide_stage_hold
     }
     stage decide_stage_abort {
         "decide_stage_abort" -> routing_stage_log,
-        wait{50ms} => done
+        next_cmd => done
     }
 
     stage done {
@@ -117,6 +117,12 @@ class StageRouting(ArcConsoleCase):
             virtual=True,
             retrieve_if_name_exists=True,
         )
+        self.client.channels.create(
+            name="next_cmd",
+            data_type=sy.DataType.UINT8,
+            virtual=True,
+            retrieve_if_name_exists=True,
+        )
         super().setup()
 
     def _write_flag(self, value: int) -> None:
@@ -141,6 +147,10 @@ class StageRouting(ArcConsoleCase):
                 }
             )
 
+    def _advance(self) -> None:
+        with self.client.open_writer(sy.TimeStamp.now(), "next_cmd") as w:
+            w.write("next_cmd", 1)
+
     def verify_sequence_execution(self) -> None:
         # Phase 1: select routing
         self.log("[select] Sequence activated, expecting select_hold")
@@ -150,34 +160,39 @@ class StageRouting(ArcConsoleCase):
         self._write_flag(1)
         self.wait_for_eq("routing_stage_log", "select_on", is_virtual=True)
 
-        self.log("[select] Waiting for select_on -> select_hold")
+        self.log("[select] Advancing select_on -> select_hold")
+        self._advance()
         self.wait_for_eq("routing_stage_log", "select_hold", is_virtual=True)
 
         self.log("[select] flag=0 -> select_off")
         self._write_flag(0)
         self.wait_for_eq("routing_stage_log", "select_off", is_virtual=True)
 
-        # Phase 2: decide_stage routing (select_off waits 50ms then -> decide_stage_hold)
-        self.log("[decide_stage] Waiting for select_off -> decide_stage_hold")
+        # Phase 2: decide_stage routing
+        self.log("[decide_stage] Advancing select_off -> decide_stage_hold")
+        self._advance()
         self.wait_for_eq("routing_stage_log", "decide_stage_hold", is_virtual=True)
 
         self.log("[decide_stage] sensor=50.0 (mid range) -> decide_stage_press")
         self._write_sensor(50.0)
         self.wait_for_eq("routing_stage_log", "decide_stage_press", is_virtual=True)
 
-        self.log("[decide_stage] Waiting for decide_stage_press -> decide_stage_hold")
+        self.log("[decide_stage] Advancing decide_stage_press -> decide_stage_hold")
+        self._advance()
         self.wait_for_eq("routing_stage_log", "decide_stage_hold", is_virtual=True)
 
         self.log("[decide_stage] sensor=10.0 (below low) -> decide_stage_vent")
         self._write_sensor(10.0)
         self.wait_for_eq("routing_stage_log", "decide_stage_vent", is_virtual=True)
 
-        self.log("[decide_stage] Waiting for decide_stage_vent -> decide_stage_hold")
+        self.log("[decide_stage] Advancing decide_stage_vent -> decide_stage_hold")
+        self._advance()
         self.wait_for_eq("routing_stage_log", "decide_stage_hold", is_virtual=True)
 
         self.log("[decide_stage] sensor=90.0 (above high) -> decide_stage_abort")
         self._write_sensor(90.0)
         self.wait_for_eq("routing_stage_log", "decide_stage_abort", is_virtual=True)
 
-        self.log("[decide_stage] Waiting for decide_stage_abort -> done")
+        self.log("[decide_stage] Advancing decide_stage_abort -> done")
+        self._advance()
         self.wait_for_eq("routing_stage_log", "done", is_virtual=True)
