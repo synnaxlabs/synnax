8ecbc08b12ef9761856c2e883fa68bdf43f7828e
sy-nico <nico@synnaxlabs.com>
2026-04-09 11:44:34 -0600

    fix writer start time

--- integration/framework/writer.py
+++ integration/framework/writer.py
@@ -19,7 +19,6 @@ class Writer:
         self._client = client
         self._writer: sy.Writer | None = None
         self._channels: set[str] = set()
-        self._start: sy.TimeStamp | None = None
         self._lock = threading.Lock()
 
     def write(self, channel_or_data: str | dict, value=None) -> None:
@@ -33,10 +32,8 @@ class Writer:
                 if self._writer is not None:
                     self._writer.close()
                 self._channels |= data.keys()
-                if self._start is None:
-                    self._start = sy.TimeStamp.now()
                 self._writer = self._client.open_writer(
-                    start=self._start,
+                    start=sy.TimeStamp.now() - 1 * sy.TimeSpan.MILLISECOND,
                     channels=list(self._channels),
                     enable_auto_commit=True,
                 )
@@ -49,4 +46,3 @@ class Writer:
                 self._writer.close()
                 self._writer = None
                 self._channels = set()
-                self._start = None
