abbec9bbe4cfa9a8723f78161e0fb2e8cf2228e4
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2026-05-06 17:39:01 -0600

    Add v10 layout migration to drop persisted getStarted state

--- console/src/layout/types/index.ts
+++ console/src/layout/types/index.ts
@@ -12,6 +12,7 @@ import { z } from "zod";
 
 import * as v0 from "@/layout/types/v0";
 import * as v1 from "@/layout/types/v1";
+import * as v10 from "@/layout/types/v10";
 import * as v2 from "@/layout/types/v2";
 import * as v3 from "@/layout/types/v3";
 import * as v4 from "@/layout/types/v4";
@@ -22,7 +23,7 @@ import * as v8 from "@/layout/types/v8";
 import * as v9 from "@/layout/types/v9";
 
 export type State<A = unknown> = v0.State<A>;
-export type SliceState = v9.SliceState;
+export type SliceState = v10.SliceState;
 export type NavDrawerLocation = v0.NavDrawerLocation;
 export type NavDrawerEntryState = v0.NavDrawerEntryState;
 export type WindowProps = v0.WindowProps;
@@ -37,7 +38,8 @@ export type AnySliceState =
   | v6.SliceState
   | v7.SliceState
   | v8.SliceState
-  | v9.SliceState;
+  | v9.SliceState
+  | v10.SliceState;
 
 export const SLICE_MIGRATIONS: migrate.Migrations = {
   [v0.VERSION]: v1.sliceMigration,
@@ -49,9 +51,10 @@ export const SLICE_MIGRATIONS: migrate.Migrations = {
   [v6.VERSION]: v7.sliceMigration,
   [v7.VERSION]: v8.sliceMigration,
   [v8.VERSION]: v9.sliceMigration,
+  [v9.VERSION]: v10.sliceMigration,
 };
 
-export const ZERO_SLICE_STATE = v9.ZERO_SLICE_STATE;
+export const ZERO_SLICE_STATE = v10.ZERO_SLICE_STATE;
 export const ZERO_MOSAIC_STATE = v0.ZERO_MOSAIC_STATE;
 export const MAIN_LAYOUT = v0.MAIN_LAYOUT;
 
@@ -63,6 +66,7 @@ export const migrateSlice = migrate.migrator<AnySliceState, SliceState>({
 
 export const anySliceStateZ = z
   .union([
+    v10.sliceStateZ,
     v9.sliceStateZ,
     v8.sliceStateZ,
     v7.sliceStateZ,
