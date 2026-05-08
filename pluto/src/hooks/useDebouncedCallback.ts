65c5d8efa648a6e5f25fce2ffad86c289d2ecaa0
Patrick Dotson <patrickdotson11@gmail.com>
2026-05-08 16:54:48 -0600

    Fix dependency issue

--- pluto/src/hooks/useDebouncedCallback.ts
+++ pluto/src/hooks/useDebouncedCallback.ts
@@ -7,12 +7,14 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-import { type CrudeTimeSpan, debounce } from "@synnaxlabs/x";
+import { type CrudeTimeSpan, debounce, TimeSpan } from "@synnaxlabs/x";
 import { type DependencyList, useCallback } from "react";
 
 export const useDebouncedCallback = <Args extends unknown[]>(
   func: (...args: Args) => void,
   waitFor: CrudeTimeSpan,
   deps: DependencyList,
-): ((...args: Args) => void) =>
-  useCallback(debounce(func, waitFor), [waitFor, ...deps]);
+): ((...args: Args) => void) => {
+  const debouncePeriod = new TimeSpan(waitFor).valueOf();
+  return useCallback(debounce(func, debouncePeriod), [func, debouncePeriod, ...deps]);
+};
