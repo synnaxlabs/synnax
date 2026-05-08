acfa890ff03caa8de3050122d24b64739212e59a
Patrick Dotson <patrickdotson11@gmail.com>
2026-05-08 16:06:41 -0600

    Fix weird type

--- console/src/hardware/task/useSetDataSaving.spec.ts
+++ console/src/hardware/task/useSetDataSaving.spec.ts
@@ -7,7 +7,7 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-import { createTestClient } from "@synnaxlabs/client";
+import { createTestClient, type rack } from "@synnaxlabs/client";
 import { id } from "@synnaxlabs/x";
 import { act, renderHook, waitFor } from "@testing-library/react";
 import { type PropsWithChildren } from "react";
@@ -20,7 +20,7 @@ const client = createTestClient();
 
 describe("useSetDataSaving", () => {
   let wrapper: React.FC<PropsWithChildren>;
-  let rack: { key: number; createTask: (...args: any[]) => Promise<any> };
+  let rack: rack.Rack;
 
   beforeEach(async () => {
     wrapper = await createAsyncSynnaxWrapper({ client });
