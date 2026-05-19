b17aef1f441a3c7b4df08dd9c45a241058d8a752
Patrick Dotson <123601024+pjdotson@users.noreply.github.com>
2026-05-19 12:35:12 -0600

    Rename Python Internal Methods and Attributes to Single Underscore Prefix (#2335)

--- freighter/py/freighter/http.py
+++ freighter/py/freighter/http.py
@@ -20,7 +20,6 @@ from freighter.codec import Codec
 from freighter.context import Context, Role
 from freighter.exceptions import Unreachable
 from freighter.transport import RQ, RS, MiddlewareCollector
-from freighter.unary import UnaryClient
 from freighter.url import URL
 from x.exceptions import ExceptionPayload, decode_exception
 
@@ -58,9 +57,6 @@ class HTTPClient(MiddlewareCollector):
         self.__secure = secure
         self.__pool = PoolManager(cert_reqs="CERT_NONE", **kwargs)
         urllib3.disable_warnings()
-
-    def __(self) -> UnaryClient:
-        return self
 
     def send(
         self, target: str, req: RQ, res_t: type[RS]
