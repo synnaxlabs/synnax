59e3fdaf328031528d93a79cdc8a28194849f34c
Patrick Dotson <patrickdotson11@gmail.com>
2026-05-19 19:46:08 -0600

    Add form selector

--- pluto/src/telem/SelectTimestampFormat.tsx
+++ pluto/src/telem/SelectTimestampFormat.tsx
@@ -7,9 +7,12 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
+import "@/telem/SelectTimestampFormat.css";
+
 import { type TimestampFormat } from "@synnaxlabs/x";
 import { type ReactElement } from "react";
 
+import { CSS } from "@/css";
 import { Select } from "@/select";
 
 const DATA: Select.StaticEntry<TimestampFormat>[] = [
@@ -22,13 +25,24 @@ const DATA: Select.StaticEntry<TimestampFormat>[] = [
   { key: "preciseDate", name: "Precise Date" },
 ];
 
+const CLS = CSS.BE("select-timestamp-format", "dialog");
+
 export interface SelectTimestampFormatProps extends Omit<
   Select.StaticProps<TimestampFormat>,
   "data" | "resourceName"
 > {}
 
-export const SelectTimestampFormat = (
-  props: SelectTimestampFormatProps,
-): ReactElement => (
-  <Select.Static {...props} data={DATA} resourceName="timestamp format" />
+export const SelectTimestampFormat = ({
+  dialogProps,
+  ...rest
+}: SelectTimestampFormatProps): ReactElement => (
+  <Select.Static
+    {...rest}
+    dialogProps={{
+      ...dialogProps,
+      className: CSS(CLS, dialogProps?.className),
+    }}
+    data={DATA}
+    resourceName="timestamp format"
+  />
 );
