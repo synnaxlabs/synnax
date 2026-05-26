0fa2515fee3ea95ffc23b9669930efc5e138cfd6
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2026-05-26 14:22:56 -0600

    SY-4066: Convert console table useCreate table import to type-only

--- console/src/table/useCreate.ts
+++ console/src/table/useCreate.ts
@@ -7,7 +7,7 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-import { table, type workspace } from "@synnaxlabs/client";
+import { type table, type workspace } from "@synnaxlabs/client";
 import { Table as PTable } from "@synnaxlabs/pluto";
 import { useCallback } from "react";
 
