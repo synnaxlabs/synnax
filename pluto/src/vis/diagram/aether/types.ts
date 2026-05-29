df30982857f5c56d9292473663a87189ebefd740
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2026-05-28 21:13:43 -0600

    SY-4256: Use type-only import for box in diagram types

--- pluto/src/vis/diagram/aether/types.ts
+++ pluto/src/vis/diagram/aether/types.ts
@@ -7,7 +7,7 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-import { box, dimensions, id, type location, xy } from "@synnaxlabs/x";
+import { type box, dimensions, id, type location, xy } from "@synnaxlabs/x";
 import type * as rf from "@xyflow/react";
 import { MarkerType } from "@xyflow/react";
 import type React from "react";
