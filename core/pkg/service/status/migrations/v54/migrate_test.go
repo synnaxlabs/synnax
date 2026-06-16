d6a45a602d4e3a118f6313138577214dd6a4561a
Patrick Dotson <patrickdotson11@gmail.com>
2026-06-16 15:21:49 -0600

    Move status v54 migration test into migrations/v54 suite

--- core/pkg/service/status/migrations/v54/migrate_test.go
+++ core/pkg/service/status/migrations/v54/migrate_test.go
@@ -7,7 +7,7 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-package status_test
+package v54_test
 
 import (
 	"github.com/google/uuid"
@@ -43,7 +43,7 @@ var _ = Describe("v54 -> current Status migration", func() {
 				{Key: uuid.New(), Name: "primary", Color: colorv54.Color{B: 200, A: 1}},
 			},
 		}
-		MustSucceed(gorp.OpenTable[string, v54.Status[any]](
+		MustSucceed(gorp.OpenTable(
 			ctx, gorp.TableConfig[string, v54.Status[any]]{DB: db},
 		))
 		Expect(gorp.NewCreate[string, v54.Status[any]]().
@@ -53,7 +53,7 @@ var _ = Describe("v54 -> current Status migration", func() {
 			DB:        db,
 			Namespace: "Status",
 			Migrations: []migrate.Migration{
-				gorp.NewEntryMigration[string, string, v54.Status[any], status.Status[any]](
+				gorp.NewEntryMigration(
 					"v54_drop_labels",
 					status.MigrateStatus[any],
 				),
