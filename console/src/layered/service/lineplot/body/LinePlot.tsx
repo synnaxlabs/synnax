8d86877e29837629245c48f0985aba3565079a19
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2026-06-24 11:24:30 -0600

    fixed copright headers

--- console/src/layered/service/lineplot/body/LinePlot.tsx
+++ console/src/layered/service/lineplot/body/LinePlot.tsx
@@ -6,7 +6,7 @@
 // As of the Change Date specified in that file, in accordance with the Business Source
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
-//
+
 import "@/layered/service/lineplot/body/LinePlot.css";
 
 import { lineplot, type ranger } from "@synnaxlabs/client";
