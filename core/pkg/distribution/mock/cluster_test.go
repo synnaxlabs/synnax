6a3c237139e1704132c7848767a465065a6f3f63
Patrick Dotson <patrickdotson11@gmail.com>
2026-06-26 11:56:16 -0600

    Standardize variable naming

--- core/pkg/distribution/mock/cluster_test.go
+++ core/pkg/distribution/mock/cluster_test.go
@@ -24,13 +24,13 @@ var _ = Describe("Cluster", func() {
 	Describe("Name", func() {
 		It("Should open a three node memory backed distribution layer", func(ctx SpecContext) {
 			cluster := mock.MustOpenCluster(ctx, 3)
-			coreOne := cluster.Nodes[node.Key(1)]
-			coreTwo := cluster.Nodes[node.Key(2)]
-			coreThree := cluster.Nodes[node.Key(3)]
+			nodeOne := cluster.Nodes[node.Key(1)]
+			nodeTwo := cluster.Nodes[node.Key(2)]
+			nodeThree := cluster.Nodes[node.Key(3)]
 
-			Expect(coreOne.Cluster.HostKey()).To(Equal(node.Key(1)))
-			Expect(coreTwo.Cluster.HostKey()).To(Equal(node.Key(2)))
-			Expect(coreThree.Cluster.HostKey()).To(Equal(node.Key(3)))
+			Expect(nodeOne.Cluster.HostKey()).To(Equal(node.Key(1)))
+			Expect(nodeTwo.Cluster.HostKey()).To(Equal(node.Key(2)))
+			Expect(nodeThree.Cluster.HostKey()).To(Equal(node.Key(3)))
 
 			ch := channel.Channel{
 				Name:        "SG_01",
@@ -39,12 +39,12 @@ var _ = Describe("Cluster", func() {
 				Leaseholder: 1,
 			}
 
-			Expect(coreOne.Channel.NewWriter(nil).Create(ctx, &ch)).To(Succeed())
+			Expect(nodeOne.Channel.NewWriter(nil).Create(ctx, &ch)).To(Succeed())
 			Expect(ch.Key().Leaseholder()).To(Equal(node.Key(1)))
 
 			Eventually(func(g Gomega) {
 				var resCh channel.Channel
-				g.Expect(coreThree.Channel.NewRetrieve().
+				g.Expect(nodeThree.Channel.NewRetrieve().
 					Where(channel.MatchKeys(ch.Key())).
 					Entry(&resCh).
 					Exec(ctx, nil)).To(Succeed())
