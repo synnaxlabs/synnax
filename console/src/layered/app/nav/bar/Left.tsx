f23fb641007bf62fa10ea081249520a12eca1c39
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2026-06-26 10:37:44 -0600

    fixed linting issues

--- console/src/layered/app/nav/bar/Left.tsx
+++ console/src/layered/app/nav/bar/Left.tsx
@@ -7,7 +7,6 @@
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
 
-import { Logo } from "@synnaxlabs/media";
 import { Nav } from "@synnaxlabs/pluto";
 import { type ReactElement, useCallback } from "react";
 import { useDispatch } from "react-redux";
