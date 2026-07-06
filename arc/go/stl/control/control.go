8f92d2587b6e8e01cea1955d95831fab345da543
sy-nico <nico@synnaxlabs.com>
2026-07-06 15:54:03 -0600

    SY-4413: Rename Arc Stdlib Module Config Structs to Inputs (#2548)

--- arc/go/stl/control/control.go
+++ arc/go/stl/control/control.go
@@ -90,17 +90,17 @@ func (h *Host) Create(_ context.Context, cfg node.Config) (node.Node, error) {
 	if cfg.Node.Type != symbolName {
 		return nil, query.ErrNotFound
 	}
-	var nodeCfg nodeConfig
-	if err := schema.Parse(cfg.Node.Inputs.ValueMap(), &nodeCfg); err != nil {
-		return nil, errors.Wrap(err, "control.set_authority config")
+	var inputs nodeInputs
+	if err := schema.Parse(cfg.Node.Inputs.ValueMap(), &inputs); err != nil {
+		return nil, errors.Wrap(err, "control.set_authority inputs")
 	}
 	var channelKey *uint32
-	if nodeCfg.Channel != 0 {
-		channelKey = &nodeCfg.Channel
+	if inputs.Channel != 0 {
+		channelKey = &inputs.Channel
 	}
 	return &setAuthority{
 		auth:       h.auth,
-		authority:  nodeCfg.Value,
+		authority:  inputs.Value,
 		channelKey: channelKey,
 	}, nil
 }
@@ -110,7 +110,7 @@ var schema = zyn.Object(map[string]zyn.Schema{
 	"channel": zyn.Number().Uint32(),
 })
 
-type nodeConfig struct {
+type nodeInputs struct {
 	Value   uint8  `json:"value"`
 	Channel uint32 `json:"channel"`
 }
