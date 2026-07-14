1269e6f4579901d382a6860cdf318f0106a84615
Patrick Dotson <patrickdotson11@gmail.com>
2026-07-14 14:30:08 -0600

    Fix imports

--- core/pkg/service/imex/imex_suite_test.go
+++ core/pkg/service/imex/imex_suite_test.go
@@ -14,7 +14,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/synnaxlabs/synnax/pkg/distribution/ontology"
+	"github.com/synnaxlabs/synnax/pkg/service/ontology"
 	"github.com/synnaxlabs/x/gorp"
 	"github.com/synnaxlabs/x/kv/memkv"
 	. "github.com/synnaxlabs/x/testutil"
