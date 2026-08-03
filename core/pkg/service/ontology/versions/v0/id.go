95f738cb32487dca3c9e5e14434e2ce728c1a525
Patrick Dotson <patrickdotson11@gmail.com>
2026-08-03 16:23:24 -0600

    SY-4233: Restore the ontology package to its rc state

--- core/pkg/service/ontology/versions/v0/id.go
+++ core/pkg/service/ontology/versions/v0/id.go
@@ -9,13 +9,6 @@
 
 package v0
 
-import (
-	"strings"
-
-	"github.com/synnaxlabs/x/errors"
-	"github.com/synnaxlabs/x/validate"
-)
-
 // String returns a string representation of the ID in the format "Type:Key".
 func (i ID) String() string { return string(i.Type) + ":" + i.Key }
 
@@ -25,25 +18,3 @@ func (i ID) IsZero() bool { return i.Key == "" && i.Type == "" }
 // IsType returns true if the ID represents a type identifier (has a Type but no Key).
 // Type IDs are used to identify resource types rather than specific resource instances.
 func (i ID) IsType() bool { return i.Type != "" && i.Key == "" }
-
-// ParseID parses the given key into an ID.
-func ParseID(key string) (ID, error) {
-	// We explicitly allow IDs that have multiple colons, i.e. "foo:bar:baz" will be
-	// parsed as ID{Type: "foo", Key: "bar:baz"}.
-	split := strings.SplitN(key, ":", 2)
-	if len(split) != 2 {
-		return ID{}, errors.Wrapf(
-			validate.ErrValidation,
-			"[ontology.resource] - failed to parse id: %s",
-			key,
-		)
-	}
-	if split[0] == "" {
-		return ID{}, errors.Wrapf(
-			validate.ErrValidation,
-			"[ontology.resource] - failed to parse id: %s (empty type)",
-			key,
-		)
-	}
-	return ID{Type: ResourceType(split[0]), Key: split[1]}, nil
-}
