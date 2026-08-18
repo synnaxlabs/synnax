ce61f4a61d83f4dd24bf50e3b0abadd83ec728cf
Patrick Dotson <patrickdotson11@gmail.com>
2026-08-18 12:30:34 -0600

    Drop zip.ts from the import barrel

--- console/src/platform/import/external.ts
+++ console/src/platform/import/external.ts
@@ -14,4 +14,3 @@ export * from "@/platform/import/import";
 export * from "@/platform/import/ingestBatch";
 export * from "@/platform/import/ingester";
 export * from "@/platform/import/useFileDrop";
-export * from "@/platform/import/zip";
