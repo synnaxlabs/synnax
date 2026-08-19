8a418f3b0c058535b6e25c1ff156dfa622315cad
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2026-08-18 20:35:22 -0600

    Fix formatting in table Row

--- pluto/src/table/Row.tsx
+++ pluto/src/table/Row.tsx
@@ -129,11 +129,7 @@ const VariantCell = memo(
     const props = useMemo(() => {
       if (cell == null) return null;
       const spec = Cell.REGISTRY[cell.variant];
-      return deep.overrideValidItems(
-        spec.defaultProps(theme),
-        cell.props,
-        spec.schema,
-      );
+      return deep.overrideValidItems(spec.defaultProps(theme), cell.props, spec.schema);
     }, [cell, theme]);
     const handleChange = useCallback(
       (props: record.Unknown) => {
