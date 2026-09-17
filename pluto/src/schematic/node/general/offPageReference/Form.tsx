e0c476354bed999c8e7c28463a3f8ce76766221d
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2026-09-17 07:41:10 +0100

    Fix copyright headers on rewritten schematic files

--- pluto/src/schematic/node/general/offPageReference/Form.tsx
+++ pluto/src/schematic/node/general/offPageReference/Form.tsx
@@ -6,6 +6,7 @@
 // As of the Change Date specified in that file, in accordance with the Business Source
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
+
 import { ontology, schematic } from "@synnaxlabs/client";
 import { color, type text } from "@synnaxlabs/x";
 import { type ReactElement, useCallback, useEffect, useState } from "react";
