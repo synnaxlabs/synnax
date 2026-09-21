c04e7e835e043f9c621e4cc75ad3e903de255580
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2026-09-21 13:17:20 -0600

    Fix the copyright header spacing and a ginkgolinter duration

--- console/src/feature/embedded/testutil.tsx
+++ console/src/feature/embedded/testutil.tsx
@@ -6,6 +6,7 @@
 // As of the Change Date specified in that file, in accordance with the Business Source
 // License, use of this software will be governed by the Apache License, Version 2.0,
 // included in the file licenses/APL.txt.
+
 import { Synnax } from "@synnaxlabs/pluto";
 import { emit } from "@tauri-apps/api/event";
 import { clearMocks, mockIPC } from "@tauri-apps/api/mocks";
